751285b712daeeff6da6dbaa0d1a2322aa246e87
Bo Tao <taobo0626@gmail.com>
2018-03-19 14:22:01 -0700

    add alarm.c, basic and working.

---
+++ posix_threads/my_work/alarm.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main (int argc, char *argv[])
+{
+	char cmd[128] = "\0";
+	int status = 0;
+	char prompt[64] = "Enter alarm > ";
+	char message[128] = "\0";
+	int val = 0;
+
+	if (argc >= 2) {
+		printf("Usage: %s <alarm_in_seconds> <message>\n", argv[0]);
+		return -1;
+	}
+
+	fprintf(stdout, "%s", prompt);
+
+	while (fgets(cmd, sizeof(cmd), stdin) != NULL) {
+		//printf("gets %s\n", cmd);
+		//val = atoi(cmd);
+		if (sscanf(cmd, "%d %s", &val, message) < 2) {
+			printf("bad arguments.\n");
+		} else {
+			sleep(val);
+			printf("slept %d, %s.\n", val, message);
+		}
+		fprintf(stdout, "%s", prompt);
+	}
+
+	return 0;
+}
