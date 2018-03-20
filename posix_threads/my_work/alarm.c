a2686b7bad95eefaf1eaaf7957ae20c3f15ce5cf
Bo Tao <taobo0626@gmail.com>
2018-03-20 15:34:16 -0700

    do fork in alarm.c

--- posix_threads/my_work/alarm.c
+++ posix_threads/my_work/alarm.c
@@ -11,6 +11,7 @@ int main (int argc, char *argv[])
 	char prompt[64] = "Enter alarm > ";
 	char message[128] = "\0";
 	int val = 0;
+	pid_t pid = 0;
 
 	if (argc >= 2) {
 		printf("Usage: %s <alarm_in_seconds> <message>\n", argv[0]);
@@ -25,8 +26,27 @@ int main (int argc, char *argv[])
 		if (sscanf(cmd, "%d %128s", &val, message) < 2) {
 			printf("bad arguments.\n");
 		} else {
-			sleep(val);
-			printf("slept %d, %s.\n", val, message);
+			pid = fork();
+			if (pid == (pid_t) -1) {
+				printf("\nfork failed, aborting...\n");
+				abort();
+			}
+			else if (pid == 0) {
+				//printf("\nThis is the child process:\n");
+				sleep(val);
+				printf("slept %d, %s.\n", val, message);
+				exit(0);
+			}
+			else {
+				//let parent to collect terminated children.
+				do {
+					pid = waitpid((pid_t)-1, NULL, WNOHANG);
+					if (pid == (pid_t) -1) {
+						printf("waiting for child, aborting...\n");
+						abort();
+					}
+				} while (pid != (pid_t)0);
+			}
 		}
 		fprintf(stdout, "%s", prompt);
 	}
