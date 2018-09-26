920ce5a9cd6340d76823cc1ab68ec7cdc6baba8e
Bo Tao <taobo0626@gmail.com>
2018-09-25 21:14:48 -0700

    035 better solution.

--- lc/python/035_search_insert_position.py
+++ lc/python/035_search_insert_position.py
@@ -21,3 +21,24 @@ class Solution(object):
 
         return start
 
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        n = len(nums)
+        start = 0
+        end = n - 1
+        while start < end:
+            mid = (start+end)/2
+            if nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid
+
+        return (start+1) if nums[start] < target else start
+
+
