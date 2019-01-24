45d7046c0d72ee6e003a06021e032d50b684d31f
Bo Tao <taobo0626@gmail.com>
2019-01-23 22:12:12 -0800

    modify 028 implement strstr

--- lc/python/028_implement_strstr.py
+++ lc/python/028_implement_strstr.py
@@ -9,6 +9,31 @@ class Solution(object):
         """
         m = len(haystack)
         n = len(needle)
+
+        if n == 0:
+            return 0
+        
+        if m == 0:
+            return -1
+        
+        for i in range(m-n+1):
+            for j in range(n):
+                if haystack[i+j] != needle[j]:
+                    break
+                if (j == n -1) and (haystack[i+j] == needle[j]):
+                    return i
+            
+        return -1
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        m = len(haystack)
+        n = len(needle)
         if n == 0:
             return 0
         if m == 0:
