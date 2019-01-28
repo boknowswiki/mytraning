2c1f6d4c66c63db994f2151b085fcb55e3d6304d
Bo Tao <taobo0626@gmail.com>
2019-01-27 21:58:10 -0800

    008 modified

--- lc/python/008_string_to_integer.py
+++ lc/python/008_string_to_integer.py
@@ -7,6 +7,43 @@ class Solution(object):
         :rtype: int
         """
         n = len(str)
+        sign = 1
+        i = 0
+        num = 0
+        max_int = 2**31-1
+        min_int = -2**31
+        if n == 0:
+            return 0
+        while i < n and str[i] == ' ':
+            i = i + 1        
+        if i < n and str[i] == '-':
+            sign = -1
+            i = i + 1
+        elif i < n and str[i] == '+':
+            sign = 1
+            i = i + 1
+
+            
+        while i < n and (str[i] >= '0' and str[i] <= '9'):
+            s = ord(str[i]) - ord('0')
+            if (num > (max_int/10)) or \
+                (num == (max_int/10) and (ord(str[i]) - ord('0') >= 8)):
+                if sign == -1:
+                    return min_int
+                else:
+                    return max_int
+            num = num * 10 + s
+            i = i + 1
+            
+        return sign * num
+
+class Solution(object):
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+        n = len(str)
         if n == 0:
             return 0
         ret = 0
