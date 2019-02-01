9ca2efbd27443c2ab27240e243f2985522ec49c4
Bo Tao <taobo0626@gmail.com>
2019-01-31 22:00:28 -0800

    066 new

--- lc/python/066_plus_one.py
+++ lc/python/066_plus_one.py
@@ -7,6 +7,30 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(digits)
+        ret = [0] * n
+        c = 0
+
+        for i in range(n-1, -1, -1):
+            if i == n-1:
+                ret[i] = (digits[i] + 1) %10
+                c = (digits[i]+1) / 10
+                continue
+                
+            ret[i] = (digits[i] + c) % 10
+            c = (digits[i]+c)/10
+            
+        if c == 1:
+            ret.insert(0,1)
+            
+        return ret
+
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        n = len(digits)
         c = 1
         for i in xrange(n-1, -1, -1):
             print digits[i]
