48a334d49c9bdf9240a9b51445f170462057c31a
Bo Tao <taobo0626@gmail.com>
2019-02-05 21:38:18 -0800

    009 new code

--- lc/python/009_palindrome_number.py
+++ lc/python/009_palindrome_number.py
@@ -6,6 +6,30 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
+        if x < 0:
+            return False
+
+        div = 1
+        while x/div >= 10:
+            div = div * 10
+
+        while x:
+            l = x/div
+            r = x%10
+            if l != r:
+                return False
+            x = (x%div)/10
+            div = div / 100
+
+        return True
+
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
         def reverse_int(x):
             ret = 0
             while x:
