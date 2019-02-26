3cb30a1cd43c6697e1c1e20ac838bbab131ea48d
Bo Tao <taobo0626@gmail.com>
2019-02-25 22:21:10 -0800

    150 new

--- lc/python/150_evaluate_reverse_polish_notation.py
+++ lc/python/150_evaluate_reverse_polish_notation.py
@@ -1,5 +1,30 @@
 #!/usr/bin/python -t
 
+class Solution:
+    # @param {string[]} tokens
+    # @return {integer}
+    def __init__(self):
+        self.operators = {
+            '+': lambda y, x: x + y,
+            '-': lambda y, x: x - y,
+            '*': lambda y, x: x * y,
+            '/': lambda y, x: int(operator.truediv(x, y))
+        }
+
+    def evalRPN(self, tokens):
+        if not tokens:
+            return 0
+
+        stack = []
+
+        for token in tokens:
+            if token in self.operators:
+                stack.append(self.operators[token](stack.pop(), stack.pop()))
+            else:
+                stack.append(int(token))
+
+        return stack[0]
+
 class Solution(object):
     def evalRPN(self, tokens):
         """
