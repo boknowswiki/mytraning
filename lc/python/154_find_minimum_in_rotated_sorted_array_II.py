7448a1d3b9efc210ecf44bb486a144fc41c27c2d
Bo Tao <taobo0626@gmail.com>
2019-03-10 22:10:06 -0700

    one more way

--- lc/python/154_find_minimum_in_rotated_sorted_array_II.py
+++ lc/python/154_find_minimum_in_rotated_sorted_array_II.py
@@ -20,3 +20,24 @@ class Solution(object):
 
         return nums[start]
 
+
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        start = 0
+        end = n - 1
+        
+        while start < end:
+            mid = (start+end)/2
+            if nums[mid] > nums[end]:
+                start = mid + 1
+            elif nums[mid] < nums[end]:
+                end = mid
+            else:
+                end = end - 1
+                
+        return nums[start]
