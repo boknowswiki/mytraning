88cc97ee23d6a58caccea3c19122da31ed7c8824
Bo Tao <taobo0626@gmail.com>
2019-04-07 21:59:50 -0700

    217 done

---
+++ lc/python/217_contains_duplicate.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python -t
+
+
+#Time: O(nlogn), space O(1)
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        n = len(nums)
+        nums.sort()
+        
+        index = 0
+        
+        for i in range(1, n):
+            if nums[i] == nums[index]:
+                return True
+            else:
+                index = index + 1
+                
+        return False
+
+        
+
+#Time: O(n), space O(n)
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        n = len(nums)
+        d = set()
+        
+        for i in range(n):
+            if nums[i] in d:
+                return True
+            else:
+                d.add(nums[i])
+                
+        return False
+
