1a44e797e776bf14ead9540e6b6df12f7db560a6
Bo Tao <taobo0626@gmail.com>
2019-04-13 22:36:33 -0700

    062 better

--- lc/python/062_unique_paths.py
+++ lc/python/062_unique_paths.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python -t
 
+#time O(mn) space O(n)
+
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        r = [0] * (n+1)
+        r[0] = 1
+        
+        for i in range(m):
+            for j in range(1, n+1):
+                r[j] = r[j-1] + r[j]
+        
+        return r[n-1]
+
+
 '''
 DP
 '''
