abaa8153ed3811bee69c28eafda95aaeb3cd6066
Bo Tao <taobo0626@gmail.com>
2019-04-22 20:07:36 -0700

    144 done

---
+++ lc/python/144_binary_tree_preorder_traversal.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python -t
+
+#time O(n) space O(n)
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        ret = []
+        stack = [root]
+        
+        while stack:
+            node = stack.pop()
+            if node:
+                ret.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+                
+        return ret
+
+#time O(n) space O(1)
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        def preorder(root, ret):
+            if root:
+                ret.append(root.val)
+                preorder(root.left, ret)
+                preorder(root.right, ret)
+                
+            return
+        
+        ret = []
+
+        preorder(root, ret)
+        return ret
