31b623d225a2c691122a1117dcfd85aa69222227
Bo Tao <taobo0626@gmail.com>
2019-04-22 22:09:06 -0700

    094 one more way

--- lc/python/094_binary_tree_inorder_traversal.py
+++ lc/python/094_binary_tree_inorder_traversal.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python -t
 
+    def inorderTraversal(self, root):
+        if root is None:
+            return []
+        res = []
+        stack = [root]
+        while len(stack) > 0:
+            curr = stack.pop()
+            if not isinstance(curr, TreeNode):
+                res.append(curr)
+                continue
+            if curr.right is not None:
+                stack.append(curr.right)
+            stack.append(curr.val)
+            if curr.left is not None:
+                stack.append(curr.left)
+        return res
+
 #time O(n) space O(n)
 
 # Definition for a binary tree node.
