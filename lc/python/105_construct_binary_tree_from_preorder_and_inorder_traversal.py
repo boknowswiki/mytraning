14b79aaac5616672618ea8cb3ef8f288e1a5896c
Bo Tao <taobo0626@gmail.com>
2019-04-26 22:38:33 -0700

    105 iter

--- lc/python/105_construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ lc/python/105_construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -25,3 +25,55 @@ class Solution(object):
         
         return node
 
+
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        if len(preorder) == 0:
+            return None
+            
+        head = TreeNode(preorder[0])
+        stack = [head]
+        i = 1
+        j = 0
+        
+        while i < len(preorder):
+            temp = None
+            t = TreeNode(preorder[i])
+            while stack and stack[-1].val == inorder[j]:
+                temp = stack.pop()
+                j += 1
+            if temp:
+                temp.right = t
+            else:
+                stack[-1].left = t
+            stack.append(t)
+            i += 1
+        
+        return head
+
+
+class Solution:
+    def buildTree(self, preorder, inorder):
+        if not preorder:
+            return None
+        
+        root = TreeNode(preorder[0])
+        stack = []
+        stack.append(root)
+        
+        pre = 1
+        ino = 0
+        while (pre < len(preorder)):
+            curr = TreeNode(preorder[pre])
+            pre += 1
+            prev = None
+            while stack and stack[-1].val == inorder[ino]:
+                prev = stack.pop()
+                ino += 1
+            if prev:
+                prev.right = curr
+            else:
+                stack[-1].left = curr
+                
+            stack.append(curr)
+        return root
