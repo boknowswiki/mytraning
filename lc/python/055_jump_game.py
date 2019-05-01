3a505a05ad539cd5a81418a5065f236028a8dc8a
Bo Tao <taobo0626@gmail.com>
2019-04-30 21:14:08 -0700

    055 add

---
+++ lc/python/055_jump_game.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python -t
+
+#time O(n) space O(1)
+
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        n = len(nums)
+        lastpos = n - 1
+        
+        for i in range(n-1, -1, -1):
+            if i + nums[i] >= lastpos:
+                lastpos = i
+                
+        return lastpos == 0
+
+
+#time limit exceeded
+#time O(2^n) space O(2n)
+
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        m = [0] *len(nums)
+        m[len(nums)-1] = 1
+        
+        for i in range(len(nums)-2, -1, -1):
+            further = min(i + nums[i], len(nums)-1)
+            for j in range(i+1, further+1):
+                if m[j] == 1:
+                    m[i] = 1
+                    break
+                    
+                
+        return m[0] == 1
+
+
+#memory limit exceeded
+#time O(2^n) space O(2n)
+
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        def dfs(nums, start, m):
+            if m[start] != 0:
+                return True if m[start] == 1 else False
+            
+            further_index = min(start+nums[start], len(nums)-1)
+            
+            for index in range(start+1, further_index+1):
+                if dfs(nums, index, m):
+                    m[start] = 1
+                    return True
+            m[start] = -1
+            return False
+        
+        m = [0] *len(nums)
+        m[len(nums)-1] = 1
+        return dfs(nums, 0, m)
+
+
+#time or memory limit exceeded
+#time O(2^n) space O(n)
+
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        def dfs(nums, start):
+            if start == len(nums) -1:
+                return True
+            
+            further_index = min(start+nums[start], len(nums)-1)
+            
+            for index in range(start+1, further_index+1):
+                if dfs(nums, index):
+                    return True
+                
+            return False
+        
+        return dfs(nums, 0)
