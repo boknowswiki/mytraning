0c574fec37f3441f8d15773109779dc11eede88c
Bo Tao <taobo0626@gmail.com>
2019-05-01 21:29:02 -0700

    162 linear and binary search

--- lc/python/162_find_peak_element.py
+++ lc/python/162_find_peak_element.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python -t
 
+#time O(n) space O(1)
+
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        
+        for i in range(n-1):
+            if nums[i] > nums[i+1]:
+                return i
+            
+        return n-1
+
+#time O(log2 ^ n) space O(1)
+
 class Solution(object):
     def findPeakElement(self, nums):
         """
