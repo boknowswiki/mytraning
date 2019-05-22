0409256fe50ecaa40fd7fef06519aacba346146a
Bo Tao <taobo0626@gmail.com>
2019-05-21 23:26:38 -0700

    350 sort and binary search

--- lc/python/350_intersection_of_two_arrays_II.py
+++ lc/python/350_intersection_of_two_arrays_II.py
@@ -25,3 +25,47 @@ class Solution(object):
                 j = j + 1
                 
         return ret
+
+#binary search
+#time max(O(mlogm), O(nlogn), O(mlogn) or max(O(mlogm), O(nlogn), O(nlogm)) space max(O(m), O(n))
+
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        def lower_bound(nums, val):
+            l = 0
+            r = len(nums) - 1
+            while l < r:
+                m = l + (r-l)/2
+                if nums[m] < val:
+                    l = m+1
+                else:
+                    r = m
+            return l
+            
+        nums1.sort()
+        nums2.sort()
+        len1 = len(nums1)
+        len2 = len(nums2)
+        i = j = 0
+        ret = []
+        
+        while i < len1:
+            index = lower_bound(nums2, nums1[i])
+            count1 = 0
+            while index < len2 and nums2[index] == nums1[i]:
+                count1 = count1 + 1
+                index = index + 1
+                
+            count2 = 0
+            while j < len1 and nums1[j] == nums1[i]:
+                count2 = count2+1
+                j = j + 1
+            ret.extend([nums1[i]]*min(count1, count2))
+            
+            i = j
+        return ret
