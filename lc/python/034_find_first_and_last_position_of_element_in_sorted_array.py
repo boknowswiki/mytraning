6af7d5c3b51136453d31173da3dd812ff49d8242
Bo Tao <taobo0626@gmail.com>
2019-05-28 22:48:41 -0700

    034 add my binary search lower/upper bound

--- lc/python/034_find_first_and_last_position_of_element_in_sorted_array.py
+++ lc/python/034_find_first_and_last_position_of_element_in_sorted_array.py
@@ -1,5 +1,51 @@
 #!/usr/bin/python -t
 
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        def left(nums, target):
+            n = len(nums)
+            l = 0
+            r = n
+            
+            while l < r:
+                m = l + (r-l)/2
+                if nums[m] < target:
+                    l = m+1
+                else:
+                    r = m
+                    
+            return l if target in nums[l:l+1] else -1
+        
+        def right(nums, target):
+            n = len(nums)
+            l = 0
+            r = n
+            
+            while l < r:
+                m = l + (r-l)/2
+                if nums[m] <= target:
+                    l = m + 1
+                else:
+                    r = m
+                    
+            print r
+            return r-1 if target == nums[r-1] else -1
+        n = len(nums)
+        if n == 0:
+            return [-1,-1]
+        
+        l = left(nums, target)
+        r = right(nums, target)
+        print l, r
+        
+        return [l, r]
+
+'''
 class Solution:
     # returns leftmost (or rightmost) index at which `target` should be inserted in sorted
     # array `nums` via binary search.
@@ -111,3 +157,26 @@ class Solution(object):
                 end = mid - 1
                 
         return ret
+
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n = len(nums)
+        if n == 0:
+            return [-1, -1]
+
+        low = bisect.bisect_left(nums, target)
+        high = bisect.bisect_right(nums, target)
+        
+        return [low, high-1] if target in nums[low:low+1] else [-1, -1]
+'''
+
+if __name__ =='__main__':
+    s = Solution()
+    ss = [5,7,7,8,8,10]
+    t = 8
+    s.searchRange(ss, t)
