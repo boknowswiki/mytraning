ea346ebcc4b8d5ca87499aa0a3edef10f4f36289
Bo Tao <taobo0626@gmail.com>
2019-05-28 22:23:41 -0700

    add python lower bound

--- lc/python/binary_search_base.py
+++ lc/python/binary_search_base.py
@@ -63,3 +63,21 @@ def upper_bound(nums, val):
 #  }
 #  return first;
 #}
+
+#lower_bound
+length = len(citations)
+         
+        first = 0
+        count = length
+         
+        while count > 0:
+            step = count / 2
+            mid = first + step
+            if citations[mid] < length - mid:
+                first = mid + 1
+                count -= (step + 1)
+            else:
+                count = step
+         
+        return length - first
+
