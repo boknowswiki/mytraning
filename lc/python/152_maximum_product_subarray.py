9c472377f0131ff26ba4718b69c00528b91b1e7c
Bo Tao <taobo0626@gmail.com>
2019-06-16 21:55:53 -0700

    152 add myself solution

--- lc/python/152_maximum_product_subarray.py
+++ lc/python/152_maximum_product_subarray.py
@@ -1,5 +1,29 @@
 #!/usr/bin/python -t
 
+#time O(n) space O(1)
+#myself resolved
+
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n == 0:
+            return 0
+        ret = max_pro = min_pro = nums[0]
+        
+        for i in range(1, n):
+            if nums[i] < 0:
+                max_pro, min_pro = min_pro, max_pro
+                
+            max_pro = max(max_pro*nums[i], nums[i])
+            min_pro = min(min_pro *nums[i], nums[i])
+            ret = max(ret, max_pro)
+            
+        return ret
+
 class Solution(object):
     def maxProduct(self, nums):
         """
