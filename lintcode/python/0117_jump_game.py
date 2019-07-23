8252d725040e01d8f71772f315d7b584e051e1fd
Bo Tao <taobo0626@gmail.com>
2019-07-22 23:29:32 -0700

    0117 add greedy solution

--- lintcode/python/0117_jump_game.py
+++ lintcode/python/0117_jump_game.py
@@ -1,6 +1,31 @@
 #!/usr/bin/python
 
+#greedy O(n)
+
+class Solution:
+    """
+    @param A: A list of integers
+    @return: An integer
+    """
+    def jump(self, A):
+        # write your code here
+        n = len(A)
+        min_step = 0
+        max_dis = 0
+        last = 0
+        
+        for i in range(n):
+            if i > last:
+                last = max_dis
+                min_step += 1
+                
+            max_dis = max(max_dis, i+A[i])
+            
+        return min_step
+
+
 #dp solution
+#O(n2)
 
 class Solution:
     """
