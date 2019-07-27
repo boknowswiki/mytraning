07bb8144a69804600e76fac839746a890cb58426
Bo Tao <taobo0626@gmail.com>
2019-07-26 17:07:45 -0700

    322 add dp solution

--- lc/python/322_coin_change.py
+++ lc/python/322_coin_change.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python -t
 
+# dp solution O(mn)
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [sys.maxint]*(amount+1)
+        dp[0] = 0
+        
+        for i in range(1, amount+1):
+            for c in coins:
+                if c <= i:
+                    dp[i] = min(dp[i], dp[i-c]+1)
+                    
+        return -1 if dp[amount] == sys.maxint else dp[amount]
+
 '''
 
 class Solution(object):
