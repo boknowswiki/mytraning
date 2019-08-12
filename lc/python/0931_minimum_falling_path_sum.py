42cc1db356f28b60b4606f413f09772bd3c5c228
boknowswiki <boknowswiki@gmail.com>
2019-08-12 13:51:01 -0700

    0931 dp solution

---
+++ lc/python/0931_minimum_falling_path_sum.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python -t
+
+# dp time O(m*n) space O(1), modified on A.
+
+class Solution(object):
+    def minFallingPathSum(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: int
+        """
+        m = len(A)
+        if m == 0:
+            return 0
+        
+        n = len(A[0])
+            
+        for i in range(m-2, -1, -1):
+            for j in range(n):
+                A[i][j] = min(A[i+1][j-1] if j>=1 else sys.maxint,
+                              A[i+1][j], 
+                              A[i+1][j+1] if j<=n-2 else sys.maxint) + A[i][j]
+        
+        return min(ret for ret in A[0])
+
+# my dp solution time O(m*n) space O(m*n)
+# state: dp[i][j] the minimum sum at point i, j
+# function: dp[i][j] = min(dp[i-1][j], dp[i-1][j-1], dp[i-1][j+1]) + A[i][j]
+# init: dp[0][j] = A[0][j] for the first row
+# result: min(dp[m-1][j] for j in range(n))
+
+import sys
+
+class Solution(object):
+    def minFallingPathSum(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: int
+        """
+        m = len(A)
+        if m == 0:
+            return 0
+        
+        n = len(A[0])
+        
+        dp = [[sys.maxint] * (n) for i in range(m)]
+        for j in range(n):
+            dp[0][j] = A[0][j]
+        
+        for i in range(1, m):
+            for j in range(n):
+                dp[i][j] = min(dp[i-1][j] if i>=1 else sys.maxint,
+                              dp[i-1][j-1] if i>= 1 and j >= 1 else sys.maxint,
+                              dp[i-1][j+1] if i >= 1 and j<=n-2 else sys.maxint) + A[i][j]
+        print dp
+        
+        return min(ret for ret in dp[m-1])
+
+if __name__ == '__main__':
+    s = [[1,2,3],[4,5,6],[7,8,9]]
+    ss = Solution()
+    print('answer is %r' % ss.minFallingPathSum(s))
