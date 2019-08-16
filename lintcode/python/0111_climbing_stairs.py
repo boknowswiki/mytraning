ece1e681300a1d4b8e48526221b5484ed09f65de
Bo Tao <taobo0626@gmail.com>
2019-08-15 22:04:17 -0700

    0111 better dp solution

--- lintcode/python/0111_climbing_stairs.py
+++ lintcode/python/0111_climbing_stairs.py
@@ -1,5 +1,32 @@
 #!/usr/bin/python -t
 
+# dp, time O(n), space O(1)
+
+class Solution:
+    """
+    @param n: An integer
+    @return: An integer
+    """
+    def climbStairs(self, n):
+        # write your code here
+        # state: dp[i], distinct ways to reach ith stair
+        # function: dp[i] = dp[i-1]+dp[i-2]
+        # init: dp[0] = 0, dp[1] = 1, dp[2] = 2
+        # result: dp[n]
+        
+        if n < 3:
+            return n
+            
+        last = 1
+        lastlast = 1
+        now = 0        
+        for i in range(2, n+1):
+            now = last + lastlast
+            lastlast = last
+            last = now
+            
+        return now
+
 # dp solution, time O(n), space O(n)
 
 class Solution:
