66952d16b77539e9b6e5927e90e7a691a09f0c53
boknowswiki <boknowswiki@gmail.com>
2019-08-21 13:30:34 -0700

    0092 explain

--- lintcode/python/0092_backpack.py
+++ lintcode/python/0092_backpack.py
@@ -1,5 +1,36 @@
 #!/usr/bin/python -t
 
+# dp solution, space O(mn) space O(mn)
+
+class Solution:
+    """
+    @param m: An integer m denotes the size of a backpack
+    @param A: Given n items with size A[i]
+    @return: The maximum size
+    """
+    def backPack(self, m, A):
+        # write your code here
+        # state: dp[i][j], for i items, can it put into j size backpack
+        # function: dp[i][j] = true if j > A[i-1] and dp[i-1][j-A[i-1] is true
+        # init: dp[i][j] = false and dp[0][0] = true
+        # result: ret = max(ret, dp[n][i])
+        
+        n = len(A)
+        
+        dp = [[False] * (m+1) for i in range(n+1)]
+        
+        dp[0][0] = True
+        
+        for i in range(n):
+            for j in range(m+1):
+                dp[i+1][j] = dp[i][j]
+                if j >= A[i] and dp[i][j-A[i]]:
+                    dp[i+1][j] = True
+                    
+        for i in range(m, -1, -1):
+            if dp[n][i]:
+                return i
+
 # dp solution, time O(mn) space O(m)
 
 class Solution:
