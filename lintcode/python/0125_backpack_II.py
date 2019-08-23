db6b714e29b20d5ea6cd9641fc664a8d62211c57
boknowswiki <boknowswiki@gmail.com>
2019-08-23 09:50:43 -0700

    my own dp solution

--- lintcode/python/0125_backpack_II.py
+++ lintcode/python/0125_backpack_II.py
@@ -1,5 +1,32 @@
 #!/usr/bin/python -t
 
+# time O(mn) space O(mn) better solution belows
+
+class Solution:
+    """
+    @param m: An integer m denotes the size of a backpack
+    @param A: Given n items with size A[i]
+    @param V: Given n items with value V[i]
+    @return: The maximum value
+    """
+    def backPackII(self, m, A, V):
+        # write your code here
+        # state: dp[i][j] the maximum val for first ith items for j size backpack
+        # function: dp[i][j] = max(dp[i-1][j], dp[i-1][j-A[i-1]]+ V[i-1])
+        # init: dp[i][j] = 0
+        # result: dp[m][n]
+        
+        n = len(A)
+        dp = [[0] * (m+1) for i in range(n+1)]
+        
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                dp[i][j] = dp[i-1][j]
+                if j >= A[i-1]:
+                    dp[i][j] = max(dp[i][j], dp[i-1][j-A[i-1]]+ V[i-1])
+                    
+        return dp[n][m]
+
 # dp solution, time O(mn), space O(m)
 
 class Solution:
