ed91b0d4cba5afe7b81ba59222445cc863ca0847
boknowswiki <boknowswiki@gmail.com>
2019-08-29 16:46:28 -0700

    0092 better dp

--- lintcode/python/0092_backpack.py
+++ lintcode/python/0092_backpack.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python -t
 
+# dp solution time O(nk) space O(k)
+
+class Solution:
+    """
+    @param m: An integer m denotes the size of a backpack
+    @param A: Given n items with size A[i]
+    @return: The maximum size
+    """
+    def backPack(self, m, A):
+        # write your code here
+        # dp[j] the max size can fill
+        n = len(A)
+        
+        dp = [0] * (m+1)
+        
+        for i in range(n):
+            for j in range(m, A[i]-1, -1):
+                dp[j] = max(dp[j], dp[j-A[i]]+A[i])
+                
+        print dp
+                
+        return dp[m]
+
 # dp solution, space O(mn) space O(mn)
 
 class Solution:
@@ -79,37 +102,6 @@ class Solution:
                 if j >= A[i] and dp[i][j-A[i]]:
                     dp[i+1][j] = True
                     
-        for i in range(m, -1, -1):
-            if dp[n][i]:
-                return i
-                
-        return 0
-
-
-class Solution:
-    """
-    @param m: An integer m denotes the size of a backpack
-    @param A: Given n items with size A[i]
-    @return: The maximum size
-    """
-    def backPack(self, m, A):
-        # write your code here
-        n = len(A)
-        
-        dp = [[False] * (m+1) for i in range(n+1)]
-        
-        dp[0][0] = True
-        
-        for i in range(n):
-            dp[i][0] = True
-        
-        for i in range(1, n+1):
-            for j in range(m+1):
-                if j >= A[i-1]:
-                    dp[i][j] = dp[i-1][j] or dp[i-1][j-A[i-1]]
-                else:
-                    dp[i][j] = dp[i-1][j]
-                
         for i in range(m, -1, -1):
             if dp[n][i]:
                 return i
