a6e23750d53ee43d0b7e5add521c98fb5684cc72
boknowswiki <boknowswiki@gmail.com>
2019-09-23 16:41:18 -0700

    1219 binary search

---
+++ lintcode/python/1219_heaters.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python -t
+
+# binary search solution
+
+class Solution:
+    """
+    @param houses: positions of houses
+    @param heaters: positions of heaters
+    @return: the minimum radius standard of heaters
+    """
+    def findRadius(self, houses, heaters):
+        # Write your code here
+        heaters.sort()
+        ans = 0
+        for house in houses:
+            ans=max(ans,self.closestHeater(house,heaters))
+        return ans
+        
+    def closestHeater(self,house,heaters):
+        start = 0
+        end = len(heaters) - 1
+        while start +1 < end:
+            m = start + (end - start) // 2
+            if heaters[m] == house:
+                return 0
+            elif heaters[m] < house:
+                start = m
+            else:
+                end = m
+                
+        return min(abs(house - heaters[start]), abs(heaters[end] - house))
+
