8c089f1bab7d494fcc694029639664c701e88321
boknowswiki <boknowswiki@gmail.com>
2019-09-23 13:27:38 -0700

    0458 binary search

---
+++ lintcode/python/0458_last_position_of_target.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python -t
+
+# binary search solution, time O(logn) space O(1)
+
+class Solution:
+    """
+    @param nums: An integer array sorted in ascending order
+    @param target: An integer
+    @return: An integer
+    """
+    def lastPosition(self, nums, target):
+        # write your code here
+        n = len(nums)
+        if n == 0:
+            return -1
+        l = 0
+        r = n - 1
+        
+        while l <= r:
+            mid = (l+r)/2
+            if nums[mid] <= target:
+                l = mid + 1
+            else:
+                r = mid
+        if nums[l] == target:
+            return l
+            
+        if nums[l-1] == target:
+            return l-1
+        
+        return -1
+
