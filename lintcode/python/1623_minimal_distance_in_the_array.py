fb96ce317e5537639080c165de9652f931737d97
Bo Tao <taobo0626@gmail.com>
2019-09-23 22:13:09 -0700

    1623 binary search

---
+++ lintcode/python/1623_minimal_distance_in_the_array.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python -t
+
+# binary search solution
+
+
+class Solution:
+    """
+    @param a: array a
+    @param b: the query array
+    @return: Output an array of length `b.length` to represent the answer
+    """
+    def minimalDistance(self, a, b):
+        # Write your code here
+        ret = b
+        n = len(a)
+        m = len(b)
+        a.sort()
+        
+        for i in range(m):
+            id = self.lower_bound(a, b[i])
+            if id == 0:
+                ret[i] = a[id]
+            elif id == n:
+                ret[i] = a[id-1]
+            else:
+                if (b[i] - a[id - 1] <= a[id] - b[i]):
+                    ret[i] = a[id - 1]
+                else:
+                    ret[i] = a[id]
+        return ret
+
+    def lower_bound(self, a, val):
+        n = len(a)
+        l = 0
+        r = n-1
+        while l < r:
+            mid = (l+r)/2
+            if a[mid] < val:
+                l = mid +1
+            else:
+                r = mid
+                
+        return l
+
