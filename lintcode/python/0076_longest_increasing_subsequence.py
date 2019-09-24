77271adc4d587d3b7ad568f5999165a422dedb43
boknowswiki <boknowswiki@gmail.com>
2019-09-24 13:57:57 -0700

    add binary search solution

--- lintcode/python/0076_longest_increasing_subsequence.py
+++ lintcode/python/0076_longest_increasing_subsequence.py
@@ -1,5 +1,44 @@
 #!/usr/bin/python -t
 
+# binary search solution time O(nlogn) space O(n)
+# both lower bound and upper bound pass the code
+
+class Solution:
+    """
+    @param nums: An integer array
+    @return: The length of LIS (longest increasing subsequence)
+    """
+    def longestIncreasingSubsequence(self, nums):
+        # write your code here
+        n = len(nums)
+        if n == 0:
+            return 0
+            
+        l, r = 0, n-1
+        
+        ret = [nums[0]]
+        
+        for num in nums[1:]:
+            if num > ret[-1]:
+                ret.append(num)
+            elif num < ret[-1]:
+                overwrite_index = self.lower_bound(ret, num)
+                ret[overwrite_index] = num
+                
+        return len(ret)
+        
+    def lower_bound(self, nums, val):
+        l,r = 0, len(nums)-1
+        
+        while l < r:
+            mid = (l+r)/2
+            if nums[mid] < val:
+                l = mid+1
+            else:
+                r = mid
+                
+        return l
+
 # dp solution, time O(n^2) space O(n)
 
 class Solution:
