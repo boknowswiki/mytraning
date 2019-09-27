df503da4508baeee50939aebd350f7dfa2ff8dff
boknowswiki <boknowswiki@gmail.com>
2019-09-27 13:25:01 -0700

    0414 done

---
+++ lintcode/python/0414_divide_two_integers.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python -t
+
+class Solution:
+    """
+    @param dividend: the dividend
+    @param divisor: the divisor
+    @return: the result
+    """
+    def divide(self, dividend, divisor):
+        # write your code here
+        INT_MAX = 2147483647
+        if divisor == 0:
+            return INT_MAX
+        neg = dividend > 0 and divisor < 0 or dividend < 0 and divisor > 0
+        a, b = abs(dividend), abs(divisor)
+        ans, shift = 0, 31
+        while shift >= 0:
+            if a >= b << shift:
+                a -= b << shift
+                ans += 1 << shift
+            shift -= 1
+        if neg:
+            ans = - ans
+        if ans > INT_MAX:
+            return INT_MAX
+        return ans
+
+if __name__ == '__main__':
+    s = 100
+    t = 9
+    ss = Solution()
+    print "answer is\n"
+    print ss.divide(s, t)
