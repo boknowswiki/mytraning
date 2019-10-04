19fcdcab483979857fb6006a3ac7300ed10ac4ad
Bo Tao <taobo0626@gmail.com>
2019-10-03 22:31:57 -0700

    0066 iteration way

--- lintcode/python/0066_binary_tree_preorder_traversal.py
+++ lintcode/python/0066_binary_tree_preorder_traversal.py
@@ -1,5 +1,38 @@
 #!/usr/bin/python -t
 
+# preorder iteration solution
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: Preorder in ArrayList which contains node values.
+    """
+    def preorderTraversal(self, root):
+        # write your code here
+        ret = []
+        if root == None:
+            return ret
+            
+        s = [root]
+        
+        while len(s) > 0:
+            node = s.pop()
+            ret.append(node.val)
+            if node.right:
+                s.append(node.right)
+            if node.left:
+                s.append(node.left)
+                
+        return ret
+
 # binary tree traversal, recursive way
 
 """
