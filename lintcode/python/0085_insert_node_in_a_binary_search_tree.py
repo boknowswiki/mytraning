da1b9ba64552baae2df19a110dcefb989299c541
boknowswiki <boknowswiki@gmail.com>
2019-10-17 08:42:14 -0700

    0085 iteration way

--- lintcode/python/0085_insert_node_in_a_binary_search_tree.py
+++ lintcode/python/0085_insert_node_in_a_binary_search_tree.py
@@ -1,5 +1,42 @@
 #!/usr/bin/python -t
 
+# iteration way
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+
+class Solution:
+    """
+    @param: root: The root of the binary search tree.
+    @param: node: insert this node into the binary search tree
+    @return: The root of the new binary search tree.
+    """
+    def insertNode(self, root, node):
+        # write your code here
+        if root == None:
+            return node
+            
+        cur = root
+        
+        while cur != node:
+            if cur.val < node.val:
+                if cur.right == None:
+                    cur.right = node
+                cur = cur.right
+            else:
+                if cur.left == None:
+                    cur.left = node
+                cur = cur.left
+                
+        return root
+        
+
 # dfs, traversal way
 
 """
