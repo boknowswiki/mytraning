b4f96843d34957a1cfedab7ae52f28fa4dce4493
Bo Tao <taobo0626@gmail.com>
2019-10-17 23:08:26 -0700

    1744 dfs reverse inorder way

--- lintcode/python/1744_increasing_order_search_tree.py
+++ lintcode/python/1744_increasing_order_search_tree.py
@@ -36,4 +36,41 @@ class Solution:
                 p = p.right
             
         return dummy.right
+
+
+# dfs reverse inorder way
         
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: a binary search tree
+    @return: Root of a tree
+    """
+    def increasingBST(self, root):
+        # Write your code here.
+        self.prev = None
+        self.dfs(root)
+        
+        return self.prev
+        
+    def dfs(self, node):
+        if node == None:
+            return None
+        
+        l, r = node.left, node.right
+        
+        self.dfs(r)
+        node.left = None
+        node.right = self.prev
+        self.prev = node
+        self.dfs(l)
+        
+        return
