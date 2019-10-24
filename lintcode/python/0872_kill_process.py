4c6ff67ddbba82107710a95b7e959587459cea31
boknowswiki <boknowswiki@gmail.com>
2019-10-24 09:36:02 -0700

    0872 dfs

--- lintcode/python/0872_kill_process.py
+++ lintcode/python/0872_kill_process.py
@@ -33,6 +33,34 @@ class Solution:
                 q += mapping[p]
                 
         return ret
+
+# DFS
+
+class Solution:
+    """
+    @param pid: the process id
+    @param ppid: the parent process id
+    @param kill: a PID you want to kill
+    @return: a list of PIDs of processes that will be killed in the end
+    """
+
+    def killProcess(self, pid, ppid, kill):
+        n = len(pid)
+        child = {}
+        for i in range(n):
+            if ppid[i] not in child:
+                child[ppid[i]] = []
+            child[ppid[i]].append(pid[i])
+
+        ans = []
+        self.dfs(kill, ans, child)
+        return ans
+
+    def dfs(self, now, ans, child):
+        ans.append(now)
+        if now in child:
+            for nxt in child[now]:
+                self.dfs(nxt, ans, child)
         
 
 if __name__ == '__main__':
