12f3919d51dbbbe939240f8f36efb01a5208bc5c
boknowswiki <boknowswiki@gmail.com>
2019-10-31 13:30:35 -0700

    0165 linked list

---
+++ lintcode/python/0165_merge_two_sorted_lists.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python -t
+
+# linked list
+
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param l1: ListNode l1 is the head of the linked list
+    @param l2: ListNode l2 is the head of the linked list
+    @return: ListNode head of linked list
+    """
+    def mergeTwoLists(self, l1, l2):
+        # write your code here
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+            
+        dummy = ListNode(0)
+        prev = dummy
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+                
+            prev = prev.next
+            
+        if l1:
+            prev.next = l1
+        if l2:
+            prev.next = l2
+            
+        return dummy.next
+        
+        
