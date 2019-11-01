0337ff39e6fb31f7e8980c11f79862e4373d76dc
Bo Tao <taobo0626@gmail.com>
2019-10-31 21:39:16 -0700

    0372 linked list

---
+++ lintcode/python/0372_delete_node_in_a_linked_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python -t
+
+# linked list
+
+"""
+Definition of ListNode
+class ListNode(object):
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+
+class Solution:
+    """
+    @param: node: the node in the list should be deleted
+    @return: nothing
+    """
+    def deleteNode(self, node):
+        # write your code here
+        if not node or not node.next:
+            return
+        
+        node.val = node.next.val
+        node.next = node.next.next
+        
+        return
+    
