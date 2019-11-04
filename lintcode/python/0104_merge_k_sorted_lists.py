f73134a302c48af7b183a7d27ae1a23d66c7e636
boknowswiki <boknowswiki@gmail.com>
2019-11-04 08:45:23 -0800

    0104 linked list

---
+++ lintcode/python/0104_merge_k_sorted_lists.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python -t
+
+# linked list with divid and conquer
+
+# time O(nlogk)
+
+"""
+Definition of ListNode
+class ListNode(object):
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+class Solution:
+    """
+    @param lists: a list of ListNode
+    @return: The head of one sorted list.
+    """
+    def mergeKLists(self, lists):
+        # write your code here
+        n = len(lists)
+        if n == 0:
+            return None
+            
+        return self.helper(lists, 0, n-1)
+            
+            
+    def helper(self, lists, start, end):
+        if start == end:
+            return lists[start]
+            
+        mid = (end-start)/2+start
+        
+        left = self.helper(lists, start, mid)
+        right = self.helper(lists, mid+1, end)
+        
+        return self.merge(left, right)
+        
+    def merge(self, l1, l2):
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+            
+        dummy = ListNode(0)
+        prev = dummy
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+                
+            prev = prev.next
+            
+        if l1:
+            prev.next = l1
+        else:
+            prev.next = l2
+            
+        return dummy.next
+        
+        
