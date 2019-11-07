a9b4ca56538d948acbb4a21ea3793f499887aab2
boknowswiki <boknowswiki@gmail.com>
2019-11-07 13:18:59 -0800

    0135 better

--- lintcode/python/0135_combination_sum.py
+++ lintcode/python/0135_combination_sum.py
@@ -1,5 +1,39 @@
 #!/usr/bin/python -t
 
+# DFS, better
+
+class Solution:
+    """
+    @param candidates: A list of integers
+    @param target: An integer
+    @return: A list of lists of integers
+    """
+    def combinationSum(self, candidates, target):
+        # write your code here
+        c = sorted(set(candidates))
+        ret = []
+        self.dfs(c, target, 0, [], ret)
+        
+        return ret
+        
+    def dfs(self, c, target, start, combination, ret):
+        if target < 0:
+            return
+        
+        if target == 0:
+            ret.append(list(combination))
+            
+        for i in range(start, len(c)):
+            if target < c[i]:
+                break
+            combination.append(c[i])
+            self.dfs(c, target-c[i], i, combination, ret)
+            combination.pop()
+            
+        return
+    
+    
+
 # DFS
 
 class Solution:
