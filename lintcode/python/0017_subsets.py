44e5669a8ecbf649cfab8fff7cf0bba609383609
boknowswiki <boknowswiki@gmail.com>
2019-11-11 17:56:21 -0800

    0017 dfs and bfs

---
+++ lintcode/python/0017_subsets.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python -t
+
+# bfs
+
+
+from collections import deque
+
+class Solution:
+    """
+    @param nums: A set of numbers
+    @return: A list of lists
+    """
+    def subsets(self, nums):
+        # write your code here
+        if not nums:
+            return [[]]
+            
+        nums.sort()
+        ret = []
+            
+        q = deque([[]])
+        
+        while len(q) > 0:
+            path = q.popleft()
+            ret.append(path)
+            
+            for i in range(len(nums)):
+                if not path or path[-1] < nums[i]:
+                    new_path = list(path)
+                    new_path.append(nums[i])
+                    q.append(new_path)
+                    
+        return ret
+        
+        
+        
+
+# dfs
+
+class Solution:
+    """
+    @param nums: A set of numbers
+    @return: A list of lists
+    """
+    def subsets(self, nums):
+        # write your code here
+        nums.sort()
+        
+        ret = []
+        
+        self.dfs(nums, 0, [], ret)
+        
+        return ret
+        
+    def dfs(self, nums, start, path, ret):
+        ret.append(list(path))
+
+        
+        for i in range(start, len(nums)):
+            path.append(nums[i])
+            self.dfs(nums, i+1, path, ret)
+            path.pop()
+            
+        return
+    
+
+
+class Solution:
+    """
+    @param nums: A set of numbers
+    @return: A list of lists
+    """
+    def subsets(self, nums):
+        # write your code here
+        nums.sort()
+        
+        ret = []
+        
+        self.dfs(nums, 0, [], ret)
+        
+        return ret
+        
+    def dfs(self, nums, start, path, ret):
+        #exit point
+        if start == len(nums):
+            ret.append(list(path))
+            return
+
+        # add nums[start]
+        path.append(nums[start])
+        self.dfs(nums, start+1, path, ret)
+        
+        # don't add nums[start]
+        path.pop()
+        self.dfs(nums, start+1, path, ret)
+            
+        return
+    
