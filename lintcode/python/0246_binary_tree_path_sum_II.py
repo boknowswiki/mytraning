006c7b0345b86062ec882c5e0f8d1080d40fee82
boknowswiki <boknowswiki@gmail.com>
2019-11-12 09:03:24 -0800

    0246 more

--- lintcode/python/0246_binary_tree_path_sum_II.py
+++ lintcode/python/0246_binary_tree_path_sum_II.py
@@ -48,3 +48,49 @@ class Solution:
         return
     
 
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+
+class Solution:
+    """
+    @param: root: the root of binary tree
+    @param: target: An integer
+    @return: all valid paths
+    """
+    def __init__(self):
+        
+        self.results = []
+    
+    def binaryTreePathSum2(self, root, target):
+        # write your code here
+        if not root:
+            return []
+            
+        self.traverse(root, target, [])
+        self.binaryTreePathSum2(root.left, target)
+        self.binaryTreePathSum2(root.right, target)
+        
+        return self.results
+                
+    def traverse(self, root, target, temp):
+        
+        if not root:
+            return
+        
+        temp.append(root.val)
+        
+        if root.val == target:
+            self.results.append(temp[:])
+        if root.left:
+            self.traverse(root.left, target - root.val, temp)
+            temp.pop()
+        
+        if root.right:
+            self.traverse(root.right, target - root.val, temp)
+            temp.pop()
