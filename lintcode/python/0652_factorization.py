109aa48fae27f57f038410c6c222a7cc0cea6a28
boknowswiki <boknowswiki@gmail.com>
2019-11-20 09:06:49 -0800

    0652 dfs

---
+++ lintcode/python/0652_factorization.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python -t
+
+# dfs
+
+import math
+
+class Solution:
+    """
+    @param n: An integer
+    @return: a list of combination
+    """
+    def getFactors(self, n):
+        # write your code here
+        ret = []
+        
+        self.dfs(2, n, [], ret)
+        
+        return ret
+        
+    def dfs(self, start, remain, path, ret):
+        if remain == 1 and len(path) != 1:
+            ret.append(list(path))
+            return
+        
+        for i in range(start, int(math.sqrt(remain))+1):
+            if remain % i == 0:
+                path.append(i)
+                self.dfs(i, remain/i, path, ret)
+                path.pop()
+        
+        if remain >= start:        
+            path.append(remain)
+            self.dfs(remain, 1, path, ret)
+            path.pop()
+            
+        return
+    
+    
