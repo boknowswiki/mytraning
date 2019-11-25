48e7009e9147e366824f54ab6f519a6144d80fa6
boknowswiki <boknowswiki@gmail.com>
2019-11-25 13:20:58 -0800

    1015 add dfs

--- lintcode/python/1015_find_eventual_safe_states.py
+++ lintcode/python/1015_find_eventual_safe_states.py
@@ -41,3 +41,39 @@ class Solution:
         return sorted(res)
         
         
+
+# dfs
+
+class Solution:
+    """
+    @param graph: a 2D integers array
+    @return: return a list of integers
+    """
+    def eventualSafeNodes(self, graph):
+        # write your code here
+        ret = set()
+        
+        for i in range(len(graph)):
+            v = set([i])
+            self.dfs(graph, i, v, ret)
+            
+        return sorted(list(ret))
+        
+    def dfs(self, graph, index, v, ret):
+        for i in graph[index]:
+            if i in v:
+                return False
+            if i in ret:
+                continue
+            
+            v.add(i)
+            if not self.dfs(graph, i, v, ret):
+                return False
+            v.remove(i)
+            
+        ret.add(index)
+        
+        return True
+        
+            
+        
