8e0c8ee177725d9776d667384e0d7be3bbb47a0a
boknowswiki <boknowswiki@gmail.com>
2019-11-26 08:13:25 -0800

    1031 add dfs

--- lintcode/python/1031_is_graph_bipartite.py
+++ lintcode/python/1031_is_graph_bipartite.py
@@ -42,3 +42,39 @@ class Solution:
                         
         return True
     
+
+# dfs
+
+class Solution:
+    """
+    @param graph: the given undirected graph
+    @return:  return true if and only if it is bipartite
+    """
+    def isBipartite(self, graph):
+        # Write your code here
+        if not graph:
+            return False
+            
+        n = len(graph)
+        
+        colors = [0] * n
+        
+        for i in range(n):
+            if colors[i] == 0 and not self.dfs(graph, i, colors, 1):
+                return False
+                
+        return True
+        
+    def dfs(self, graph, index, colors, color):
+        if colors[index]:
+            return colors[index] == color
+            
+        colors[index] = color
+        
+        for c in graph[index]:
+            if not self.dfs(graph, c, colors, -color):
+                return False
+                
+        return True
+        
+        
