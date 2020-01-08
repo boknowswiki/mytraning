e6d3a11da162198d896f80c539f4e9bbc41a9b3a
Bo Tao <taobo0626@gmail.com>
2020-01-07 22:34:14 -0800

    0058 two pointers

---
+++ lintcode/python/0058_4sum.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python -t
+
+# two pointers
+
+class Solution:
+    """
+    @param numbers: Give an array
+    @param target: An integer
+    @return: Find all unique quadruplets in the array which gives the sum of zero
+    """
+    def fourSum(self, numbers, target):
+        # write your code here
+        nums = numbers
+        nums.sort()
+        res = []
+        length = len(nums)
+        for i in range(0, length - 3):
+            if i and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, length - 2):
+                if j != i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                sum = target - nums[i] - nums[j]
+                left, right = j + 1, length - 1
+                while left < right:
+                    if nums[left] + nums[right] == sum:
+                        res.append([nums[i], nums[j], nums[left], nums[right]])
+                        right -= 1
+                        left += 1
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+                    elif nums[left] + nums[right] > sum:
+                        right -= 1
+                    else:
+                        left += 1
+        return res
+        
+        
