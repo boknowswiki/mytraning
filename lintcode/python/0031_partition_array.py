be4aa9e357d469a24727c3205062414e1f7764af
boknowswiki <boknowswiki@gmail.com>
2020-01-09 08:31:27 -0800

    0031 two pointers better

--- lintcode/python/0031_partition_array.py
+++ lintcode/python/0031_partition_array.py
@@ -11,6 +11,32 @@ class Solution:
     def partitionArray(self, nums, k):
         # write your code here
         n = len(nums)
+        l = 0
+        r = n-1
+        
+        while l <= r:
+            while l <= r and nums[l] < k:
+                l += 1
+            while l <= r and nums[r] >= k:
+                r -= 1
+            
+            if l <= r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+            
+        return l
+        
+
+class Solution:
+    """
+    @param nums: The integer array you should partition
+    @param k: An integer
+    @return: The index after partition
+    """
+    def partitionArray(self, nums, k):
+        # write your code here
+        n = len(nums)
         if n == 0:
             return 0
             
