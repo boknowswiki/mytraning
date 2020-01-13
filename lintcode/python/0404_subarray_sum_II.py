c77ba042c0cb96afe4b16876a6114b62600bc9e9
Bo Tao <taobo0626@gmail.com>
2020-01-12 22:50:20 -0800

    0404 add two pointers solution

--- lintcode/python/0404_subarray_sum_II.py
+++ lintcode/python/0404_subarray_sum_II.py
@@ -1,6 +1,42 @@
 #!/usr/bin/python -t
 
+# two pointers
+# time O(n), space O(n)
+
+class Solution:
+    """
+    @param A: An integer array
+    @param start: An integer
+    @param end: An integer
+    @return: the number of possible answer
+    """
+    def subarraySumII(self, A, start, end):
+        # write your code here
+        n = len(A)
+        pre_sum = [0] * (n+1)
+        ret = 0
+        
+        
+        for i in range(1, n+1):
+            pre_sum[i] = pre_sum[i-1] + A[i-1]
+        
+        l = 0
+        r = 0
+            
+        for i in range(1, n+1):
+            while l < i and pre_sum[i] - pre_sum[l] > end:
+                l += 1
+                
+            while r < i and pre_sum[i] - pre_sum[r] >= start:
+                r += 1
+                
+            ret += r-l
+            
+        return ret
+        
+
 # binary search solution
+# time O(nlogn) space O(n)
 
 class Solution:
     """
