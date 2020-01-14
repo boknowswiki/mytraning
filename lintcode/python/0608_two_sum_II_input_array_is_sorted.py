35b362987c755e961c976e9d519d94d47cf7e97d
boknowswiki <boknowswiki@gmail.com>
2020-01-14 12:34:30 -0800

    add better

--- lintcode/python/0608_two_sum_II_input_array_is_sorted.py
+++ lintcode/python/0608_two_sum_II_input_array_is_sorted.py
@@ -1,6 +1,32 @@
 #!/usr/bin/python -t
 
-# two pointers, binary search
+# two pointers
+# time O(n)
+
+class Solution:
+    """
+    @param nums: an array of Integer
+    @param target: target = nums[index1] + nums[index2]
+    @return: [index1 + 1, index2 + 1] (index1 < index2)
+    """
+    def twoSum(self, nums, target):
+        # write your code here
+        n = len(nums)
+        l = 0
+
+        for r in range(n-1, -1, -1):
+            while l < r and nums[l] + nums[r] < target:
+                l += 1
+                
+            if l < r and nums[l] + nums[r] == target:
+                return [l+1, r+1]
+                
+        return [-1, -1]
+        
+        
+
+# binary search
+# time O(nlogn)
 
 class Solution:
     """
