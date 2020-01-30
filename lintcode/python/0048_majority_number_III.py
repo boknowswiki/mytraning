9abacaa1c41b127940cc70acff4a3788a67f5d86
boknowswiki <boknowswiki@gmail.com>
2020-01-30 08:56:06 -0800

    0048 better

--- lintcode/python/0048_majority_number_III.py
+++ lintcode/python/0048_majority_number_III.py
@@ -1,5 +1,42 @@
 #!/usr/bin/python -t
 
+# hash table time O(n) space O(k)
+
+class Solution:
+    """
+    @param nums: A list of integers
+    @param k: An integer
+    @return: The majority number
+    """
+    def majorityNumber(self, nums, k):
+        # write your code here
+        d = dict()
+        n = len(nums)
+        
+        for num in nums:
+            if num in d:
+                d[num] += 1
+            elif len(d) < k-1:
+                d[num] = 1
+            else:
+                keys = d.keys()
+                for key in keys:
+                    d[key] -= 1
+                    if d[key] == 0:
+                        del d[key]
+  
+        for key in d:
+            d[key] = 0
+            
+        for num in nums:
+            if num in d:
+                d[num] += 1
+                if d[num] > n/k:
+                    return num
+                    
+        return -1
+        
+
 # linked list tag, but haspmap
 
 class Solution:
