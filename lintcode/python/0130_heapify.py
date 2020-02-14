71f0698f6e6fc9dce5337d54c1ebf43f436c40b8
Bo Tao <taobo0626@gmail.com>
2020-02-13 21:25:28 -0800

    0130 one more sift down

--- lintcode/python/0130_heapify.py
+++ lintcode/python/0130_heapify.py
@@ -30,3 +30,38 @@ class Solution:
             
         return
     
+
+# sift down
+
+class Solution:
+    """
+    @param: A: Given an integer array
+    @return: nothing
+    """
+    def heapify(self, A):
+        # write your code here
+        n = len(A)
+        for i in range((n-1-1)/2, -1, -1):
+            self.siftdown(A, n, i)
+            
+        return
+    
+    def siftdown(self, A, end, start):
+        while start < end:
+            left = 2*start+1
+            right = left + 1
+            
+            min_index = start
+            
+            if left < end and A[left] < A[min_index]:
+                min_index = left
+            if right < end and A[right] < A[min_index]:
+                min_index = right
+                
+            if min_index == start:
+                break
+            A[min_index], A[start] = A[start], A[min_index]
+            start = min_index
+            
+        return
+
