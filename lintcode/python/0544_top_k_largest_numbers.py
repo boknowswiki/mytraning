d9aab2e260a044f99bbbfc2046cb68ea321d7c9d
Bo Tao <taobo0626@gmail.com>
2020-02-15 22:43:44 -0800

    0544 heap

---
+++ lintcode/python/0544_top_k_largest_numbers.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python -t
+
+# heap
+
+
+import heapq
+
+class Solution:
+    """
+    @param nums: an integer array
+    @param k: An integer
+    @return: the top k largest numbers in array
+    """
+    def topk(self, nums, k):
+        # write your code here
+        heapq.heapify(nums)
+        #print nums
+        topk = heapq.nlargest(k, nums)
+        topk.sort()
+        topk.reverse()
+        return topk
+        
