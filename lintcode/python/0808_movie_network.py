06a83204251b26f22dc5bbca8584f24f099fb49b
boknowswiki@gmail.com <taobo0626@gmail.com>
2020-02-19 21:17:52 -0800

    0808 bfs and heap AC

--- lintcode/python/0808_movie_network.py
+++ lintcode/python/0808_movie_network.py
@@ -1,5 +1,57 @@
 #!/usr/bin/python -t
 
+# bfs and heap
+# AC
+
+
+import heapq, collections
+
+class Solution:
+    """
+    @param rating: the rating of the movies
+    @param G: the realtionship of movies
+    @param S: the begin movie
+    @param K: top K rating 
+    @return: the top k largest rating moive which contact with S
+    """
+    def topKMovie(self, rating, G, S, K):
+        # Write your code here
+        related_movies = self.bfs(G, S)
+        #print related_movies
+        
+        heap = []
+        ret = []
+        
+        for m in related_movies:
+            if len(heap) >= K:
+                heapq.heappushpop(heap, (rating[m], m))
+            else:
+                heapq.heappush(heap, (rating[m], m))
+        
+        #print heap
+        
+        ret = sorted([m for _, m in heap])
+        
+        return ret
+        
+        
+    def bfs(self, g, s):
+        q = collections.deque([s])
+        v = set([s])
+        ret = []
+        
+        while len(q) != 0:
+            movie = q.popleft()
+            ret.append(movie)
+            for m in g[movie]:
+                if m in v:
+                    continue
+                q.append(m)
+                v.add(m)
+                
+        return ret[1:]
+            
+
 # BFS with heap
 
 
