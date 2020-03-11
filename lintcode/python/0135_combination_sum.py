7d37b7493efe1e29d95a9b3c622d23c7a0dd0562
boknowswiki <boknowswiki@gmail.com>
2020-03-11 08:33:59 -0700

    0135 better

--- lintcode/python/0135_combination_sum.py
+++ lintcode/python/0135_combination_sum.py
@@ -1,5 +1,42 @@
 #!/usr/bin/python -t
 
+# dfs, better
+# 时间复杂度
+# 答案个数不知道, 假设为S;
+# 每个答案用的时间=target;
+# 所以总的时间复杂度= O(S*target), 是个NP问题
+
+class Solution:
+    """
+    @param candidates: A list of integers
+    @param target: An integer
+    @return: A list of lists of integers
+    """
+    def combinationSum(self, candidates, target):
+        # write your code here
+        s = sorted(set(candidates))
+        
+        ret = []
+        
+        self.dfs(s, target, 0, [], ret)
+        
+        return ret
+        
+    def dfs(self, s, target, start, path, ret):
+        if target == 0:
+            ret.append(list(path))
+            return
+        
+        for i in range(start, len(s)):
+            if target < s[i]:
+                return
+            path.append(s[i])
+            self.dfs(s, target-s[i], i, path, ret)
+            path.pop()
+            
+        return
+    
+
 # DFS, better
 
 class Solution:
