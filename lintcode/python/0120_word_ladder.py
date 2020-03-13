73f5e8e3b05f9e891781aec2b1833e9aa7207ea5
boknowswiki@gmail.com <taobo0626@gmail.com>
2020-03-12 21:31:50 -0700

    myself solution, but TLE

--- lintcode/python/0120_word_ladder.py
+++ lintcode/python/0120_word_ladder.py
@@ -56,3 +56,55 @@ class Solution:
         return words
         
         
+
+# bfs, hit TLE, and got AC after replace line 92-95 to line 37-42
+
+
+import collections
+
+class Solution:
+    """
+    @param: start: a string
+    @param: end: a string
+    @param: dict: a set of string
+    @return: An integer
+    """
+    def ladderLength(self, start, end, dict):
+        # write your code here
+        if self.isOneoff(start, end):
+            return 2
+            
+        q = collections.deque([start])
+        cnt = 1
+        v = set()
+        v.add(start)
+        
+        while len(q) != 0:
+            cnt += 1
+            l = len(q)
+            
+            for _ in range(l):
+                cur = q.popleft()
+            
+                if self.isOneoff(cur, end):
+                    return cnt
+                
+                for w in dict:
+                    if self.isOneoff(cur, w) and w not in v:
+                        v.add(w)
+                        q.append(w)
+                    
+        return 0
+            
+            
+            
+    def isOneoff(self, s1, s2):
+        cnt = 0
+        
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                cnt += 1
+                if cnt > 1:
+                    return False
+                    
+        return True
