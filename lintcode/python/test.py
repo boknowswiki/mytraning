a77081e3401b2d53309192b228a90150154dfafe
boknowswiki@gmail.com <taobo0626@gmail.com>
2020-04-12 20:16:46 -0700

    quick sort myself

--- lintcode/python/test.py
+++ lintcode/python/test.py
@@ -1,41 +1,38 @@
 #!/usr/bin/python -t
 
 class sort():
-    def mergesort(self, l):
-        if len(l) > 1:
-            mid = len(l)/2
-
-            left = l[:mid]
-            right = l[mid:]
-
-            self.mergesort(left)
-            self.mergesort(right)
-
-            i = j = k = 0
-            while i < len(left) and j < len(right):
-                if left[i] < right[j]:
-                    l[k] = left[i]
-                    i += 1 
-                else:
-                    l[k] = right[j]
-                    j += 1
-                k += 1
-
-            if i < len(left):
-                l[k] = left[i]
-                k += 1
-                i += 1
-            if j < len(right):
-                l[k] = right[j]
-                k += 1
-                j += 1
+    def quicksort(self, l):
+        self.helper(l, 0, len(l)-1)
 
         return
+
+    def helper(self, l, start, end):
+        if start < end:
+            part = self.partition(l, start, end)
+            self.helper(l, start, part-1)
+            self.helper(l, part+1, end)
+
+        return
+
+    def partition(self, l, start, end):
+        part = l[end]
+        i = start
+
+        while start < end:
+            if l[start] <= part:
+                l[i], l[start] = l[start], l[i]
+                i += 1
+
+            start += 1
+
+        l[i], l[end] = l[end], l[i]
+
+        return i
 
 
 if __name__ == "__main__":
     l = [3, 2, 2, 1, 4]
     print l
     s = sort()
-    s.mergesort(l)
+    s.quicksort(l)
     print l
