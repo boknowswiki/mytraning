052698e759b02735873bdf89d84f56190c62a8d3
Bo Tao <botao@Bos-MacBook-Pro.local>
2020-05-22 22:03:01 -0700

    0130 heapify add shift up.

--- lintcode/python/0130_heapify.py
+++ lintcode/python/0130_heapify.py
@@ -2,36 +2,9 @@
 
 # heap
 
-class Solution:
-    """
-    @param: A: Given an integer array
-    @return: nothing
-    """
-    def heapify(self, A):
-        # write your code here
-        n = len(A)
-        
-        for i in range(n, -1, -1):
-            self.helper(A, n, i)
-            
-    def helper(self, A, end, start):
-        min_val = start
-        left = 2*start+1
-        right = 2*start+2
-        
-        if left < end and A[left] < A[start]:
-            min_val = left
-        if right < end and A[right] < A[min_val]:
-            min_val = right
-            
-        if min_val != start:
-            A[start], A[min_val] = A[min_val], A[start]
-            self.helper(A, end, min_val)
-            
-        return
-    
-
 # sift down
+
+# time O(n)
 
 class Solution:
     """
@@ -65,3 +38,34 @@ class Solution:
             
         return
 
+# shift up
+# time O(nlogn)
+
+class Solution:
+    """
+    @param: A: Given an integer array
+    @return: nothing
+    """
+    def heapify(self, A):
+        # write your code here
+        n = len(A)
+        if n < 2:
+            return A
+            
+        for i in range(0, n):
+            self.shiftup(A, i)
+        
+        return A
+        
+    def shiftup(self, A, i):
+        while True:
+            p = (i-1)/2
+            if p >= 0:
+                if A[i] > A[p]:
+                    break
+                A[i], A[p] = A[p], A[i]
+                self.shiftup(A, p)
+            else:
+                break
+                
+        return
