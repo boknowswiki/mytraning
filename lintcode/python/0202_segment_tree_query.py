a838c8ad270cf3c2a6b0b6f4cba64aad20dd10b4
boknowswiki@gmail.com <taobo0626@gmail.com>
2020-06-14 14:23:39 -0700

    better solution 0202 segment tree query

--- lintcode/python/0202_segment_tree_query.py
+++ lintcode/python/0202_segment_tree_query.py
@@ -2,6 +2,52 @@
 
 # segment tree query
 
+# beats 100%
+
+"""
+Definition of SegmentTreeNode:
+class SegmentTreeNode:
+    def __init__(self, start, end, max):
+        self.start, self.end, self.max = start, end, max
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: The root of segment tree.
+    @param start: start value.
+    @param end: end value.
+    @return: The maximum number in the interval [start, end]
+    """
+    def query(self, root, start, end):
+        # write your code here
+        if root.start == start and root.end == end:
+            return root.max
+            
+        mid = (root.start+root.end)/2
+        left, right = -sys.maxint, -sys.maxint
+        #print root.start, root.end, mid
+        #print start, end
+        # left side
+        if start <= mid:
+            if mid < end:
+                left = self.query(root.left, start, mid)
+            else:
+                left = self.query(root.left, start, end)
+                
+        # right side
+        if mid < end:
+            #print "right side ", start, end
+            if start <= mid:
+                right = self.query(root.right, mid+1, end)
+            else:
+                right = self.query(root.right, start, end)
+                
+        # or not within root start and end area.
+        
+        return max(left, right)
+                
+
 """
 Definition of SegmentTreeNode:
 class SegmentTreeNode:
