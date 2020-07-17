577e1d9e0b0f67f8feda328afb0c985331ebcd59
boknowswiki@gmail.com <taobo0626@gmail.com>
2020-07-16 22:02:56 -0700

    0066 both recursive and non-recursive

--- lintcode/golang/0066_binary_tree_preorder_traversal.go
+++ lintcode/golang/0066_binary_tree_preorder_traversal.go
@@ -1,3 +1,45 @@
+// non-recursive
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+/**
+ * @param root: A Tree
+ * @return: Preorder in ArrayList which contains node values.
+ */
+func preorderTraversal (root *TreeNode) []int {
+    // write your code here
+    ret := make([]int, 0)
+    stack := make([]*TreeNode, 0)
+    
+    if root != nil {
+        stack = append(stack, root)
+    }
+    
+    for len(stack) != 0 {
+        node := stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+        
+        ret = append(ret, node.Val)
+        
+        if node.Right != nil {
+            stack = append(stack, node.Right)
+        }
+        if node.Left != nil {
+            stack = append(stack, node.Left)
+        }
+    }
+    
+    return ret
+}
+
+// recursive
 
 /**
  * Definition for a binary tree node.
