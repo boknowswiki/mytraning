38b4c62a55b84113aef989ae222ff9c842c18620
boknowswiki@gmail.com <taobo0626@gmail.com>
2020-09-05 13:30:55 -0700

    0242 done

--- lintcode/python/0242_convert_binary_tree_to_linked_lists_by_depth.py
+++ lintcode/python/0242_convert_binary_tree_to_linked_lists_by_depth.py
@@ -45,4 +45,48 @@ class Solution:
             
         return ret
         
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        this.val = val
+        this.left, this.right = None, None
+Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+"""
+
+class Solution:
+    # @param {TreeNode} root the root of binary tree
+    # @return {ListNode[]} a lists of linked list
+    def binaryTreeToLists(self, root):
+        # Write your code here
+        if root == None:
+            return []
+            
+        q = [root]
+        ret = []
+        
+        while len(q) != 0:
+            head = None
+            level = []
+            #print q
+            for node in q:
+                if head == None:
+                    cur = ListNode(node.val)
+                    head = cur
+                else:
+                    cur.next = ListNode(node.val)
+                    cur = cur.next
+                if node.left != None:
+                    level.append(node.left)
+                if node.right != None:
+                    level.append(node.right)
+                    
+            ret.append(head)
+            q = level
+            
+        return ret
             
