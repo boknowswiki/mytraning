573daf3cc18abf75b358402ccaab6d2e8b2dbdd9
boknowswiki@gmail.com <taobo0626@gmail.com>
2020-09-05 13:43:05 -0700

    0375 done

--- lintcode/golang/0375_clone_binary_tree.go
+++ lintcode/golang/0375_clone_binary_tree.go
@@ -1,3 +1,73 @@
+
+
+type TreeQueue struct {
+    data []*TreeNode
+}
+
+func (s *TreeQueue) Empty() bool {
+	return len(s.data) == 0
+}
+
+func (s *TreeQueue) Pop() {
+	if len(s.data) == 0 {
+		return
+	}
+	s.data = s.data[1:]
+}
+
+func (s *TreeQueue) Top() *TreeNode {
+	if len(s.data) == 0 {
+		return nil
+	}
+	return s.data[0]
+}
+
+func (s *TreeQueue) Push(node *TreeNode) {
+	s.data = append(s.data, node)
+}
+
+/**
+ * @param root: The root of binary tree
+ * @return: root of new tree
+ */
+func cloneTree(root *TreeNode) *TreeNode {
+	var rs *TreeNode
+	queue1 := &TreeQueue{
+		data: []*TreeNode{},
+	}
+	queue2 := &TreeQueue{
+		data: []*TreeNode{},
+	}
+	if root == nil {
+		return rs
+	}
+	rs = &TreeNode{}
+	queue1.Push(root)
+	queue2.Push(rs)
+	for !queue1.Empty() {
+		node := queue1.Top()
+		cpNode := queue2.Top()
+		cpNode.Val = node.Val
+
+		queue1.Pop()
+		queue2.Pop()
+		if node.Left != nil {
+			queue1.Push(node.Left)
+			cpNode.Left = &TreeNode{}
+			queue2.Push(cpNode.Left)
+
+		}
+		if node.Right != nil {
+			queue1.Push(node.Right)
+			cpNode.Right = &TreeNode{}
+			queue2.Push(cpNode.Right)
+		}
+	}
+
+	return rs
+}
+
+
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
