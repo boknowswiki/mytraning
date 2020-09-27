785648bf9ce4c9f8f438e3546df2a6b10f949126
boknowswiki@gmail.com <taobo0626@gmail.com>
2020-09-27 14:19:11 -0700

    0691 done

---
+++ lintcode/golang/0691_recover_binary_search_tree.go
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+/**
+ * @param root: the given tree
+ * @return: the tree after swapping
+ */
+ 
+const MaxInt = int(^uint(0)>>1)
+const MinInt = -MaxInt-1
+
+var pre *TreeNode
+var first, second *TreeNode
+
+func bstSwappedNode (root *TreeNode) *TreeNode {
+    // write your code here
+    if root == nil {
+        return nil
+    }
+    
+    pre = &TreeNode{Val: MinInt}
+    
+    dfs(root)
+    
+    if first != nil  && second != nil {
+        tmp := first.Val
+        first.Val = second.Val
+        second.Val = tmp
+    }
+    
+    return root
+}
+
+func dfs(node *TreeNode) {
+    if node == nil {
+        return
+    }
+    
+    dfs(node.Left)
+    
+    if pre.Val > node.Val {
+        if first == nil {
+            first = pre
+        }
+        second = pre
+    }
+    
+    dfs(node.Right)
+    
+    return
+}
