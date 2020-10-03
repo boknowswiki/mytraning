fbff0a0a18019454c72e55b115164f6e3e2ebc76
boknowswiki@gmail.com <taobo0626@gmail.com>
2020-10-03 14:44:54 -0700

    1101 done

--- lintcode/golang/1101_maximum_width_of_binary_tree.go
+++ lintcode/golang/1101_maximum_width_of_binary_tree.go
@@ -1,3 +1,67 @@
+
+//bfs
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+/**
+ * @param root: the root
+ * @return: the maximum width of the given tree
+ */
+
+type record struct {
+    node *TreeNode
+    depth int
+    index int
+} 
+
+func widthOfBinaryTree (root *TreeNode) int {
+    // Write your code here
+    if root == nil {
+        return 0
+    }
+    
+    q := make([]record, 0)
+    q = append(q, record{node: root})
+    var ret, curDepth, left int
+    
+    for len(q) != 0 {
+        level := make([]record, 0)
+        
+        for _, r := range q {
+            if r.node != nil {
+                level = append(level, record{node: r.node.Left, depth: r.depth+1, index: r.index*2})
+                level = append(level, record{node: r.node.Right, depth: r.depth+1, index: r.index*2+1})
+                if curDepth != r.depth {
+                    curDepth = r.depth
+                    left = r.index
+                }
+                
+                ret = max(ret, r.index-left+1)
+            }
+        }
+        q = level
+    }
+    
+    return ret
+}
+
+func max (a, b int ) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+
+
+//dfs
+
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
