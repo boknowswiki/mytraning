d980a1493584fdc5b8e167d2717532b68a612f4e
boknowswiki@gmail.com <taobo0626@gmail.com>
2020-10-05 21:17:28 -0700

    move 0915 and 1108 to python dir.

--- lintcode/python/test.py
+++ lintcode/python/test.py
@@ -1,88 +1,54 @@
 #!/usr/bin/python -t
 
-class Node:
-    def __init__(self, key, val, next=None):
-        self.key = key
-        self.val = val
-        self.next = next
 
-class LRUCache:
-    """
-    @param: capacity: An integer
-    """
-    def __init__(self, capacity):
-        # do intialization if necessary
-        self.cap = capacity
-        self.key_to_prev = {}
-        self.head = Node(0, 0)
-        self.tail = self.head
-        
 
+import heapq
+
+class Solution:
     """
-    @param: key: An integer
-    @return: An integer
+    @param nums: a list of integers
+    @param k: a integer
+    @return: return a integer
     """
-    def get(self, key):
+    def smallestDistancePair(self, nums, k):
         # write your code here
-        if key not in self.key_to_prev:
-            return -1
-        else:
-            self.move_to_tail(self.key_to_prev[key])
+        n = len(nums)
+        if n == 0:
+            return 0
             
-        return self.key_to_prev[key].next.val
-
-    """
-    @param: key: An integer
-    @param: value: An integer
-    @return: nothing
-    """
-    def set(self, key, value):
-        # write your code here
-        if key in self.key_to_prev:
-            self.move_to_tail(self.key_to_prev[key])
-            self.key_to_prev[key].next.val = value
-        else:
-            self.add_node(key, value)
-            if len(self.key_to_prev) > self.cap:
-                self.remove_head()
-        return
-    
-    def move_to_tail(self, prev):
-        node = prev.next
+        hq = []
+        nums.sort()
+        print nums
         
-        if node != self.tail:
-            self.key_to_prev[node.next.val] = prev
-            prev.next = node.next
-            node.next = None
-        
-            self.tail.next = node
-            self.key_to_prev[node.val] = self.tail
-            self.tail = node
-        
-        return
-    
-    def add_node(self, key, val):
-        self.key_to_prev[key] = self.tail
-        node = Node(key, val)
-        self.tail.next = node
-        self.tail = node
-        
-        return
-    
-    def remove_head(self):
-        del self.key_to_prev[self.head.next.key]
-        
-        self.key_to_prev[self.head.next.next.key] = self.head
-        self.head.next = self.head.next.next
-        
-        return
-
+        for i in range(1, n):
+            print i, hq
+            if len(hq) >= k:
+                break
+            for j in range(n-i):
+                for l in range(j+1, j+1+i):
+                    
+                    diff = nums[l]-nums[j]
+                    print j, l, diff
+                    if diff < 0:
+                        print "error diff", diff
+                    heapq.heappush(hq, -diff)
+                    print hq
+                    if len(hq) == k+1:
+                        heapq.heappop(hq)
+              
+        print "outside"
+        print len(hq)
+        #heapq.heappop(hq)
+        while len(hq) > k:
+            print "in loop"
+            val =heapq.heappop(hq)
+            print val
+        print hq
+        return hq[0]
 
 if __name__ == "__main__":
-    l = LRUCache(2)
-    l.set(2,1)
-    l.set(1,1)
-    print l.get(2)
-    l.set(4,1)
-    print l.get(1)
-    print l.get(2)
+    nums = [9,10,7,10,6,1,5,4,9,8]
+    k = 18
+    s = Solution()
+    r = s.smallestDistancePair(nums, k)
+    print r
