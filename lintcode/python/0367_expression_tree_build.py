5278b988b2ecac281366631ff9273eda59b5a17d
boknowswiki@gmail.com <taobo0626@gmail.com>
2020-10-06 21:55:19 -0700

    0367 add more comments

--- lintcode/python/0367_expression_tree_build.py
+++ lintcode/python/0367_expression_tree_build.py
@@ -4,6 +4,10 @@
 # https://www.lintcode.com/problem/convert-expression-to-reverse-polish-notation/solution?_from=ladder&&fromId=106
 # 根据运算符的优先级来构造单调递增栈，最后弹出栈中元素，最后一个就是根节点，返回即可。
 
+# from jiuzhang
+# 解法跟 Lintcode 370 差不多，就是把priority較高的運算符pop出來進行Calculate
+# 一律把數字放進 Stack opd 備用，等到下一次從 Stack opt 彈出運算符時進行Calculate.
+# Calculate 就是把 Stack opd 的上面兩個元素拿出來跟 operator 結合型成一個新的ExpressionTreeNode operand 再放回 Stack opd備用.
 """
 Definition of ExpressionTreeNode:
 class ExpressionTreeNode:
