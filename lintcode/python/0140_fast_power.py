d2fbf23e902c86005ae3e8114bcaa54a92964c83
boknowswiki@gmail.com <taobo0626@gmail.com>
2020-10-08 20:48:50 -0700

    0140 done

---
+++ lintcode/python/0140_fast_power.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python -t
+
+class Solution:
+    """
+    @param a: A 32bit integer
+    @param b: A 32bit integer
+    @param n: A 32bit integer
+    @return: An integer
+    """
+    def fastPower(self, a, b, n):
+        # write your code here
+        ans = 1
+        while n > 0:
+            if n % 2 == 1:
+                ans = (ans * a) % b
+            a = a * a % b
+            n = n // 2
+        return ans % b
