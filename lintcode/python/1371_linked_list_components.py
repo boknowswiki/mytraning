83bea0e177f5e7ce9402d40dc6108454e0a53bd3
boknowswiki@gmail.com <taobo0626@gmail.com>
2020-11-19 10:20:44 -0800

    1371 done

--- lintcode/python/1371_linked_list_components.py
+++ lintcode/python/1371_linked_list_components.py
@@ -33,3 +33,58 @@ class Solution:
         return ret
         
         
+# union find solution
+
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class uf:
+    def __init__(self, s):
+        self.f = {i:i for i in s}
+        self.cnt = len(s)
+        
+    def find(self, x):
+        if x not in self.f:
+            return -1
+            
+        if x != self.f[x]:
+            self.f[x] = self.find(self.f[x])
+    
+        return self.f[x]
+    
+    def union(self, x, y):
+        rx = self.find(x)
+        ry = self.find(y)
+        
+        if rx == -1 or ry == -1:
+            return
+        
+        if rx == ry:
+            return
+        
+        self.f[rx] = ry
+        self.cnt -= 1
+        
+        return
+
+class Solution:
+    """
+    @param head: the head
+    @param G: an array
+    @return: the number of connected components in G
+    """
+    def numComponents(self, head, G):
+        # Write your code here
+        s = set(G)
+        myuf = uf(s)
+        
+        while head and head.next:
+            myuf.union(head.val, head.next.val)
+            head = head.next
+            
+        return myuf.cnt
