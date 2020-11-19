12cca189db1a3c7f88aa7254fc3388c6ebe1f8ed
boknowswiki@gmail.com <taobo0626@gmail.com>
2020-11-18 21:20:28 -0800

    0930 done

--- lintcode/python/0930_connected_components_in_list.py
+++ lintcode/python/0930_connected_components_in_list.py
@@ -1,5 +1,74 @@
 #!/usr/bin/python -t
 
+# union find AC
+
+
+"""
+Definition of DoublyListNode
+class ListNode(object):
+    def __init__(self, val, prev=None, next=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+"""
+class myuf:
+    def __init__(self, nodes):
+        self.f = {}
+        self.cnt = 0
+        for i in nodes:
+            #print(i)
+            self.f[i] = i
+            self.cnt += 1
+            
+    def find(self, x):
+        #print(x)
+        if x not in self.f:
+            return -1
+        if x != self.f[x]:
+            self.f[x] = self.find(self.f[x])
+            
+        return self.f[x]
+        
+    def union(self, x, y):
+        rx = self.find(x)
+        ry = self.find(y)
+        if rx == ry:
+            return
+        if rx == -1 or ry == -1:
+            return
+        
+        self.f[rx] = ry
+        self.cnt -= 1
+        
+        return
+
+class Solution:
+    """
+    @param head: the given doubly linked list
+    @param nodes: the given nodes array
+    @return: the number of blocks in the given array
+    """
+    def blockNumber(self, head, nodes):
+        # write your code here
+        #print(nodes)
+        uf = myuf(nodes)
+        cur = head
+        if not head:
+            return 0
+        if not head.next:
+            if head.val in nodes:
+                return 1
+            else:
+                return 0
+                
+        while cur.next:
+            uf.union(cur.val, cur.next.val)
+            cur = cur.next
+            
+        return uf.cnt
+        
+        
+
 # linked list, not AC, union find set
 
 """
