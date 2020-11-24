520a716cc6a06b81ee26a5c1ea2e3f99d37a2d9f
boknowswiki@gmail.com <taobo0626@gmail.com>
2020-11-23 21:15:24 -0800

    1102 done

---
+++ lintcode/python/1102_image_smoother.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python -t
+
+class Solution:
+    """
+    @param M: a 2D integer matrix
+    @return: a 2D integer matrix
+    """
+    def imageSmoother(self, M):
+        # Write your code here
+        R, C = len(M), len(M[0])
+        ans = [[0] * C for _ in M]
+
+        for r in xrange(R):
+            for c in xrange(C):
+                count = 0
+                for nr in (r-1, r, r+1):
+                    for nc in (c-1, c, c+1):
+                        if 0 <= nr < R and 0 <= nc < C:
+                            ans[r][c] += M[nr][nc]
+                            count += 1
+                ans[r][c] /= count
+
+        return ans
