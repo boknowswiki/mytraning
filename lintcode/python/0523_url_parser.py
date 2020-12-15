7f66540d5c500e595fe2d7da77d94e6432b2712d
boknowswiki@gmail.com <taobo0626@gmail.com>
2020-12-15 14:38:57 -0800

    0523 done

---
+++ lintcode/python/0523_url_parser.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python -t
+
+
+import re
+
+class HtmlParser:
+    """
+    @param: content: content source code
+    @return: a list of links
+    """
+    def parseUrls(self, content):
+        # write your code here
+        pattern="(?:href|HREF)\s*=\s*(?:'|\")([^\s]*?)(?:'|\")"
+        urls=re.findall(pattern,content)
+        return [url for url in urls if len(url) and not url.startswith("#")]
