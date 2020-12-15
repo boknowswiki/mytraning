bff24d0df9b9b999bedc7b382b5423bdfd88435e
boknowswiki@gmail.com <taobo0626@gmail.com>
2020-12-15 14:43:57 -0800

    0523 done

--- lintcode/python/0523_url_parser.py
+++ lintcode/python/0523_url_parser.py
@@ -10,6 +10,20 @@ class HtmlParser:
     """
     def parseUrls(self, content):
         # write your code here
+        links = re.findall("[Hh][Rr][Ee][Ff]\s*=\s*['\"](.*?)['\"]", content, re.I)
+        links = [link for link in links if len(link) and not link.startswith('#')]
+        return list(links)
+
+
+import re
+
+class HtmlParser:
+    """
+    @param: content: content source code
+    @return: a list of links
+    """
+    def parseUrls(self, content):
+        # write your code here
         pattern="(?:href|HREF)\s*=\s*(?:'|\")([^\s]*?)(?:'|\")"
         urls=re.findall(pattern,content)
         return [url for url in urls if len(url) and not url.startswith("#")]
