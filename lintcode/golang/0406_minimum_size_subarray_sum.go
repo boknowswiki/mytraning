755445585a618e54d49a6a7cda5b830926f41343
boknowswiki@gmail.com <taobo0626@gmail.com>
2021-01-28 21:57:30 -0800

    0406 again done

--- lintcode/golang/0406_minimum_size_subarray_sum.go
+++ lintcode/golang/0406_minimum_size_subarray_sum.go
@@ -38,3 +38,42 @@ func min(a, b int) int {
     }
     return b
 }
+
+
+/**
+ * @param nums: an array of integers
+ * @param s: An integer
+ * @return: an integer representing the minimum size of subarray
+ */
+func minimumSize (nums []int, s int) int {
+    // write your code here
+    n := len(nums)
+    if n == 0 || (n == 1 && nums[0] < s){
+        return -1
+    }
+    
+    minLen := n+1
+    val := 0
+    l := 0
+    
+    for r := 0; r < n; r++ {
+        val += nums[r]
+        for l <= r && val >= s {
+            minLen = min(minLen, r-l+1)
+            val -= nums[l]
+            l++
+        }
+    }
+    if minLen == n+1 {
+        return -1
+    }
+    
+    return minLen
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
