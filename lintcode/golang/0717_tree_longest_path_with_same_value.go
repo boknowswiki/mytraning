01ac4df6634137d33c21a2702c1053b4e9e315a5
boknowswiki@gmail.com <taobo0626@gmail.com>
2021-02-24 21:48:01 -0800

    0717 done

--- lintcode/golang/0717_tree_longest_path_with_same_value.go
+++ lintcode/golang/0717_tree_longest_path_with_same_value.go
@@ -58,3 +58,56 @@ func max (a, b int) int {
     return b
 }
 
+
+/**
+ * @param A: as indicated in the description
+ * @param E: as indicated in the description
+ * @return: Return the number of edges on the longest path with same value.
+ */
+func LongestPathWithSameValue (A []int, E []int) int {
+    // write your code here
+    tree := make(map[int][]int)
+    ret := 0
+    
+    for i := 0; i < len(E); i += 2 {
+        a, b := E[i], E[i+1]
+        tree[a] = append(tree[a], b)
+        tree[b] = append(tree[b], a)
+    }
+    
+    var dfs func(int, int, map[int][]int, []int) int
+    dfs = func(node int, parent int, graph map[int][]int, a []int) int {
+        if node == 0 {
+            return 0
+        }
+        
+        max_one, max_two := 0, 0
+        
+        for _, child := range graph[node] {
+            if child != parent {
+                val := dfs(child, node, graph, a)
+                if a[node-1] == a[child-1] {
+                    if 1+val > max_one {
+                        max_two = max_one
+                        max_one = 1+val
+                    } else if 1+val > max_two {
+                        max_two = 1+val
+                    }
+                }
+            }
+        }
+        
+        ret = max(ret, max_two+max_one)
+        return max_one
+    }
+    dfs(1, 0, tree, A)
+    
+    return ret
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
