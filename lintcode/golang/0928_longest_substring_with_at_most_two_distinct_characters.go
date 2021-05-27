75208bcc0b1db8d5e943dd484ad140094457e178
boknowswiki@gmail.com <taobo0626@gmail.com>
2021-05-26 22:11:38 -0700

    0928 hash and two pointers done.

--- lintcode/golang/0928_longest_substring_with_at_most_two_distinct_characters.go
+++ lintcode/golang/0928_longest_substring_with_at_most_two_distinct_characters.go
@@ -1,3 +1,48 @@
+
+// hash and two pointers
+
+/**
+ * @param s: a string
+ * @return: the length of the longest substring T that contains at most 2 distinct characters
+ */
+func lengthOfLongestSubstringTwoDistinct (s string) int {
+    // Write your code here
+    n := len(s)
+    if n <= 2 {
+        return n
+    }
+
+    ret := 0
+    l := 0
+    d := make(map[byte]int)
+
+    for r := 0; r < n; r++ {
+        d[s[r]]++
+
+        for len(d) > 2 {
+            d[s[l]]--
+            if d[s[l]] == 0 {
+                delete(d, s[l])
+            }
+            l++
+        }
+
+        if len(d) == 2 {
+            ret = max(ret, r-l+1)
+        }
+    }
+
+    return ret
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+
+
 /**
  * @param s: a string
  * @return: the length of the longest substring T that contains at most 2 distinct characters
