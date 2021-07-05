a714364834c91d3d6af000c81181b9c6fad964c6
boknowswiki@gmail.com <taobo0626@gmail.com>
2021-07-04 22:50:31 -0700

    0919 heap done

--- lintcode/golang/0919_meeting_rooms_II.go
+++ lintcode/golang/0919_meeting_rooms_II.go
@@ -1,3 +1,33 @@
+
+
+// heap
+// nlogn
+
+import (
+	"container/heap"
+	"sort"
+)
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
 /**
  * Definition of Interval:
  * type Interval struct {
@@ -9,6 +39,46 @@
  * @param intervals: an array of meeting time intervals
  * @return: the minimum number of conference rooms required
  */
+func minMeetingRooms(intervals []*Interval) int {
+	// Write your code here
+	if intervals == nil || len(intervals) == 0 {
+		return 0
+	}
+
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i].Start < intervals[j].Start ||
+			(intervals[i].Start == intervals[j].Start && intervals[i].End < intervals[j].End)
+	})
+
+	minHeap := &IntHeap{}
+	heap.Push(minHeap, intervals[0].End)
+
+	for i := 1; i < len(intervals); i++ {
+		if intervals[i].Start < []int(*minHeap)[0] {
+			heap.Push(minHeap, intervals[i].End)
+		} else {
+			heap.Pop(minHeap)
+			heap.Push(minHeap, intervals[i].End)
+		}
+	}
+
+	return minHeap.Len()
+}
+
+/**
+ * Definition of Interval:
+ * type Interval struct {
+ *     Start, End int
+ * }
+ */
+
+/**
+ * @param intervals: an array of meeting time intervals
+ * @return: the minimum number of conference rooms required
+ */
+
+
+// sort with o(n)
 
 import "sort"
 
