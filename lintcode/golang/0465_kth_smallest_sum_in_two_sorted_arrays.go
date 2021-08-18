713ca0963f628b8807adf53543067ca4b89ebd74
boknowswiki@gmail.com <taobo0626@gmail.com>
2021-08-17 21:49:23 -0700

    0465 heap more.

--- lintcode/golang/0465_kth_smallest_sum_in_two_sorted_arrays.go
+++ lintcode/golang/0465_kth_smallest_sum_in_two_sorted_arrays.go
@@ -92,3 +92,107 @@ func main() {
 	k := 3
 	fmt.Println(kthSmallestSum(a, b, k))
 }
+
+
+// other's solution
+
+
+/**
+ * @param A: an integer arrays sorted in ascending order
+ * @param B: an integer arrays sorted in ascending order
+ * @param k: An integer
+ * @return: An integer
+ */
+type Node struct {
+    indexA int
+    indexB int
+    value int
+}
+
+func kthSmallestSum (A []int, B []int, k int) int {
+    // write your code here
+    if len(A) == 0 && len(B) == 0 {
+        return 0
+    }
+    
+    if len(A) == 0 {
+        if len(B) >= k {
+            return B[k - 1]
+        } else {
+            return 0
+        }
+    } else if len(B) == 0 {
+        if len(A) >= k {
+            return A[k - 1]
+        } else {
+            return 0
+        }
+    }
+    
+    ALen := len(A)
+    BLen := len(B)
+    
+    visited := make([][]bool, ALen)
+    for i := range visited {
+        visited[i] = make([]bool, BLen)
+    }
+    
+    heap := make([]*Node, 0)
+    
+    Less := func(A, B *Node) bool {
+        return A.value < B.value
+    }
+    
+    shiftUp := func(heap []*Node, index int) {
+        for {
+            i := (index - 1) / 2
+            if !Less(heap[index], heap[i]) {
+                break
+            }
+            
+            heap[index], heap[i] = heap[i], heap[index]
+            index = i
+        }
+    }
+    
+    shiftDown := func(heap []*Node, index int) {
+        for index * 2 + 1 < len(heap) {
+            son := index * 2 + 1
+            if index * 2 + 2 < len(heap) && !Less(heap[son], heap[index * 2 + 2]) {
+                son = index * 2 + 2
+            }
+            
+            if Less(heap[index], heap[son]) {
+                break
+            }
+            
+            heap[index], heap[son] = heap[son], heap[index]
+            
+            index = son
+        }
+    }
+    
+    dx := []int{0, 1}
+    dy := []int{1, 0}
+    
+    heap = append(heap, &Node{indexA: 0, indexB: 0, value: A[0] + B[0]})
+    visited[0][0] = true
+    
+    for i := 0; i < k - 1; i++ {
+        top := heap[0]
+        heap[0], heap[len(heap) - 1] = heap[len(heap) - 1], heap[0]
+        heap = heap[:len(heap) - 1]
+        shiftDown(heap, 0)
+        for j := 0; j < 2; j++ {
+            nx := top.indexA + dx[j]
+            ny := top.indexB + dy[j]
+            if nx >= 0 && nx < ALen && ny >= 0 && ny < BLen && !visited[nx][ny] {
+                heap = append(heap, &Node{indexA: nx, indexB: ny, value: A[nx] + B[ny]})
+                shiftUp(heap, len(heap) - 1)
+                visited[nx][ny] = true
+            }
+        }
+    }
+    
+    return heap[0].value
+}
