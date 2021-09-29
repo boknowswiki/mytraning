093b12d8392613d6a741c1165ebcba395a1a7a19
boknowswiki@gmail.com <taobo0626@gmail.com>
2021-09-28 21:40:59 -0700

    0076 dp done

--- lintcode/golang/0076_longest_increasing_subsequence.go
+++ lintcode/golang/0076_longest_increasing_subsequence.go
@@ -5,6 +5,7 @@ import (
 )
 
 // dp
+// time O(n^2), space O(n)
 
 const MaxUint = ^uint(0)
 const MinUint = 0
@@ -54,4 +55,47 @@ func main() {
 	a := []int{5, 4, 1, 2, 3}
 	//a := []int{9, 3, 6, 2, 7}
 	fmt.Println(longestIncreasingSubsequence(a))
+}
+
+// binary search time(nlogn) space(o(n))
+
+func lengthOfLIS(nums []int) int {
+    l := len(nums)
+    if l < 1 {
+        return 0
+    }
+    tmp := make([]int, l, l)
+    tmp[0] = nums[0]
+    index := 0
+    for i := 1; i < l; i++ {
+        if nums[i] < tmp[0] {
+            tmp[0] = nums[i]
+        } else if nums[i] > tmp[index] {
+            index++
+            tmp[index] = nums[i]
+        } else {
+            tmp[lisBinarySearch(tmp, 0, index, nums[i])] = nums[i]
+        }
+    }
+    
+    return index + 1
+}
+
+func lisBinarySearch(tmp []int, start, end, target int) int {
+    for start + 1 < end {
+        mid := start + (end - start) / 2
+        if tmp[mid] == target {
+            return mid
+        }
+        if tmp[mid] < target {
+            start = mid
+        } else {
+            end = mid
+        }
+    }
+    
+    if tmp[start] >= target {
+        return start
+    }
+    return end
 }
