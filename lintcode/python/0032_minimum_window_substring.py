4a4af28014cf6f1f2c78e253765399850b287604
boknowswiki@gmail.com <taobo0626@gmail.com>
2021-09-29 21:48:38 -0700

    0032 two pointers done

--- lintcode/python/0032_minimum_window_substring.py
+++ lintcode/python/0032_minimum_window_substring.py
@@ -8,6 +8,56 @@
 #3.更新最小值
 #4.循环前3个步骤
 
+
+
+class Solution:
+    """
+    @param source : A string
+    @param target: A string
+    @return: A string denote the minimum window, return "" if there is no such a string
+    """
+    def minWindow(self, source , target):
+        # write your code here
+        n = len(source)
+
+        d = {}
+        for t in target:
+            d[t] = d.get(t, 0) + 1
+
+        ret = float("inf")
+        start, end = -1, -1
+        needed = len(d)
+        #print("start need", needed)
+        left = 0
+
+
+        #print(d)
+        for right in range(n):
+            c = source[right]
+            #print(c)
+            if c in d:
+                #print("reduce", c, d)
+                d[c] -= 1
+                if d[c] == 0:
+                    needed -= 1
+                    #print("needed", needed)
+            
+            while needed == 0 and left <= right:
+                #print(left, right)
+                if right - left+1 < ret:
+                    ret = right-left+1
+                    start, end = left, right
+                    #print ("update", start, end)
+                l_c = source[left]
+                if l_c in d:
+                    if d[l_c] == 0:
+                        needed += 1
+                    d[l_c] += 1
+                left += 1
+
+        return source[start:end+1] if start != -1 else ""
+                    
+
 class Solution:
     """
     @param source : A string
