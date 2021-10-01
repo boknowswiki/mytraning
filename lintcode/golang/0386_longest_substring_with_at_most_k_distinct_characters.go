79d05586ed84f3eda27d75f1f0b9f845aaef76ed
boknowswiki@gmail.com <taobo0626@gmail.com>
2021-09-30 17:19:09 -0700

    0386 two pointers done.

--- lintcode/golang/0386_longest_substring_with_at_most_k_distinct_characters.go
+++ lintcode/golang/0386_longest_substring_with_at_most_k_distinct_characters.go
@@ -1,3 +1,66 @@
+package main
+
+import (
+	"fmt"
+)
+
+// two pionters
+
+const MaxUint = ^uint(0)
+const MinUint = 0
+const MaxInt = int(MaxUint >> 1)
+const MinInt = -MaxInt - 1
+
+/**
+ * @param s: A string
+ * @param k: An integer
+ * @return: An integer
+ */
+func lengthOfLongestSubstringKDistinct(s string, k int) int {
+	// write your code here
+	n := len(s)
+	if n < k {
+		return n
+	}
+
+	d := make(map[uint8]int)
+	ret := 0
+	left := 0
+	for right := 0; right < n; right++ {
+		if val, ok := d[s[right]]; ok {
+			d[s[right]] = val + 1
+		} else {
+			d[s[right]] = 1
+		}
+		//fmt.Println(d)
+		for len(d) > k && left <= right {
+			//fmt.Println("removing left ", s[left])
+			d[s[left]]--
+			if d[s[left]] == 0 {
+				delete(d, s[left])
+			}
+			left++
+		}
+		ret = max(ret, right-left+1)
+	}
+
+	return ret
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	a := "eceba"
+	b := 3
+	fmt.Println(lengthOfLongestSubstringKDistinct(a, b))
+}
+
+
 /**
  * @param s: A string
  * @param k: An integer
