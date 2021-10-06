5c902822715eb4d88b56a370005783623e0486a6
Bo Tao <boknowswiki@gmail.com>
2021-10-06 16:36:15 -0700

    0201 segment tree build done again.

--- lintcode/python/0201_segement_tree_build.py
+++ lintcode/python/0201_segement_tree_build.py
@@ -32,3 +32,42 @@ class Solution:
         
         return node
 
+
+
+# python3 env to run
+
+"""
+Definition of SegmentTreeNode:
+class SegmentTreeNode:
+    def __init__(self, start, end):
+        self.start, self.end = start, end
+        self.left, self.right = None, None
+"""
+
+class SegmentTreeNode:
+    def __init__(self, start, end):
+        self.start, self.end = start, end
+        self.left, self.right = None, None
+
+class Solution:
+    """
+    @param: start: start value.
+    @param: end: end value.
+    @return: The root of Segment Tree.
+    """
+    def build(self, start, end):
+        # write your code here
+        if start > end:
+            return None
+
+        if start == end:
+            return SegmentTreeNode(start, start)
+        
+        root = SegmentTreeNode(start, end)
+        root.left = self.build(start, (start+end)//2)
+        root.right = self.build((start+end)//2+1, end)
+        return root
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.build(1,4))
