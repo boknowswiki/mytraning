e445783fd9289ef4d10cbc02c7fd51a0619197d0
Bo Tao <boknowswiki@gmail.com>
2021-10-06 16:54:37 -0700

    update tests

--- lintcode/python/test/test.py
+++ lintcode/python/test/test.py
@@ -1,59 +1,37 @@
 #!/usr/bin/python -t
 
-class trieNode:
-    def __init__(self):
-        self.children = {}
-        self.is_word = False
+"""
+Definition of SegmentTreeNode:
+class SegmentTreeNode:
+    def __init__(self, start, end):
+        self.start, self.end = start, end
+        self.left, self.right = None, None
+"""
 
-class WordDictionary:
-    def __init__(self):
-        self.root = trieNode()
+class SegmentTreeNode:
+    def __init__(self, start, end):
+        self.start, self.end = start, end
+        self.left, self.right = None, None
 
+class Solution:
     """
-    @param: word: Adds a word into the data structure.
-    @return: nothing
+    @param: start: start value.
+    @param: end: end value.
+    @return: The root of Segment Tree.
     """
-    def addWord(self, word):
+    def build(self, start, end):
         # write your code here
-        cur = self.root
-        for w in word:
-            if w not in cur.children:
-                cur.children[w] = trieNode()
-            cur = cur.children[w]
+        if start > end:
+            return None
 
-        cur.is_word = True
-
-        return
-
-    """
-    @param: word: A word could contain the dot character '.' to represent any one letter.
-    @return: if the word is in the data structure.
-    """
-    def search(self, word):
-        # write your code here
-        if word == None:
-            return False
-        return self.find(word, self.root, 0)
+        if start == end:
+            return SegmentTreeNode(start, start)
         
+        root = SegmentTreeNode(start, end)
+        root.left = self.build(start, (start+end)/2)
+        root.right = self.build((start+end)/2+1, end)
+        return root
 
-    def find(self, word, node, index):
-        if node == None:
-            return False
-        if index >= len(word):
-            return node.is_word
-            
-        c = word[index]
-        if c != ".":
-            return self.find(word, node.children.get(c), index+1)
-        
-        for child in node.children:
-            if self.find(word, node.children[child], index+1):
-                return True
-
-        return False
-
-if __name__ == "__main__":
-    s = WordDictionary()
-    s.addWord("a")
-    print(s.search("."))
-    
+if __name__ == '__main__':
+    s = Solution()
+    print(s.build(1,4))
