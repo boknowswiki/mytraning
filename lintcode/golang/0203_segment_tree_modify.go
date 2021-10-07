e951072a3913cfa43f978ff2e4efaf82729bca35
Bo Tao <boknowswiki@gmail.com>
2021-10-06 21:49:32 -0700

    0203 segment tree done.

--- lintcode/golang/0203_segment_tree_modify.go
+++ lintcode/golang/0203_segment_tree_modify.go
@@ -1,3 +1,15 @@
+package main
+
+// segment tree
+
+/**
+ * Definition of SegmentTreeNode:
+ */
+type SegmentTreeNode struct {
+	Start, End, Max int
+	Left, Right     *SegmentTreeNode
+}
+
 /**
  * Definition of SegmentTreeNode:
  * type SegmentTreeNode struct {
@@ -12,33 +24,36 @@
  * @param value: value
  * @return: nothing
  */
-func modify (root **SegmentTreeNode, index int, value int)  {
-    // write your code here
-    if *root == nil {
-        return
-    }
-    
-    if (*root).Start == index && (*root).End == index {
-        (*root).Max = value
-        return
-    }
-    
-    mid := ((*root).Start + (*root).End)/2
-    
-    if (*root).Start <= mid && index <= mid {
-        modify(&(*root).Left, index, value)
-    }
-    
-    if index <= (*root).End && index > mid {
-        modify(&(*root).Right, index, value)
-    }
-    
-    (*root).Max = max((*root).Left.Max, (*root).Right.Max)
+func modify(root **SegmentTreeNode, index int, value int) {
+	// write your code here
+	if (*root).Start == (*root).End && (*root).Start == index {
+		(*root).Max = value
+		return
+	}
+	if (*root).Left != nil && ((*root).Left.Start <= index && (*root).Left.End >= index) {
+		modify(&(*root).Left, index, value)
+	}
+	if (*root).Right != nil && ((*root).Right.Start <= index && (*root).Right.End >= index) {
+		modify(&(*root).Right, index, value)
+	}
+	if (*root).Left != nil && (*root).Right != nil {
+		(*root).Max = max((*root).Left.Max, (*root).Right.Max)
+	} else if (*root).Left == nil && (*root).Right != nil {
+		(*root).Max = (*root).Right.Max
+	} else if (*root).Right == nil && (*root).Left != nil {
+		(*root).Max = (*root).Left.Max
+	}
+
+	return
 }
 
 func max(a, b int) int {
-    if a > b {
-        return a
-    }
-    return b
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+
 }
