fd54332b8f1f628f22ae8772ef9887ba090c64e5
Bo Tao <boknowswiki@gmail.com>
2021-10-07 15:53:18 -0700

    0205 segment tree done.

--- lintcode/golang/0205_interval_minimum_number.go
+++ lintcode/golang/0205_interval_minimum_number.go
@@ -1,79 +1,115 @@
+package main
+
+import "fmt"
+
+// segment tree
+
+const MaxUint = ^uint(0)
+const MaxInt = int(MaxUint >> 1)
+
+type segNode struct {
+	Start, End, Min int
+	Left, Right     *segNode
+}
+
 /**
  * Definition of Interval:
  * type Interval struct {
  *     Start, End int
  * }
  */
+type Interval struct {
+	Start, End int
+}
 
 /**
  * @param A: An integer array
  * @param queries: An query list
  * @return: The result list
  */
- 
-type SegNode struct {
-    Start, End, Min int
-    Left, Right *SegNode
+func intervalMinNumber(A []int, queries []*Interval) []int {
+	// write your code here
+	segTree := build(A)
+
+	ret := []int{}
+
+	for i := range queries {
+		ret = append(ret, query(segTree, queries[i].Start, queries[i].End))
+	}
+
+	return ret
+}
+
+func build(a []int) *segNode {
+	root := helper(a, 0, len(a)-1)
+	return root
+}
+
+func helper(a []int, start, end int) *segNode {
+	if start > end {
+		return nil
+	}
+	if start == end {
+		return &segNode{
+			Start: start,
+			End:   end,
+			Min:   a[start],
+		}
+	}
+	node := &segNode{
+		Start: start,
+		End:   end,
+	}
+	mid := (start + end) / 2
+	node.Left = helper(a, start, mid)
+	node.Right = helper(a, mid+1, end)
+	if node.Left != nil && node.Right != nil {
+		node.Min = min(node.Left.Min, node.Right.Min)
+	}
+	fmt.Println(node, node.Left, node.Right)
+
+	return node
+}
+
+func query(root *segNode, start, end int) int {
+	if root.Start == start && root.End == end {
+		return root.Min
+	}
+
+	mid := (root.Start + root.End) / 2
+	left := MaxInt
+	right := MaxInt
+	if start <= mid {
+		if mid < end {
+			left = query(root.Left, start, mid)
+		} else {
+			left = query(root.Left, start, end)
+		}
+	}
+	if mid < end {
+		if start <= mid {
+			right = query(root.Right, mid+1, end)
+		} else {
+			right = query(root.Right, start, end)
+		}
+	}
+
+	return min(left, right)
 }
 
 func min(a, b int) int {
-    if a < b {
-        return a
-    }
-    return b
+	if a < b {
+		return a
+	}
+	return b
 }
 
-func buildSeg(A[]int, start, end int) *SegNode {
-    if start > end {
-        return nil
-    }
-    
-    if start == end {
-        return &SegNode {
-            Start: start,
-            End: end,
-            Min: A[start],
-        }
-    }
-    
-    root := &SegNode {
-        Start: start,
-        End: end,
-    }
-    
-    mid := (start+end)/2
-    root.Left = buildSeg(A, start, mid)
-    root.Right = buildSeg(A, mid+1, end)
-    root.Min = min(root.Left.Min, root.Right.Min)
-    
-    return root
+func main() {
+	a := []int{1, 2, 7, 8, 5}
+	q := []*Interval{
+		&Interval{Start: 1, End: 2},
+		&Interval{Start: 0, End: 4},
+		&Interval{Start: 2, End: 4},
+	}
+	fmt.Println(intervalMinNumber(a, q))
 }
-
-func querySeg(node *SegNode, start, end int) int {
-    if start > node.End || end < node.Start {
-        return 1<<31
-    }
-    if start <= node.Start && end >= node.End {
-        return node.Min
-    }
-    
-    return min(querySeg(node.Left, start, end), querySeg(node.Right, start, end))
-}
-
-func intervalMinNumber (A []int, queries []*Interval) []int {
-    // write your code here
-    var ret []int
-    n := len(A)
-    if n == 0 {
-        return ret
-    }
-    
-    root := buildSeg(A, 0, len(A)-1)
-    
-    for _, q := range queries {
-        ret = append(ret, querySeg(root, q.Start, q.End))
-    }
-    
-    return ret
-}
-
