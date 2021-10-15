e5b82c4a0c11fc5bf3c35dc45e4fe8c8f1ebed3a
Bo Tao <boknowswiki@gmail.com>
2021-10-14 21:46:12 -0700

    0360 heap done

--- lintcode/golang/0360_sliding_window_median.go
+++ lintcode/golang/0360_sliding_window_median.go
@@ -1,5 +1,146 @@
+package main
+
+// heap, ac works!
+//time O(nlogn)
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type Node struct {
+	val   int
+	index int
+}
+
+type MinHeap []*Node
+
+func (h MinHeap) Len() int           { return len(h) }
+func (h MinHeap) Less(i, j int) bool { return h[i].val < h[j].val }
+func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MinHeap) Push(x interface{}) {
+	*h = append(*h, x.(*Node))
+}
+
+func (h *MinHeap) Pop() interface{} {
+	cur := *h
+	n := len(cur)
+	x := cur[n-1]
+	*h = cur[:n-1]
+	return x
+}
+
+/**
+ * @param nums: A list of integers
+ * @param k: An integer
+ * @return: The median of the element inside the window at each moving
+ */
+func medianSlidingWindow(nums []int, k int) []int {
+	// write your code here
+	n := len(nums)
+	if n == 0 || n < k || k == 0 {
+		return []int{}
+	}
+
+	var ret []int
+	minH := &MinHeap{}
+	maxH := &MinHeap{}
+	mid := 0
+
+	for i, val := range nums[:k] {
+		heap.Push(maxH, &Node{val: -val, index: i})
+	}
+
+	for i := k / 2; i >= 0; i-- {
+		node := heap.Pop(maxH).(*Node)
+		node.val = -node.val
+		heap.Push(minH, node)
+	}
+
+	for maxH.Len() > minH.Len()+1 {
+		node := heap.Pop(maxH).(*Node)
+		node.val = -node.val
+		heap.Push(minH, node)
+	}
+	for minH.Len() > maxH.Len() {
+		node := heap.Pop(minH).(*Node)
+		node.val = -node.val
+		heap.Push(maxH, node)
+	}
+
+	mid = -(*maxH)[0].val
+
+	ret = append(ret, mid)
+
+	for i := k; i < n; i++ {
+		fmt.Println("mid is ", mid)
+		if nums[i] <= mid {
+			heap.Push(maxH, &Node{val: -nums[i], index: i})
+		} else {
+			heap.Push(minH, &Node{val: nums[i], index: i})
+		}
+
+		if nums[i-k] <= mid {
+			index := getIndex(maxH, true, nums[i-k])
+			if index == -1 {
+				panic("max not find num")
+			}
+			heap.Remove(maxH, index)
+		} else {
+			index := getIndex(minH, false, nums[i-k])
+			if index == -1 {
+				panic("min not find num")
+			}
+			heap.Remove(minH, index)
+		}
+
+		for maxH.Len() > minH.Len()+1 {
+			node := heap.Pop(maxH).(*Node)
+			node.val = -node.val
+			heap.Push(minH, node)
+		}
+		for minH.Len() > maxH.Len() {
+			node := heap.Pop(minH).(*Node)
+			node.val = -node.val
+			heap.Push(maxH, node)
+		}
+		mid = -(*maxH)[0].val
+		ret = append(ret, mid)
+	}
+
+	return ret
+}
+
+func getIndex(a *MinHeap, negtived bool, target int) int {
+	for i := 0; i < a.Len(); i++ {
+		if negtived {
+			if (*a)[i].val == -target {
+				return i
+			}
+		} else {
+			if (*a)[i].val == target {
+				return i
+			}
+		}
+	}
+	return -1
+}
+
+func main() {
+	a := []int{
+		1, 2, 7, 8, 5,
+	}
+	k := 3
+	fmt.Println(medianSlidingWindow(a, k))
+}
+
+
 
 // heap, not ac, but should correct.
+
+
+
 
 
 
