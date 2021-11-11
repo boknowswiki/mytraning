00ff1bdfc66204dacfca87479338d14b21c8421f
Bo Tao <boknowswiki@gmail.com>
2021-11-11 09:43:52 -0800

    0117 dp with comments.

--- lintcode/golang/0117_jump_game_II.go
+++ lintcode/golang/0117_jump_game_II.go
@@ -35,6 +35,38 @@ func jump (A []int) int {
 // ETL
 // dp
 
+
+/**
+ * @param A: A list of integers
+ * @return: An integer
+ */
+func jump(A []int) int {
+	// write your code here
+	// state: dp[i] the minimum step at ith index.
+	// func: dp[i] = min(dp[i], dp[j]+1) j from 0 to i.
+	// init: dp[i] = maxInt, dp[0] = 0
+	// result: dp[n-1]
+
+	n := len(A)
+	if n == 0 {
+		return 0
+	}
+
+	dp := make([]int, n)
+	dp[0] = 0
+
+	for i := 1; i < n; i++ {
+		dp[i] = MaxInt
+		for j := 0; j < i; j++ {
+			if dp[j] != MaxInt && A[j]+j >= i {
+				dp[i] = min(dp[i], dp[j]+1)
+			}
+		}
+	}
+
+	return dp[n-1]
+}
+
 /**
  * @param A: A list of integers
  * @return: An integer
