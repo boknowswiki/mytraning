eddeb3e1e8a2355c0dd7ba6dcac509ff7d97c968
Bo Tao <boknowswiki@gmail.com>
2021-11-22 21:07:08 -0800

    0159 binary search better done.

--- lintcode/golang/0159_find_minimum_in_a_rotated_sorted_array.go
+++ lintcode/golang/0159_find_minimum_in_a_rotated_sorted_array.go
@@ -1,3 +1,51 @@
+
+package main
+
+// binary search O(logn)
+
+import (
+	"fmt"
+)
+
+/**
+ * @param nums: a rotated sorted array
+ * @return: the minimum number in the array
+ */
+func findMin(nums []int) int {
+	// write your code here
+	n := len(nums)
+	l := 0
+	r := n - 1
+
+	for l+1 < r {
+		mid := (l + r) / 2
+		if nums[mid] > nums[l] && nums[l] > nums[r] {
+			l = mid
+		} else {
+			r = mid
+		}
+	}
+	return min(nums[l], nums[r])
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	a := []int{4, 5, 6, 7, 0, 1, 2}
+	//a := []int{1, 2, 4, 5, 6, 7, 0}
+	//a := []int{0, 1, 2, 4, 5, 6, 7}
+	//a := []int{1, 2, 4}
+	//b := 3
+	fmt.Println(findMin(a))
+}
+
+
+
 /**
  * @param nums: a rotated sorted array
  * @return: the minimum number in the array
