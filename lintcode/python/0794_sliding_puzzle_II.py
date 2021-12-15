ffa6666b0427477ae3bd06a75a43cf50b323a8b6
Bo Tao <boknowswiki@gmail.com>
2021-12-14 21:25:48 -0800

    0794 bfs done

--- lintcode/python/0794_sliding_puzzle_II.py
+++ lintcode/python/0794_sliding_puzzle_II.py
@@ -61,3 +61,58 @@ class Solution:
 
         return nexts
 
+
+
+# bfs
+
+import collections
+
+class Solution:
+    """
+    @param init_state: the initial state of chessboard
+    @param final_state: the final state of chessboard
+    @return: return an integer, denote the number of minimum moving
+    """
+    def minMoveStep(self, init_state, final_state):
+        # # write your code here
+        boardStr = ""
+        for i in range(len(init_state)):
+            for j in range(len(init_state[0])):
+                boardStr += str(init_state[i][j])
+
+        expectStr = ""
+        for i in range(len(final_state)):
+            for j in range(len(final_state[0])):
+                expectStr += str(final_state[i][j])
+        q = collections.deque([boardStr])
+        v = set()
+        v.add(boardStr)
+        dirs = [(1,0), (-1, 0), (0, 1), (0, -1)]
+        ret = 0
+
+        while q:
+            qLen = len(q)
+            for _ in range(qLen):
+                curBoard = q.popleft()
+
+                if curBoard == expectStr:
+                    return ret
+                zeroIndex = curBoard.find('0')
+
+                x = zeroIndex//3
+                y = zeroIndex%3
+                for i in range(len(dirs)):
+                    nx = x + dirs[i][0]
+                    ny = y + dirs[i][1]
+                    #print(curBoard, nx, ny, zeroIndex)
+                    if 0 <= nx < 3 and 0 <= ny < 3:
+                        minIndex = min(nx*3+ny, zeroIndex)
+                        maxIndex = max(nx*3+ny, zeroIndex)
+                        nextBoard = curBoard[:minIndex] + curBoard[maxIndex] + curBoard[minIndex+1:maxIndex] + curBoard[minIndex]+ curBoard[maxIndex+1:]
+                        if nextBoard not in v:
+                            v.add(nextBoard)
+                            q.append(nextBoard)
+
+            ret += 1
+
+        return -1
