8df3b84b4712a194f9f6877f9ba2a4e539a7e019
Bo Tao <boknowswiki@gmail.com>
2021-12-16 21:18:07 -0800

    0448 done

--- lintcode/python/0448_inorder_successor_in_bst.py
+++ lintcode/python/0448_inorder_successor_in_bst.py
@@ -1,5 +1,37 @@
 #!/usr/bin/python -t
 
+
+# binary search tree
+# similar to 0915
+
+"""
+Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+"""
+
+
+class Solution:
+    """
+    @param: root: The root of the BST.
+    @param: p: You need find the successor node of p.
+    @return: Successor of p.
+    """
+    def inorderSuccessor(self, root, p):
+        # write your code here
+        suc = None
+        while root:
+            if root.val > p.val:
+                suc = root
+                root = root.left
+            else:
+                root = root.right
+
+        return suc
+
 #首先要确定中序遍历的后继:
 #
 #如果该节点有右子节点, 那么后继是其右子节点的子树中最左端的节点
