fca7ef5ef05b9ee0f119a2df4e2a66524105e943
Bo Tao <boknowswiki@gmail.com>
2021-12-18 21:07:56 -0800

    0123 dfs done

--- lintcode/python/0123_word_search.py
+++ lintcode/python/0123_word_search.py
@@ -1,5 +1,54 @@
 #!/usr/bin/python -t
 
+
+# dfs
+
+class Solution:
+    """
+    @param board: A list of lists of character
+    @param word: A string
+    @return: A boolean
+    """
+    def exist(self, board, word):
+        # write your code here
+        m = len(board)
+        n = len(board[0])
+
+        v = set()
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == word[0]:
+                    v.add((i, j))
+                    if self.dfs(board, i, j, v, 1, word):
+                        return True
+                    v.remove((i, j))
+
+        return False
+
+    def dfs(self, board, x, y, v, index, word):
+        if index == len(word):
+            return True
+
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        for k in range(len(dirs)):
+            nx = dirs[k][0] + x
+            ny = dirs[k][1] + y
+            if 0 <= nx < len(board) and 0 <= ny < len(board[0]) and (nx, ny) not in v and board[nx][ny] == word[index]:
+                v.add((nx, ny))
+                if self.dfs(board, nx, ny, v, index+1, word):
+                    return True
+                v.remove((nx,ny))
+
+        return False
+
+
+if __name__ == '__main__':
+    s = Solution()
+    a = ["ABCE","SFES","ADEE"]
+    b = "ABCESEEEFS"
+    print(s.exist(a,b))
+
 # dfs and backtracking
 
 class Solution:
