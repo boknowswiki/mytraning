e6faf1fe2d54494c81e21d73615a712190864f66
Bo Tao <boknowswiki@gmail.com>
2022-01-02 21:20:34 -0800

    0494 done again

--- lintcode/python/0494_implement_stack_by_two_queues.py
+++ lintcode/python/0494_implement_stack_by_two_queues.py
@@ -1,6 +1,56 @@
 #!/usr/bin/python -t
 
 
+# stack
+
+
+import collections
+
+class Stack:
+    def __init__(self):
+        self.q1 = collections.deque()
+        self.q2 = collections.deque()
+
+    """
+    @param: x: An integer
+    @return: nothing
+    """
+    def push(self, x):
+        # write your code here
+        self.q1.append(x)
+        return
+
+    """
+    @return: nothing
+    """
+    def pop(self):
+        # write your code here
+        while len(self.q1) > 1:
+            self.q2.append(self.q1.popleft())
+
+        val = self.q1.popleft()
+
+        self.q1, self.q2 = self.q2, self.q1
+
+        return val
+    """
+    @return: An integer
+    """
+    def top(self):
+        # write your code here
+        val = self.pop()
+        self.push(val)
+
+        return val
+
+    """
+    @return: True if the stack is empty
+    """
+    def isEmpty(self):
+        # write your code here
+        return not self.q1
+
+
 import Queue
 
 class Stack:
