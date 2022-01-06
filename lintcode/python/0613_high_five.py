893f3f188a7ca30eebebd699bd85dd016cc2d0ab
Bo Tao <boknowswiki@gmail.com>
2022-01-05 21:25:04 -0800

    0613 done again

--- lintcode/python/0613_high_five.py
+++ lintcode/python/0613_high_five.py
@@ -1,6 +1,39 @@
 #!/usr/bin/python -t
 
 # heap
+
+
+import heapq
+
+'''
+Definition for a Record
+class Record:
+    def __init__(self, id, score):
+        self.id = id
+        self.score = score
+'''
+class Solution:
+    # @param {Record[]} results a list of <student_id, score>
+    # @return {dict(id, average)} find the average of 5 highest scores for each person
+    # <key, value> (student_id, average_score)
+    def highFive(self, results):
+        # Write your code here
+        d = {}
+        for result in results:
+            if result.id not in d:
+                d[result.id] = []
+
+            heapq.heappush(d[result.id], result.score)
+            if len(d[result.id]) > 5:
+                heapq.heappop(d[result.id])
+        #print(d)
+        ret = {}
+        for k in d:
+            ret[k] = (sum(d[k])/5)
+
+        return ret
+
+# heap
 # 利用map，找到对应的人的成绩，然后暴力比较出前5个。
 
 '''
