29d4c136d48b1b67dc94e49c5dd1e194536b7988
Bo Tao <boknowswiki@gmail.com>
2022-01-15 12:32:08 -0800

    1492 binary search done

--- lintcode/python/1492_koko_eating_bananas.py
+++ lintcode/python/1492_koko_eating_bananas.py
@@ -1,5 +1,44 @@
 #!/usr/bin/python -t
 
+# binary search
+
+
+import sys, math
+
+class Solution:
+    """
+    @param piles: an array
+    @param H: an integer
+    @return: the minimum integer K
+    """
+    def minEatingSpeed(self, piles, H):
+        # Write your code here
+        m = len(piles)
+        start = 1
+        end = max(piles)
+        #print(start, end)
+
+        while start + 1 < end:
+            mid = (start+end)//2
+            hour = self.getHour(piles, mid)
+            if hour <= H:
+                end = mid
+            else:
+                start = mid
+
+        if self.getHour(piles, start) <= H:
+            return start
+
+        return end
+
+    def getHour(self, piles, val):
+        hour = 0
+        for i in range(len(piles)):
+            hour += math.ceil(piles[i]/val)
+
+        return hour
+
+
 # binary search solution
 
 class Solution:
