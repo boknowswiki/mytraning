f36174fb672f943a942f23e9b82e7718c60181eb
Bo Tao <boknowswiki@gmail.com>
2022-01-22 21:24:46 -0800

    1031 bfs done again

--- lintcode/python/1031_is_graph_bipartite.py
+++ lintcode/python/1031_is_graph_bipartite.py
@@ -1,5 +1,52 @@
 #!/usr/bin/python -t
 
+
+# bfs
+# 需要注意的是， 不是所有的nodes都联通， 所有要额外搞个loop来保证所有nodes都visit过。
+
+
+import collections
+
+class Solution:
+    """
+    @param graph: the given undirected graph
+    @return:  return true if and only if it is bipartite
+    """
+    def isBipartite(self, graph):
+        # Write your code here
+        if not graph:
+            return False
+
+        q = collections.deque()
+        v = set()
+        nodes_set = [set(), set()]
+        step = 0
+
+        for i in range(len(graph)):
+            if i in v:
+                continue
+
+            if graph[i]:
+                q.append(i)
+                v.add(i)
+
+            while q:
+                q_size = len(q)
+                for _ in range(q_size):
+                    node = q.popleft()
+                    nodes_set[step%2].add(node)
+                    for next_node in graph[node]:
+                        if next_node in nodes_set[step%2]:
+                            return False
+                        if next_node not in v:
+                            q.append(next_node)
+                            v.add(next_node)
+
+                step += 1
+
+        return True
+
+
 # BFS
 
 
