4a1fe320f37b7664de912b36e37b2c9beb8f063e
Bo Tao <boknowswiki@gmail.com>
2022-06-07 21:20:48 -0700

    0017 done again

--- lintcode/python/0017_subsets.py
+++ lintcode/python/0017_subsets.py
@@ -1,4 +1,50 @@
-#!/usr/bin/python -t
+#!/usr/bin/python3 -t
+
+# time O(n*2^n)
+# space O(n)
+#时间复杂度：O(n×2^n)。一共 2^n2 个状态，每种状态需要 O(n) 的时间来构造子集。
+
+#空间复杂度：O(n)。临时数组 t 的空间代价是 O(n)，递归时栈空间的代价为 O(n)。
+
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param nums: A set of numbers
+    @return: A list of lists
+             we will sort your return value in output
+    """
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # write your code here
+        if not nums:
+            return [[]]
+
+        nums.sort()
+        ret = []
+        self.dfs(nums, 0, [], ret)
+
+        return ret
+
+    def dfs(self, nums, index, path, ret):
+        if index == len(nums):
+            ret.append(list(path))
+            return
+
+        path.append(nums[index])
+        self.dfs(nums, index+1, path, ret)
+        path.pop()
+        self.dfs(nums, index+1, path, ret)
+
+
+
+
+if __name__ == '__main__':
+    s = Solution()
+    a = [0]
+    a = [1,2,3]
+    print(s.subsets(a))
 
 # bfs
 
