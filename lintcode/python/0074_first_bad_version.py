11fab8b0d4f4b28a65e6ddfd1607af97cb071786
Bo Tao <boknowswiki@gmail.com>
2022-06-16 21:32:45 -0700

    0074 binary search done again

--- lintcode/python/0074_first_bad_version.py
+++ lintcode/python/0074_first_bad_version.py
@@ -1,4 +1,36 @@
-#!/usr/bin/python -t
+#!/usr/bin/python3 -t
+
+
+# binary search
+# time O(logn)
+# space O(1)
+
+#class SVNRepo:
+#    @classmethod
+#    def isBadVersion(cls, id)
+#        # Run unit tests to check whether verison `id` is a bad version
+#        # return true if unit tests passed else false.
+# You can use SVNRepo.isBadVersion(10) to check whether version 10 is a
+# bad version.
+class Solution:
+    """
+    @param n: An integer
+    @return: An integer which is the first bad version.
+    """
+    def findFirstBadVersion(self, n):
+        # write your code here
+        start = 1
+        end = n
+        while start + 1 < end:
+            mid = start + (end-start)//2
+            if SVNRepo.isBadVersion(mid):
+                end = mid
+            else:
+                start = mid
+
+        if SVNRepo.isBadVersion(start):
+            return start
+        return end
 
 # binary search solution
 
