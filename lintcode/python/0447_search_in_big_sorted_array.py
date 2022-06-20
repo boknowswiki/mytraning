578747810ccc3395e4a691de01dbef9d739ac524
Bo Tao <boknowswiki@gmail.com>
2022-06-20 11:15:39 -0700

    0447 binary search done again.

--- lintcode/python/0447_search_in_big_sorted_array.py
+++ lintcode/python/0447_search_in_big_sorted_array.py
@@ -1,4 +1,44 @@
-#!/usr/bin/python -t
+#!/usr/bin/python3 -t
+
+
+# binary search
+# time O(logn)
+# space O(1)
+
+
+class Solution:
+    """
+    @param reader: An instance of ArrayReader.
+    @param target: An integer
+    @return: An integer which is the first index of target.
+    """
+    def searchBigSortedArray(self, reader, target):
+        # write your code here
+        end = self.get_end(reader, target)
+
+        start = 0
+
+        while start + 1 < end:
+            mid = start + (end-start)//2
+            if reader.get(mid) < target:
+                start = mid
+            else:
+                end = mid
+
+        if reader.get(start) == target:
+            return start
+
+        if reader.get(end) == target:
+            return end
+
+        return -1
+
+    def get_end(self, reader, target):
+        order = 1
+        while reader.get(target*order) < target:
+            order = order * 2
+
+        return target*order
 
 # binary search solution
 
