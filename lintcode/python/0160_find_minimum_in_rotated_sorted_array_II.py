d16195675669ae9efc750fb370bf83cb12106fee
Bo Tao <boknowswiki@gmail.com>
2022-06-20 13:27:30 -0700

    0160 binary search done again

--- lintcode/python/0160_find_minimum_in_rotated_sorted_array_II.py
+++ lintcode/python/0160_find_minimum_in_rotated_sorted_array_II.py
@@ -1,4 +1,49 @@
-#!/usr/bin/python -t
+#!/usr/bin/python3 -t
+
+
+# binary search
+# time O(logn), worst case O(n)
+# space O(1)
+
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param nums: a rotated sorted array
+    @return: the minimum number in the array
+    """
+    def find_min(self, nums: List[int]) -> int:
+        # write your code here
+        n = len(nums)
+        if n == 0:
+            return -1
+
+        start = 0
+        end = n-1
+
+        while start + 1 < end:
+            if nums[start] < nums[end]:
+                return nums[start]
+
+            mid = start + (end-start)//2
+
+            if nums[mid] > nums[end]:
+                start = mid
+            elif nums[mid] < nums[end]:
+                end = mid
+            else:
+                end -= 1
+
+        return min(nums[start], nums[end])
+
+
+if __name__ == '__main__':
+    s = Solution()
+    a = [4,4,5,6,7,0,1,2]
+    a = [1,1,-1,1]
+    print(s.find_min(a))
 
 # binary search solution
 
