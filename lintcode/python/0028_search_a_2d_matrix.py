b33b834dc21fce5134754059993990cf7cba8867
Bo Tao <boknowswiki@gmail.com>
2022-06-21 21:25:02 -0700

    0028 binary search done again

--- lintcode/python/0028_search_a_2d_matrix.py
+++ lintcode/python/0028_search_a_2d_matrix.py
@@ -1,4 +1,76 @@
-#!/usr/bin/python -t
+#!/usr/bin/python3 -t
+
+# binary search
+# time O(log(m) + log(n))
+# space O(1)
+
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param matrix: matrix, a list of lists of integers
+    @param target: An integer
+    @return: a boolean, indicate whether matrix contains target
+    """
+    def search_matrix(self, matrix: List[List[int]], target: int) -> bool:
+        # write your code here
+        m = len(matrix)
+        if m == 0:
+            return False
+        n = len(matrix[0])
+        if n == 0:
+            return False
+
+        row = self.find_row(matrix, target)
+
+        return self.find_num(matrix[row], target)
+
+    def find_row(self, matrix, target):
+        m, n = len(matrix), len(matrix[0])
+        if target >= matrix[m-1][0]:
+            return m-1
+        start = 0
+        end = m-1
+        while start + 1 < end:
+            mid = start + (end-start)//2
+            if matrix[mid][0] > target:
+                end = mid
+            else:
+                start = mid
+
+        if target > matrix[end][0]:
+            return end
+        return start
+
+    def find_num(self, row, target):
+        start = 0
+        end = len(row)-1
+
+        while start + 1 < end:
+            mid = start + (end-start)//2
+            if row[mid] > target:
+                end = mid
+            else:
+                start = mid
+
+        if row[end] == target:
+            return True
+        if row[start] == target:
+            return True
+        return False
+
+if __name__ == '__main__':
+    s = Solution()
+    a = [
+        [1, 3, 5, 7],
+        [10, 11, 16, 20],
+        [23, 30, 34, 50]
+        ]
+    a = [[1,4,5],[6,7,8]]
+    b = 3
+    print(s.search_matrix(a, b))
 
 # binary search one search
 
