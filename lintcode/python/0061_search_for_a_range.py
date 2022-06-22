2590384e2bbd648bb3a24c68de6449fde8673a93
Bo Tao <boknowswiki@gmail.com>
2022-06-21 21:42:09 -0700

    0061 binary search done again

--- lintcode/python/0061_search_for_a_range.py
+++ lintcode/python/0061_search_for_a_range.py
@@ -1,4 +1,76 @@
-#!/usr/bin/python -t
+#!/usr/bin/python3 -t
+
+# binary search
+# time O(logn)
+# space O(1)
+
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param a: an integer sorted array
+    @param target: an integer to be inserted
+    @return: a list of length 2, [index1, index2]
+    """
+    def search_range(self, a: List[int], target: int) -> List[int]:
+        # write your code here
+        ret = [-1, -1]
+        if not a:
+            return ret
+
+        if len(a) == 1 and a[0] != target:
+            return ret
+
+        ret[0] = self.find_lower(a, target)
+        ret[1] = self.find_higher(a, target)
+
+        return ret
+
+    def find_lower(self, a, target):
+        start = 0
+        end = len(a) - 1
+
+        while start + 1 < end:
+            mid = start + (end-start)//2
+            if a[mid] >= target:
+                end = mid
+            else:
+                start = mid
+
+        if a[start] == target:
+            return start
+        if a[end] == target:
+            return end
+
+        return -1
+
+    def find_higher(self, a, target):
+        start = 0
+        end = len(a) - 1
+
+        while start + 1 < end:
+            mid = start + (end-start)//2
+            if a[mid] > target:
+                end = mid
+            else:
+                start = mid
+
+        if a[end] == target:
+            return end
+        if a[start] == target:
+            return start
+
+        return -1
+
+
+if __name__ == '__main__':
+    s = Solution()
+    a = []
+    a = [5, 7, 7, 8, 8, 10]
+    b = 8
+    print(s.search_range(a, b))
 
 # binary search solution
 
