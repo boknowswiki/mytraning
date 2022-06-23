fe29b8bf8e28612c0697e5a14f7f17b0cd935463
Bo Tao <boknowswiki@gmail.com>
2022-06-22 21:13:14 -0700

    0600 binary search done again

--- lintcode/python/0600_smallest_rectangle_enclosing_black_pixels.py
+++ lintcode/python/0600_smallest_rectangle_enclosing_black_pixels.py
@@ -1,4 +1,124 @@
-#!/usr/bin/python -t
+#!/usr/bin/python3 -t
+
+# binary search
+# time O(nlogm + mlogn)
+# space O(1)
+
+# can improve the code by find_lower and find_higher with col or row to call
+# empty_col or empty_row accordingly.
+
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param image: a binary matrix with '0' and '1'
+    @param x: the location of one of the black pixels
+    @param y: the location of one of the black pixels
+    @return: an integer
+    """
+    def min_area(self, image: List[List[str]], x: int, y: int) -> int:
+        # write your code here
+        m = len(image)
+        if m == 0:
+            return 0
+        n = len(image[0])
+        if n == 0:
+            return 0
+
+        left = self.find_left(image, 0, y)
+        right = self.find_right(image, y, n-1)
+        top = self.find_top(image, 0, x)
+        bottom = self.find_bottom(image, x, m-1)
+
+        #print(f"{left}, {right}, {top}, {bottom}")
+        return (right-left+1) * (bottom-top+1)
+
+    def find_left(self, image, start, end):
+        m, n = len(image), len(image[0])
+
+        while start + 1 < end:
+            mid = start + (end-start)//2
+            if self.empty_col(image, mid):
+                start = mid
+            else:
+                end = mid
+
+        #print(f"{start}, {end}")
+        if not self.empty_col(image, start):
+            return start
+
+        return end
+
+    def find_right(self, image, start, end):
+        m, n = len(image), len(image[0])
+
+        while start + 1 < end:
+            mid = start + (end-start)//2
+            if self.empty_col(image, mid):
+                end = mid
+            else:
+                start = mid
+
+        if not self.empty_col(image, end):
+            return end
+
+        return start
+
+    def find_top(self, image, start, end):
+        m, n = len(image), len(image[0])
+
+        while start + 1 < end:
+            mid = start + (end-start)//2
+            if self.empty_row(image, mid):
+                start = mid
+            else:
+                end = mid
+
+        if not self.empty_row(image, start):
+            return start
+
+        return end
+
+    def find_bottom(self, image, start, end):
+        m, n = len(image), len(image[0])
+
+        while start + 1 < end:
+            mid = start + (end-start)//2
+            if self.empty_row(image, mid):
+                end = mid
+            else:
+                start = mid
+
+        if not self.empty_row(image, end):
+            return end
+
+        return start
+
+    def empty_col(self, image, col):
+        m, n = len(image), len(image[0])
+        for i in range(m):
+            #print(f"empty col {col}, {image[i][col]}")
+            if image[i][col] == "1":
+                return False
+
+        return True
+
+    def empty_row(self, image, row):
+        m, n = len(image), len(image[0])
+        for i in range(n):
+            if image[row][i] == "1":
+                return False
+
+        return True
+
+if __name__ == '__main__':
+    s = Solution()
+    a = ["0010","0110","0100"]
+    b = 0
+    c = 2
+    print(s.min_area(a, b, c))
 
 # binary search
 # time nlog(m) + mlog(n)
