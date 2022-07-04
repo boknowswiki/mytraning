3986c9ee0a0ae9c1d4e9ad38b85785a3f1eb75fd
Bo Tao <boknowswiki@gmail.com>
2022-07-03 20:33:13 -0700

    1311 binary tree done again

--- lintcode/python/1311_lowest_common_ancestor_of_a_binary_search_tree.py
+++ lintcode/python/1311_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -1,5 +1,62 @@
 #!/usr/bin/python -t
 
+# binary tree
+# iteration way
+# time O(n)
+# space O(1)
+
+
+class Solution:
+    """
+    @param root: root of the tree
+    @param p: the node p
+    @param q: the node q
+    @return: find the LCA of p and q
+    """
+    def lowestCommonAncestor(self, root, p, q):
+        # write your code here
+        if not root:
+            return None
+
+        if p.val > q.val:
+            return self.lowestCommonAncestor(root, q, p)
+
+        s = [root]
+
+        while len(s) != 0:
+            node = s.pop()
+
+            if p.val <= node.val <= q.val:
+                return node
+            elif p.val <= node.val and q.val <= node.val:
+                s.append(node.left)
+            else:
+                s.append(node.right)
+
+        return None
+
+# binary tree
+# traversal way
+# time O(n)
+# space O(1)
+
+class Solution:
+    """
+    @param root: root of the tree
+    @param p: the node p
+    @param q: the node q
+    @return: find the LCA of p and q
+    """
+    def lowestCommonAncestor(self, root, p, q):
+        # write your code here
+        if p.val > q.val:
+            return self.lowestCommonAncestor(root, q, p)
+        if p.val <= root.val <= q.val:
+            return root
+        if p.val <= root.val and q.val <= root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        return self.lowestCommonAncestor(root.right, p, q)
+
 # dfs, traversal solution
 
 """
