a0d64fa1a6eadbf57fecaceebc93ad5da96d6ec0
Bo Tao <boknowswiki@gmail.com>
2022-07-28 21:37:29 -0700

    0136 dfs done again

--- lintcode/python/0136_palindrome_partitioning.py
+++ lintcode/python/0136_palindrome_partitioning.py
@@ -1,4 +1,53 @@
-#!/usr/bin/python -t
+#!/usr/bin/python3 -t
+
+# dfs with memorization
+# time O(n*2^n)
+# space O(n)
+
+class Solution:
+    """
+    @param: s: A string
+    @return: A list of lists of string
+    """
+    def partition(self, s):
+        # write your code here
+        if not s:
+            return []
+
+        return self.dfs(s, {})
+
+    def dfs(self, s, memo):
+        if not s:
+            return []
+        if s in memo:
+            return memo[s]
+
+        parts = []
+
+        for i in range(len(s)-1):
+            prefix = s[:i+1]
+            if not self.is_palindrome(prefix):
+                continue
+
+            sub_parts = self.dfs(s[i+1:], memo)
+            for p in sub_parts:
+                parts.append([prefix]+p)
+
+        if self.is_palindrome(s):
+            parts.append([s])
+
+        memo[s] = parts
+        return parts
+
+    def is_palindrome(self, s):
+        return s == s[::-1]
+
+
+if __name__ == '__main__':
+    s = Solution()
+    a = "a"
+    a = "aab"
+    print(s.partition(a))
 
 # dfs
 # time O(2^n)
