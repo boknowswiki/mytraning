8a0b22739ea25e5d7c4a0ab822487328b93a0abc
Bo Tao <boknowswiki@gmail.com>
2022-08-02 21:22:20 -0700

    0450 linked list done again

--- lintcode/python/0450_reverse_nodes_in_k_group.py
+++ lintcode/python/0450_reverse_nodes_in_k_group.py
@@ -1,5 +1,72 @@
 #!/usr/bin/python -t
 
+
+# linked list
+# time O(n)
+# space O(1)
+
+
+from lintcode import (
+    ListNode,
+)
+
+"""
+Definition of ListNode:
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param head: a ListNode
+    @param k: An integer
+    @return: a ListNode
+    """
+    def reverse_k_group(self, head: ListNode, k: int) -> ListNode:
+        # write your code here
+        if not head or k <= 1:
+            return head
+
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+        end = dummy
+
+        while end.next != None:
+            for i in range(k):
+                if end != None:
+                    end = end.next
+                else:
+                    break
+            if end == None:
+                break
+
+            start = prev.next
+            next = end.next
+            end.next = None
+            prev.next = self.reverse(start)
+            start.next = next
+            prev = start
+            end = prev
+
+        return dummy.next
+
+    def reverse(self, head):
+        if not head or not head.next:
+            return head
+
+        prev = None
+
+        while head:
+            next = head.next
+            head.next = prev
+            prev = head
+            head = next
+
+        return prev
+
 # linked list
 
 """
