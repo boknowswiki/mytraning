caa88b6045c84f58ec519618bd95cbd99e0744a1
Bo Tao <boknowswiki@gmail.com>
2022-08-04 21:06:30 -0700

    0380 linked list done again

--- lintcode/python/0380_intersection_of_two_linked_lists.py
+++ lintcode/python/0380_intersection_of_two_linked_lists.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python -t
 
 # linked list
+# time O(n)
+# space O(1)
 
 
 """
@@ -29,3 +31,53 @@ class Solution:
         return ha
         
         
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param headA: the first list
+    @param headB: the second list
+    @return: a ListNode
+    """
+    def getIntersectionNode(self, headA, headB):
+        # write your code here
+        if not headA or not headB:
+            return None
+
+        lenA = self.get_len(headA)
+        lenB = self.get_len(headB)
+
+        while lenA > lenB:
+            headA = headA.next
+            lenA -= 1
+
+        while lenB > lenA:
+            headB = headB.next
+            lenB -= 1
+
+        if headA == headB:
+            return headA
+
+        while headA != headB:
+            headA = headA.next
+            headB = headB.next
+            if headA == headB:
+                return headA
+
+        return None
+
+
+    def get_len(self, head):
+        cnt = 0
+
+        while head:
+            cnt += 1
+            head = head.next
+
+        return cnt
