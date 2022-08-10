14c9d23556314eccd98a142ee07e364fc033c4d0
Bo Tao <boknowswiki@gmail.com>
2022-08-09 21:45:23 -0700

    0005 quick select done again

--- lintcode/python/0005_kth_largest_element.py
+++ lintcode/python/0005_kth_largest_element.py
@@ -1,4 +1,63 @@
-#!/usr/bin/python -t
+#!/usr/bin/python3 -t
+
+# quick select
+# time O(n)
+# space O(1)
+
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param k: An integer
+    @param nums: An array
+    @return: the Kth largest element
+    """
+    def kth_largest_element(self, k: int, nums: List[int]) -> int:
+        # write your code here
+        n = len(nums)
+        if n == 0 or n < k:
+            return 0
+
+        return self.quick_select(nums, 0, n-1, k-1)
+
+    def quick_select(self, nums, start, end, k):
+        if start == end:
+            return nums[start]
+
+        part = self.partition(nums, start, end)
+        #print(f"{nums}, {part}")
+        if part == k:
+            return nums[part]
+        elif part < k:
+            return self.quick_select(nums, part+1, end, k)
+        else:
+            return self.quick_select(nums, start, part-1, k)
+
+
+    def partition(self, nums, start, end):
+        pivot = nums[end]
+
+        i = start
+
+        for j in range(start, end):
+            if nums[j] >= pivot:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+
+        nums[i], nums[end] = nums[end], nums[i]
+
+        return i
+
+
+
+
+if __name__ == '__main__':
+    s = Solution()
+    a = 1
+    b = [1,3,4,2]
+    print(s.kth_largest_element(a, b))
 
 # quick select
 # similar to 0461
