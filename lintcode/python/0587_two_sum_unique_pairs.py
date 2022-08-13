b558893817c9dba4efc1af0062669507ea3ee13c
Bo Tao <boknowswiki@gmail.com>
2022-08-12 20:54:37 -0700

    0587 hash table and two pointers done again

--- lintcode/python/0587_two_sum_unique_pairs.py
+++ lintcode/python/0587_two_sum_unique_pairs.py
@@ -67,3 +67,34 @@ class Solution:
         return ret
         
         
+# hash table
+# time O(n)
+# space O(n)
+
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param nums: an array of integer
+    @param target: An integer
+    @return: An integer
+    """
+    def two_sum6(self, nums: List[int], target: int) -> int:
+        # write your code here
+        num_map = {}
+
+        for num in nums:
+            num_map[num] = num_map.get(num, 0) + 1
+
+        cnt = 0
+
+        for num in num_map:
+            if num <= target//2 and target-num in num_map:
+                if num == target-num and num_map[num] >= 2:
+                    cnt += 1
+                if num != target-num:
+                    cnt += 1
+
+        return cnt
