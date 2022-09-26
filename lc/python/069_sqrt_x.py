af9b1bd36f9f49a0038fe2255c46f352392ace68
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-09-26 16:04:12 -0700

    Update 069_sqrt_x.py
    069 binary search

--- lc/python/069_sqrt_x.py
+++ lc/python/069_sqrt_x.py
@@ -1,5 +1,31 @@
 #!/usr/bin/python -t
 
+# binary search
+# time O(logn)
+# space O(1)
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 1:
+            return x
+        
+        l = 0
+        r = x-1
+        
+        while l+1 < r:
+            mid = l + (r-l)//2
+            val = mid*mid
+            if val == x:
+                return mid
+            elif val < x:
+                l = mid
+            else:
+                r = mid
+                
+        if r * r < x:
+            return r
+        return l
+
 class Solution(object):
     def mySqrt(self, x):
         """
