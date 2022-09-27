8c5e50eb3965543cb2f95657d3120f32af5120dc
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-09-27 13:00:19 -0700

    704 binary search

--- lc/python/704_binary_search.py
+++ lc/python/704_binary_search.py
@@ -1,5 +1,30 @@
 #!/usr/bin/python -t
 
+# binary search
+# time O(logn)
+# space O(1)
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums)-1
+        
+        while l + 1 < r:
+            mid = l + (r-l)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid
+            else:
+                r = mid
+                
+        if nums[l] == target:
+            return l
+        if nums[r] == target:
+            return r
+        
+        return -1
+
 class Solution(object):
     def search(self, nums, target):
         """
