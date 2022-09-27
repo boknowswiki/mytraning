64a613e09f733dc25dd03490374f525277bdc68b
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-09-27 12:47:16 -0700

    374 binary search

--- lc/python/374_guess_number_higher_or_lower.py
+++ lc/python/374_guess_number_higher_or_lower.py
@@ -1,5 +1,33 @@
 #!/usr/bin/python -t
 
+# binary search
+# time O(logn)
+# space O(1)
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        l = 0
+        r = n
+        
+        while l <= r:
+            mid = l + (r-l)//2
+            val = guess(mid)
+            if val == 0:
+                return mid
+            elif val < 0:
+                r = mid - 1
+            else:
+                l = mid + 1
+                
+        return -1
+
 #time O(log2 ^n) space O(1)
 
 # The guess API is already defined for you.
