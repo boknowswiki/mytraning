3158ecce4ad5afce2912ac0a2d8e45fe8aa37984
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-09-27 12:53:11 -0700

    441 binary search

--- lc/python/441_arranging_coins.py
+++ lc/python/441_arranging_coins.py
@@ -1,5 +1,31 @@
 #!/usr/bin/python -t
 
+# binary search
+# time O(logn)
+# space O(1)
+
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        l = 0
+        r = n
+        
+        while l + 1 < r:
+            mid = l + (r-l)//2
+            coin_needed = self.coin_num(mid)
+            if coin_needed == n:
+                return mid
+            elif coin_needed < n:
+                l = mid
+            else:
+                r = mid
+                
+        if self.coin_num(r) <= n:
+            return r
+        return l
+    
+    def coin_num(self, num):
+        return (1+num)*num//2
+
 #time O(log2^n) space O(1)
 
 class Solution(object):
