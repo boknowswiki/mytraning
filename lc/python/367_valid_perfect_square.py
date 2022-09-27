7c7fd2eea9acd0faf22db2cf9c88172a7e2ed9d7
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-09-27 12:41:46 -0700

    367 binary search

--- lc/python/367_valid_perfect_square.py
+++ lc/python/367_valid_perfect_square.py
@@ -1,5 +1,29 @@
 #!/usr/bin/python -t
 
+# binary search
+# time O(logn)
+# space O(1)
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        if num == 1:
+            return True
+        
+        l = 0
+        r = num-1
+        
+        while l <= r:
+            mid = l + (r-l)//2
+            val = mid * mid
+            if val == num:
+                return True
+            elif val < num:
+                l = mid+1
+            else:
+                r = mid - 1
+                
+        return False
+
 class Solution(object):
     def isPerfectSquare(self, num):
         """
