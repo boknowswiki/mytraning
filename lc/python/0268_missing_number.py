230462acf617a99b41c5825ca82cc474fdeaefad
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-09-26 20:58:47 -0700

    0268 add binary search

--- lc/python/0268_missing_number.py
+++ lc/python/0268_missing_number.py
@@ -14,3 +14,27 @@ class Solution:
         return total
       
 # binary search
+# time O(nlogn)
+# space O(1)
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 0:
+            return 0
+        
+        nums.sort()
+        print(f"nums {nums}")
+        
+        l = 0
+        r = n
+        
+        while l < r:
+            mid = l + (r-l)//2
+            print(f"mid {mid}, nums[mid] {nums[mid]}, l {l}, r {r}")
+            if nums[mid] > mid:
+                r = mid
+            else:
+                l = mid+1
+                
+        return l
