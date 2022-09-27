0435619cb50929f36a9fb30cf90f537d54404e7c
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-09-26 21:04:10 -0700

    0278 binary search

--- lc/python/278_first_bad_version.py
+++ lc/python/278_first_bad_version.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python -t
 
+# binary search
+# time O(logn)
+# space O(1)
+
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l = 0
+        r = n
+        while l + 1 < r:
+            mid = l + (r-l)//2
+            if isBadVersion(mid):
+                r = mid
+            else:
+                l = mid
+                
+        return r
+
 #time O(logn) space O(1)
 
 # The isBadVersion API is already defined for you.
