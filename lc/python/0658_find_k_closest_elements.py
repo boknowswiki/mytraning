871a2be2d57a2bb5bb6929fc7a92ea0d309a8249
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-09-28 21:31:00 -0700

    0658 binary search

---
+++ lc/python/0658_find_k_closest_elements.py
@@ -0,0 +1,17 @@
+# binary search
+# time O(log(n-k))
+# space O(1)
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l = 0
+        r = len(arr)-k
+        
+        while l < r:
+            mid = l + (r-l)//2
+            if x-arr[mid] > arr[mid+k] - x:
+                l = mid + 1
+            else:
+                r = mid
+                
+        return arr[l:l+k]
