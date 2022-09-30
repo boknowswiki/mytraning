de657e9e37ef1d91c41da0c599304449aa5ef2f1
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-09-30 16:14:49 -0700

    034 binary search

--- lc/python/034_find_first_and_last_position_of_element_in_sorted_array.py
+++ lc/python/034_find_first_and_last_position_of_element_in_sorted_array.py
@@ -1,5 +1,55 @@
 #!/usr/bin/python -t
 
+# binary search
+# time O(logn)
+# space O(1)
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        if n == 0:
+            return [-1, -1]
+        low = self.find_low(nums, target)
+        high = self.find_high(nums, target)
+        
+        return [low, high]
+    
+    def find_low(self, nums, target):
+        l = 0
+        r = len(nums)-1
+        
+        while l + 1 < r:
+            mid = l + (r-l)//2
+            if nums[mid] >= target:
+                r = mid
+            else:
+                l = mid
+                
+        if nums[l] == target:
+            return l
+        if nums[r] == target:
+            return r
+        
+        return -1
+    
+    def find_high(self, nums, target):
+        l = 0
+        r = len(nums)-1
+        
+        while l + 1 < r:
+            mid = l + (r-l)//2
+            if nums[mid] > target:
+                r = mid
+            else:
+                l = mid
+            
+        if nums[r] == target:
+            return r
+        if nums[l] == target:
+            return l
+        
+        return -1      
+
 class Solution(object):
     def searchRange(self, nums, target):
         """
