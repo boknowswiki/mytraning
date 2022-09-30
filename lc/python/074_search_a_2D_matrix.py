f6042e79f77c0fc979c7bf5ba6e6bda64815f098
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-09-30 16:30:11 -0700

    074 binary search

--- lc/python/074_search_a_2D_matrix.py
+++ lc/python/074_search_a_2D_matrix.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python -t
 
+# binary search
 #Time O(logmn) space O(1)
 
 class Solution(object):
@@ -27,4 +28,51 @@ class Solution(object):
             else:
                 end = mid - 1
                 
+        return False
+
+    class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        if m == 0:
+            return False
+        n = len(matrix[0])
+        if n == 0:
+            return False
+        
+        row = self.find_row(matrix, target)
+        
+        return self.find_num(matrix[row], target)
+    
+    def find_row(self, matrix, target):
+        l = 0
+        r = len(matrix)-1
+        
+        while l + 1 < r:
+            mid = l + (r-l)//2
+            if matrix[mid][0] <= target:
+                l = mid
+            else:
+                r = mid
+                
+        if matrix[r][0] <= target:
+            return r
+        return l
+    
+    def find_num(self, nums, target):
+        l = 0
+        r = len(nums)-1
+        
+        while l + 1 < r:
+            mid = l + (r-l)//2
+            if nums[mid] == target:
+                return True
+            elif nums[mid] < target:
+                l = mid
+            else:
+                r = mid
+                
+        if nums[l] == target:
+            return True
+        if nums[r] == target:
+            return True
         return False
