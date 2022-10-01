10293507822e9b4eaac5e1377c8926c7c1fe98b0
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-09-30 17:01:45 -0700

    162 binary search

--- lc/python/162_find_peak_element.py
+++ lc/python/162_find_peak_element.py
@@ -1,5 +1,36 @@
 #!/usr/bin/python -t
 
+# binary search
+# time O(logn)
+# space O(1)
+
+from typing import List
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        l = 0
+        r = n-1
+        
+        while l + 1 < r:
+            mid = l + (r-l)//2
+            if mid >0 and mid < n-1 and nums[mid-1] < nums[mid] > nums[mid+1]:
+                return mid
+            if mid > 0 and nums[mid] > nums[mid-1]:
+                l = mid
+            else:
+                r = mid
+                
+        if nums[l] > nums[r]:
+            return l
+        return r
+
+if __name__ == "__main__":
+    s = Solution()
+    a = [1,2,3,1]
+    a = [1,6,5,4,3,2,1]
+    print(s.findPeakElement(a))
+
 #time O(n) space O(1)
 
 class Solution(object):
