11d386039107548830f0821e442c62aafec9b6ed
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-10-02 22:33:13 -0700

    287 binary search

--- lc/python/287_find_the_duplicate_number.py
+++ lc/python/287_find_the_duplicate_number.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python -t
 
+# binary search
+# time O(nlogn)
+# space O(1)
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        l = 1
+        r = len(nums) - 1
+        dup = -1
+        
+        while l <= r:
+            mid = l + (r-l)//2
+            cnt = 0
+            
+            cnt = sum(num <= mid for num in nums)
+            if cnt > mid:
+                dup = mid
+                r = mid-1
+            else:
+                l = mid+1
+                
+        return dup
+
 class Solution(object):
     def findDuplicate(self, nums):
         """
