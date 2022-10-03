68360771b8924ab88552e87f94355bdf73c152ec
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-10-02 17:12:57 -0700

    300 binary search and greedy

--- lc/python/300_longest_increasing_subsequence.py
+++ lc/python/300_longest_increasing_subsequence.py
@@ -1,5 +1,60 @@
 #!/usr/bin/python -t
 
+# greedy and binary search
+# time O(nlogn)
+# space O(n)
+
+
+from typing import List
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 0:
+            return 0
+        
+        q = []
+        
+        for num in nums:
+            print(f"num {num}, q {q}")
+            index = self.find_num(q, num)
+            print(f"index {index}")
+            if index >= len(q):
+                q.append(num)
+            else:
+                q[index] = num
+                
+        return len(q)
+    
+    def find_num(self, arr, num):
+        if len(arr) == 0:
+            return 0
+        l = 0
+        r = len(arr)-1
+        
+        while l + 1 < r:
+            mid = l + (r-l)//2
+            if arr[mid] > num:
+                r = mid
+            else:
+                l = mid
+                
+        if r == len(arr)-1 and arr[r] < num:
+            return len(arr)
+
+        if arr[l] >= num:
+            return l
+
+        return r
+
+if __name__ == "__main__":
+    s = Solution()
+    a = [10,9,2,5,3,7,101,18]
+    a = [0,1,0,3,2,3]
+    a = [4,10,4,3,8,9]
+    a = [3,5,6,2,5,4,19,5,6,7,12]
+    print(s.lengthOfLIS(a))
+
 #dp my own solution
 # time O(n^2)
 
