51cf7f3046b57b336baaedfbdbd7812513c9f5a4
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-10-11 13:07:19 -0700

    104 binary tree and dfs and bfs

--- lc/python/104_maximum_depth_of_binary_tree.py
+++ lc/python/104_maximum_depth_of_binary_tree.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python -t
 
+# binary tree and dfs
+# time O(n)
+# space O(1)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        return max(self.maxDepth(root.left), self.maxDepth(root.right))+1
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
