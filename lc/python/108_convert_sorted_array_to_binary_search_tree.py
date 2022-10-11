9e0ef9447fc0dfc5ba14d1c66c034e82beef0974
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-10-11 13:11:52 -0700

    0108 binary tree and dfs, bfs

--- lc/python/108_convert_sorted_array_to_binary_search_tree.py
+++ lc/python/108_convert_sorted_array_to_binary_search_tree.py
@@ -1,5 +1,36 @@
 #!/usr/bin/python -t
 
+# binary tree and dfs
+# time O(n)
+# space O(1)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if len(nums) == 0:
+            return None
+        
+        return self.helper(nums, 0, len(nums)-1)
+    
+    def helper(self, nums, start, end):
+        if start > end:
+            return None
+        
+        if start == end:
+            return TreeNode(val=nums[start])
+        
+        mid = start + (end-start)//2
+        node = TreeNode(val=nums[mid])
+        node.left = self.helper(nums, start, mid-1)
+        node.right = self.helper(nums, mid+1, end)
+        
+        return node
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
