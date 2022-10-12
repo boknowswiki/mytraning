97f4cfd1524cefe6f7fae0880c132993d412a230
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-10-11 22:02:09 -0700

    144 binary tree and dfs

--- lc/python/144_binary_tree_preorder_traversal.py
+++ lc/python/144_binary_tree_preorder_traversal.py
@@ -1,5 +1,35 @@
 #!/usr/bin/python -t
 
+# binary tree dfs
+# time O(n)
+# space O(1)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return None
+        
+        ret = []
+        self.helper(root, ret)
+        
+        return ret
+    
+    def helper(self, node, ret):
+        if not node:
+            return
+        
+        ret.append(node.val)
+        self.helper(node.left, ret)
+        self.helper(node.right, ret)
+        
+        return
+
 #time O(n) space O(n)
 
 # Definition for a binary tree node.
