16c052c90df2ae41deed7cf679aa8e64359d69a2
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-10-11 21:29:18 -0700

    226 binary tree bfs and dfs

--- lc/python/226_invert_binary_tree.py
+++ lc/python/226_invert_binary_tree.py
@@ -1,5 +1,36 @@
 #!/usr/bin/python -t
 
+# binary tree and bfs
+# time O(n)
+# space O(n)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        q = collections.deque([root])
+        
+        while q:
+            cur = q.popleft()
+            left = cur.left
+            cur.left = cur.right
+            cur.right = left
+            
+            if cur.left:
+                q.append(cur.left)
+            if cur.right:
+                q.append(cur.right)
+                
+        return root
+    
+
 #time O(n) space O(n)
 
 # Definition for a binary tree node.
