5cc6286676f732a1635c21e1bae11b411e43dde2
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-10-13 21:41:01 -0700

    0572 binary tree and dfs

---
+++ lc/python/0572_subtree_of_another_tree.py
@@ -0,0 +1,43 @@
+# binary tree and dfs
+# time O(n)
+# space O(1)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        # Check for all subtree rooted at all nodes of tree "root"
+        def dfs(node):
+
+            # If this node is Empty, then no tree is rooted at this Node
+            # Thus "tree rooted at node" cannot be same as "rooted at subRoot"
+            # "tree rooted at subRoot" will always be non-empty (constraints)
+            if node is None:
+                return False
+
+            # If "tree rooted at node" is identical to "tree at subRoot"
+            elif is_identical(node, subRoot):
+                return True
+
+            # If "tree rooted at node" was not identical.
+            # Check for tree rooted at children
+            return dfs(node.left) or dfs(node.right)
+
+        def is_identical(node1, node2):
+
+            # If any one Node is Empty, both should be Empty
+            if node1 is None or node2 is None:
+                return node1 is None and node2 is None
+
+            # Both Nodes Value should be Equal
+            # And their respective left and right subtree should be identical
+            return (node1.val == node2.val and
+                    is_identical(node1.left, node2.left) and
+                    is_identical(node1.right, node2.right))
+
+        # Check for node rooted at "root"
+        return dfs(root)
