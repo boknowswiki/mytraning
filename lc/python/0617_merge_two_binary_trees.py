e6f2ca65f68436395133aad3f64e35cf626a12de
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-10-13 21:22:06 -0700

    0617 binary tree and dfs

---
+++ lc/python/0617_merge_two_binary_trees.py
@@ -0,0 +1,55 @@
+# binary tree and dfs
+# time O(n)
+# space O(1)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        return self.helper(root1, root2)
+    
+    def helper(self, node1, node2):
+        if node1 == None:
+            return node2
+        if node2 == None:
+            return node1
+        
+        left = self.helper(node1.left, node2.left)
+        right = self.helper(node1.right, node2.right)
+        cur = TreeNode(val=node1.val+node2.val, left=left, right=right)
+        
+        return cur
+        
+   # iterative way
+  
+  public class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null)
+            return t2;
+        Stack < TreeNode[] > stack = new Stack < > ();
+        stack.push(new TreeNode[] {t1, t2});
+        while (!stack.isEmpty()) {
+            TreeNode[] t = stack.pop();
+            if (t[0] == null || t[1] == null) {
+                continue;
+            }
+            t[0].val += t[1].val;
+            if (t[0].left == null) {
+                t[0].left = t[1].left;
+            } else {
+                stack.push(new TreeNode[] {t[0].left, t[1].left});
+            }
+            if (t[0].right == null) {
+                t[0].right = t[1].right;
+            } else {
+                stack.push(new TreeNode[] {t[0].right, t[1].right});
+            }
+        }
+        return t1;
+    }
+}
