ec6ae3bf72a6c1ef8ce398b7d38d049cbfd9ef06
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-10-18 13:08:48 -0700

    109 binary tree and linked list

--- lc/python/109_convert_sorted_list_to_binary_search_tree.py
+++ lc/python/109_convert_sorted_list_to_binary_search_tree.py
@@ -1,5 +1,43 @@
 #!/usr/bin/python -t
 
+# binary tree and linked list
+# time O(n)
+# space O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+        return self.helper(head)
+    
+    def helper(self, node):
+        if not node:
+            return None
+        if not node.next:
+            return TreeNode(node.val)
+        
+        slow, fast = node, node.next.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+        root_node = slow.next
+        slow.next = None
+        root = TreeNode(val=root_node.val)
+        root.left = self.helper(node)
+        root.right = self.helper(root_node.next)
+        
+        return root
+
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
@@ -42,3 +80,26 @@ class Solution(object):
 
         return self.sortedlisttobst(0, n-1)
 
+    
+    public TreeNode sortedListToBST(ListNode head) {
+    if(head == null)
+        return null;
+    ListNode fast = head;
+    ListNode slow = head;
+    ListNode prev =null; 
+    while(fast != null && fast.next != null)
+    {
+        fast = fast.next.next;
+        prev =slow;
+        slow=slow.next;
+    }
+    TreeNode root = new TreeNode(slow.val);
+    if(prev != null)
+        prev.next = null;
+    else
+        head  = null;
+        
+    root.left = sortedListToBST(head);
+    root.right = sortedListToBST(slow.next);
+    return root;
+}
