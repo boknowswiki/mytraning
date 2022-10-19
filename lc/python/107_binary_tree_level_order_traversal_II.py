6971178cbd2a3a88558191b7271da52ef407f4cc
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-10-18 17:22:52 -0700

    107 binary tree and bfs and dfs

--- lc/python/107_binary_tree_level_order_traversal_II.py
+++ lc/python/107_binary_tree_level_order_traversal_II.py
@@ -1,5 +1,39 @@
 #!/usr/bin/python -t
 
+# binary tree and bfs
+# time O(n)
+# space O(n)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        
+        ret = []
+        
+        q = collections.deque([root])
+        
+        while q:
+            level = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                
+            ret.append(list(level))
+
+                
+        return ret[::-1]
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
