18ea91d269f91c28a89f6a336d43c05bc1be3c08
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-10-19 22:19:30 -0700

    0116 binary tree and bfs

---
+++ lc/python/0116_populating_next_right_pointers_in_each_node.py
@@ -0,0 +1,46 @@
+# binary tree and bfs
+# time O(n)
+# space O(n)
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return None
+        
+        q = collections.deque([root])
+        ret = None
+        
+        while q:
+            l = len(q)
+            prev = None
+            for i in range(l):
+                #print(f"l {l}, prev {prev}")
+                node = q.popleft()
+                #print(f"node {node.val}")
+                if ret == None:
+                    ret = node
+                    node.next = None
+                if prev == None:
+                    prev = node
+                else:
+                    prev.next = node
+                    prev = prev.next
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            
+            if prev != None:
+                prev.next = None
+            
+        return ret
