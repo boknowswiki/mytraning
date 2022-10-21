8d07446817c5581b22b8d4f0064a11204be8fede
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-10-20 21:08:40 -0700

    235 binary tree and dfs

--- lc/python/235_lowest_common_ancestor_of_a_binary_search_tree.py
+++ lc/python/235_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python -t
 
+# binary tree and dfs
+# time O(n)
+# space O(1)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if p.val > q.val:
+            return self.lowestCommonAncestor(root, q, p)
+        
+        if p.val <= root.val <= q.val:
+            return root
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        return self.lowestCommonAncestor(root.right, p, q)
+    
+
 #time O(n) space O(n)
 #myself with hint
 
