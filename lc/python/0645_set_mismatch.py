45e9646ee7ca98c2d6d1505cfd5511b6928c2858
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-10-23 21:32:12 -0700

    0645 math better

--- lc/python/0645_set_mismatch.py
+++ lc/python/0645_set_mismatch.py
@@ -1,3 +1,23 @@
+# math
+# time O(n)
+# space O(1)
+
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        dup = missing = -1
+        
+        for num in nums:
+            if nums[abs(num)-1] < 0:
+                dup = abs(num)
+            else:
+                nums[abs(num)-1] *= -1
+                
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                missing = i+1
+                
+        return [dup, missing]
+
 # hashmap
 # time O(n)
 # space O(n)
