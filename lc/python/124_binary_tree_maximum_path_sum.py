f0a49f4c1b4abf2b634245a8961f5eeab4d8766d
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-10-24 21:07:02 -0700

    124 binary tree and dfs

--- lc/python/124_binary_tree_maximum_path_sum.py
+++ lc/python/124_binary_tree_maximum_path_sum.py
@@ -1,5 +1,33 @@
 #!/usr/bin/python -t
 
+# binary tree and dfs
+# time O(n)
+# space O(1)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        self.ret = -sys.maxsize-1
+        def helper(node):
+            if not node:
+                return 0
+            
+            left = max(0, helper(node.left))
+            right = max(0, helper(node.right))
+            self.ret = max(left+right+node.val, self.ret)
+            
+            return max(left, right) + node.val
+        
+        helper(root)
+        
+        return self.ret
+    
+
 import sys
 # Definition for a binary tree node.
 # class TreeNode(object):
