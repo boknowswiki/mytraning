7a682379425137df70b0fc86bbbdf3b129be49bd
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-10-27 21:34:38 -0700

    0215 heap and quick select

---
+++ lc/python/0215_kth_largest_element_in_an_array.py
@@ -0,0 +1,52 @@
+
+# heap
+# time O(nlogn)
+# space O(n-k)
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        hq = []
+        
+        for num in nums:
+            heapq.heappush(hq, -num)
+            while len(hq) > len(nums)-k+1:
+                heapq.heappop(hq)
+                
+        return -hq[0]
+      
+# quick select
+# time O(n)
+# space O(1)
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        if n == 0 or n < k:
+            return 0
+        
+        return self.qs(nums, 0, n-1, k-1)
+    
+    def qs(self, nums, start, end, k):
+        if start == end:
+            return nums[start]
+        
+        part = self.part(nums, start, end)
+        if part == k:
+            return nums[part]
+        elif part < k:
+            return self.qs(nums, part+1, end, k)
+        else:
+            return self.qs(nums, start, part-1, k)
+        
+    def part(self, nums, start, end):
+        pivot = nums[end]
+        i = start
+        
+        for j in range(start, end):
+            if nums[j] >= pivot:
+                nums[j], nums[i] = nums[i], nums[j]
+                i += 1
+                
+        nums[i], nums[end] = nums[end], nums[i]
+        
+        return i
