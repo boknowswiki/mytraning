15fdfab2eef4475f2afdafb91b9785f317d74e52
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-11-07 18:08:19 -0800

    0404 binary tree and bfs

--- lc/python/0404_sum_of_left_leaves.py
+++ lc/python/0404_sum_of_left_leaves.py
@@ -1,3 +1,34 @@
+
+# binary tree and bfs
+# time O(n)
+# space O(n)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        ret = 0
+        if not root:
+            return ret
+        
+        q = collections.deque([root])
+        
+        while q:
+            cur = q.popleft()
+            if cur.left and cur.left.left is None and cur.left.right is None:
+                ret += cur.left.val
+                
+            if cur.left:
+                q.append(cur.left)
+            if cur.right:
+                q.append(cur.right)
+                
+        return ret
+
 # binary tree and dfs
 # time O(n)
 # space O(1)
