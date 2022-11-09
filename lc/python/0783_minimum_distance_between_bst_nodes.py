1fbb83f647a11b54286d73e7f29f3afb6661de7f
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-11-08 18:02:12 -0800

    0783 binary tree, inorder and dfs

--- lc/python/0783_minimum_distance_between_bst_nodes.py
+++ lc/python/0783_minimum_distance_between_bst_nodes.py
@@ -1,4 +1,39 @@
 
+# binary tree and dfs
+# time O(n)
+# space O(1)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        self.ret = sys.maxsize
+        self.prev = None
+        self.helper(root)
+        return self.ret
+    
+    def helper(self, node):
+        if not node:
+            return
+        
+        self.helper(node.left)
+        if self.prev is None:
+            self.prev = node.val
+        else:
+            self.ret = min(self.ret, node.val-self.prev)
+            self.prev = node.val
+            
+        self.helper(node.right)
+        
+        return
+    
 # binary tree and inorder and dfs
 # time O(n)
 # space O(n)
