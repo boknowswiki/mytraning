3be9d3a795ed5c17359bcfe26c3f772835f0108b
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-11-10 21:18:31 -0800

    026 two pointers

--- lc/python/026_remove_duplicates_from_sorted_array.py
+++ lc/python/026_remove_duplicates_from_sorted_array.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python -t
 
+# two pointers and array
+# time O(n)
+# space O(1)
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 0:
+            return 0
+        
+        index = 0
+        
+        for i in range(1, n):
+            if nums[i] != nums[index]:
+                index +=1
+                nums[index] = nums[i]
+                
+        return index+1
+
 class Solution(object):
     def removeDuplicates(self, nums):
         """
