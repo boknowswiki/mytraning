e459e9ece6b909274ab462d11ab4aa278474341b
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-11-20 14:35:26 -0800

    329 bfs and topology sort

--- lc/python/329_longest_increasing_path_in_a_matrix.py
+++ lc/python/329_longest_increasing_path_in_a_matrix.py
@@ -1,5 +1,33 @@
 #!/usr/bin/python -t
 
+# bfs and topology sort
+
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+	# Step 1: build a directed acyclic graph
+        graph = collections.defaultdict(list)
+        indegree = collections.defaultdict(int)
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                neighbors = [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]
+                for x, y in neighbors:
+                    if 0<=x<len(matrix) and 0<=y<len(matrix[0]) and matrix[i][j] < matrix[x][y]:
+                        graph[(i,j)].append((x,y))
+                        indegree[(x,y)]+=1
+
+        # Step 2: Topological sorting with Kahn's algorithm
+        queue = collections.deque([(i, j) for i in range(len(matrix)) for j in range(len(matrix[0])) if (i,j) not in indegree])
+        max_path_len = 0
+        while queue:
+            max_path_len += 1
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                for neigh in graph[node]:
+                    indegree[neigh] -= 1
+                    if not indegree[neigh]:
+                        queue.append(neigh)
+        return max_path_len
+
 # dp dfs and memorization
 
 class Solution(object):
