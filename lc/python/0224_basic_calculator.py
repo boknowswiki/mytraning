ffc82b54c92048ff2c8e8a47e43b82fe27721e48
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-11-20 21:21:31 -0800

    0224 stack

---
+++ lc/python/0224_basic_calculator.py
@@ -0,0 +1,32 @@
+# stack
+# time O(n)
+# space O(n)
+
+class Solution:
+    def calculate(self, s: str) -> int:
+        def calc(it):
+            def update(op, v):
+                if op == "+": stack.append(v)
+                if op == "-": stack.append(-v)
+                if op == "*": stack.append(stack.pop() * v)
+                if op == "/": stack.append(stack.pop() // v)
+        
+            num, stack, sign = 0, [], "+"
+            
+            while it < len(s):
+                if s[it].isdigit():
+                    num = num * 10 + int(s[it])
+                elif s[it] in "+-*/":
+                    update(sign, num)
+                    num, sign = 0, s[it]
+                elif s[it] == "(":
+                    num, j = calc(it + 1)
+                    it = j - 1
+                elif s[it] == ")":
+                    update(sign, num)
+                    return sum(stack), it + 1
+                it += 1
+            update(sign, num)
+            return sum(stack)
+
+        return calc(0)
