2e86060fa002431b69aae9cdee25c72dc9399d78
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-11-20 21:34:42 -0800

    100 binary tree and dfs

--- lc/python/100_same_tree.py
+++ lc/python/100_same_tree.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python -t
 
+# binary tree and dfs
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p:
+            return not q
+        if not q:
+            return not p
+        
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+    
+
 # binary tree and bfs
 # time O(n)
 # space O(n)
