98d0905f4e94367df9e7df6b1cc8cbbaa770b0fc
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-11-25 21:24:43 -0800

    0559 dfs

--- lc/python/0559_maximum_depth_of_n_ary_tree.py
+++ lc/python/0559_maximum_depth_of_n_ary_tree.py
@@ -1,3 +1,34 @@
+# tree and dfs
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        
+        self.ret = 1
+        self.helper(root, 1)
+        return self.ret
+    
+    def helper(self, node, d):
+        if not node:
+            return
+        if not node.children:
+            self.ret = max(self.ret, d)
+            return
+
+        for c in node.children:
+            self.helper(c, d+1)
+            
+        return
+
 # tree and bfs
 # time O(n)
 # space O(n)
