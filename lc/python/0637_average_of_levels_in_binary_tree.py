6d9b5f488eca262752ef120be3a5a7828eef9dac
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-11-27 11:36:08 -0800

    0637 binary tree and dfs

--- lc/python/0637_average_of_levels_in_binary_tree.py
+++ lc/python/0637_average_of_levels_in_binary_tree.py
@@ -1,3 +1,35 @@
+
+# binary tree and dfs
+# time O(n)
+# space O(n)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        self.level = collections.defaultdict(list)
+        self.dfs(root, 0)
+        self.ret = []
+        
+        for i in range(len(self.level)):
+            self.ret.append(sum(self.level[i])/len(self.level[i]))
+            
+        return self.ret
+    
+    def dfs(self, node, level):
+        if not node:
+            return
+        
+        self.dfs(node.left, level+1)
+        self.level[level].append(node.val)
+        self.dfs(node.right, level+1)
+        
+        return
+
 # binary tree and bfs
 # time O(n)
 # space O(n)
