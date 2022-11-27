0ef8694802be388fa70ca91c40034ec56f4eb1d0
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-11-27 11:57:40 -0800

    0653 binary tree and dfs

--- lc/python/0653_two_sum_IV_input_is_a_bst.py
+++ lc/python/0653_two_sum_IV_input_is_a_bst.py
@@ -1,3 +1,33 @@
+# dfs
+# time O(n)
+# space O(n)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        if not root:
+            return False
+        
+        self.s = set()
+        
+        return self.dfs(root, k)
+    
+    def dfs(self, node, k):
+        if not node:
+            return False
+        
+        if k - node.val in self.s:
+            return True
+        self.s.add(node.val)
+        
+        return self.dfs(node.left, k) or self.dfs(node.right, k)
+        
+
 # bfs
 # time O(n)
 # space O(n)
