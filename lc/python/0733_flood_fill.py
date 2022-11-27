3e0cc90562f3b66fadeaa463e8aaea6beb3bbd1d
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-11-27 13:42:06 -0800

    0733 dfs and bfs

--- lc/python/0733_flood_fill.py
+++ lc/python/0733_flood_fill.py
@@ -1,6 +1,31 @@
+# dfs
+# time O(m*n)
+# space O(m*n)
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        old = image[sr][sc]
+        if old != color:
+            v = {(sr, sc)}
+            dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+            
+            def dfs(x, y):
+                image[x][y] = color
+                for i in range(len(dirs)):
+                    dx = x + dirs[i][0]
+                    dy = y + dirs[i][1]
+                    
+                    if 0 <= dx < len(image) and 0 <= dy < len(image[0]) and (dx, dy) not in v and image[dx][dy] == old:
+                        v.add((dx, dy))
+                        dfs(dx, dy)
+            
+            dfs(sr, sc)
+            
+        return image
+
 # bfs
-# time O(n)
-# space O(n)
+# time O(m*n)
+# space O(m*n)
 
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
