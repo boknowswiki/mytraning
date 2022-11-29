67cc878f9c31a95e08a40e874b86f6802e5a3429
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-11-28 21:30:39 -0800

    0099 binary tree and dfs

--- lc/python/0099_recover_binary_search_tree.py
+++ lc/python/0099_recover_binary_search_tree.py
@@ -1,3 +1,44 @@
+# binary tree and dfds
+# time O(n)
+# space O(1)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if not root:
+            return
+        
+        self.first, self.second = None, None
+        self.prev = None
+        
+        def dfs(node):
+            if not node:
+                return
+            
+            dfs(node.left)
+            
+            if self.prev is not None and node.val < self.prev.val:
+                if not self.first:
+                    self.first = self.prev
+                self.second = node
+            self.prev = node
+            dfs(node.right)
+            
+            return
+            
+        dfs(root)
+        self.first.val, self.second.val = self.second.val, self.first.val
+        
+        return
+
 # binary tree and iterative
 # time O(n)
 # space O(n)
