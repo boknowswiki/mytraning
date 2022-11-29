0f502fac4024a73e23f0153e5b7eee27529cd317
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-11-28 22:15:34 -0800

    0117 binary tree, dfs and bfs

--- lc/python/0117_populating_next_right_pointers_in_each_node_II.py
+++ lc/python/0117_populating_next_right_pointers_in_each_node_II.py
@@ -1,4 +1,41 @@
 
+# binary tree and dfs
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+        self.left_most = root
+        while self.left_most:
+            head = self.left_most
+            self.left_most = self.prev = None
+            while head: # Base on the above level
+                self.link_node(head.left) # Try to link nodes on the current level
+                self.link_node(head.right) # Try to link nodes on the current level 
+                head = head.next
+                
+        return root
+    
+    def link_node(self, node):
+        if not node:
+            return
+        if self.left_most == None:
+            self.left_most = node
+        if self.prev != None:
+            self.prev.next = node
+        self.prev = node
+        
+        
 # binary tree and bfs
 # time O(n)
 # space O(1)
