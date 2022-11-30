92eb2332abe49e1c5c70706cefbaef282353d967
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-11-29 21:43:57 -0800

    133 dfs and bfs

--- lc/python/133_clone_graph.py
+++ lc/python/133_clone_graph.py
@@ -1,5 +1,34 @@
 #!/usr/bin/python -t
 
+# dfs
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def __init__(self):
+        self.v = {}
+        
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return None
+        
+        if node in self.v:
+            return self.v[node]
+        
+        clone_node = Node(val=node.val)
+        self.v[node] = clone_node
+        
+        if node.neighbors:
+            clone_node.neighbors = [self.cloneGraph(n) for n in node.neighbors]
+                
+        return clone_node
+
 # bfs and graph
 # time O(n)
 # space O(n)
