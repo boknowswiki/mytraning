4e8ba3d1bcfc1b53fdce9a1c145185f8def26c5b
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-11-29 21:51:11 -0800

    199 binary tree, dfs and bfs

--- lc/python/199_binary_tree_right_side_view.py
+++ lc/python/199_binary_tree_right_side_view.py
@@ -1,5 +1,36 @@
 #!/usr/bin/python -t
 
+# dfs
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        self.ret = []
+        
+        self.dfs(root, 0)
+        
+        return self.ret
+    
+    def dfs(self, node, depth):
+        if not node:
+            return
+        
+        if depth == len(self.ret):
+            self.ret.append(node.val)
+
+        self.dfs(node.right, depth+1)
+        self.dfs(node.left, depth+1)
+            
+        return
+
 # binary tree and bfs
 # time O(n)
 # space O(n)
