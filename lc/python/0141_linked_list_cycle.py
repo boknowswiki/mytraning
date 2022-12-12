b7f0e1de29e82ba2f1178d711cf3b7414166271f
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-12-11 21:38:50 -0800

    0141 linked list

---
+++ lc/python/0141_linked_list_cycle.py
@@ -0,0 +1,24 @@
+# linked list
+# time O(n)
+# space O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return False
+
+        slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+
+        return False
