a79192e2ca7714ade6362b71e7b18f1db03c7a18
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-12-11 21:36:32 -0800

    0083 linked list

---
+++ lc/python/0083_remove_duplicates_from_sorted_linked_list.py
@@ -0,0 +1,22 @@
+# linked list
+# time O(n)
+# space O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        cur = head
+        while cur.next:
+            if cur.val == cur.next.val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+
+        return head
