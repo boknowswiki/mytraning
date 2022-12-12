bf9da797e9def0bb26c68726db491c16c3b8da73
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-12-11 21:30:40 -0800

    021 linked list

--- lc/python/021_merge_two_sorted_list.py
+++ lc/python/021_merge_two_sorted_list.py
@@ -1,5 +1,41 @@
 #!/usr/bin/python -t
 
+# linked list
+# time O(n)
+# space O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        dummy = ListNode()
+        prev = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                prev.next = list1
+                list1 = list1.next
+            else:
+                prev.next = list2
+                list2 = list2.next
+
+            prev = prev.next
+
+        if list1:
+            prev.next = list1
+        if list2:
+            prev.next = list2
+
+        return dummy.next
+    
+    
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
