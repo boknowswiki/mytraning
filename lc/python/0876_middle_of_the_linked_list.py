f3b5dede567579b95665721c25cfadf904c5f4f9
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-12-12 18:01:09 -0800

    0876 linked list

--- lc/python/0876_middle_of_the_linked_list.py
+++ lc/python/0876_middle_of_the_linked_list.py
@@ -1,4 +1,23 @@
 # linked list
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        f = s = head
+        while f and f.next:
+            f = f.next.next
+            s = s.next
+
+        return s
+
+# linked list
 # time O(n)
 # space O(1)
 
