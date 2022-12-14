376b44e84d1b395dd0d52f4d66517ccbc56a8859
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-12-13 21:31:22 -0800

    024 linked list

--- lc/python/024_swap_nodes_in_pairs.py
+++ lc/python/024_swap_nodes_in_pairs.py
@@ -1,5 +1,32 @@
 #!/usr/bin/python -t
 
+# linked list
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        dummy = ListNode()
+        prev = dummy
+        cur = head
+
+        while cur and cur.next:
+            nxt = cur.next
+            tmp = nxt.next
+            prev.next = nxt
+            nxt.next = cur
+            cur.next = tmp
+            prev = cur
+            cur = tmp
+
+        return dummy.next
+
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
