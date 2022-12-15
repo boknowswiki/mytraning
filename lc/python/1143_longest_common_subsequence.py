af0bafabf65ce028da7f28eafb383159d08c8b6a
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-12-14 22:05:56 -0800

    1143 dp

--- lc/python/1143_longest_common_subsequence.py
+++ lc/python/1143_longest_common_subsequence.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python -t
 
+# dp
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        # Make a grid of 0's with len(text2) + 1 columns 
+        # and len(text1) + 1 rows.
+        dp_grid = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+        
+        # Iterate up each column, starting from the last one.
+        for col in reversed(range(len(text2))):
+            for row in reversed(range(len(text1))):
+                # If the corresponding characters for this cell are the same...
+                if text2[col] == text1[row]:
+                    dp_grid[row][col] = 1 + dp_grid[row + 1][col + 1]
+                # Otherwise they must be different...
+                else:
+                    dp_grid[row][col] = max(dp_grid[row + 1][col], dp_grid[row][col + 1])
+        
+        # The original problem's answer is in dp_grid[0][0]. Return it.
+        return dp_grid[0][0]
+
 # dp solution time O(m*n) space O(m*n)
 
 # state: dp[i][j]: the longest common subsequence for first i from string 1, and first j from string 2
