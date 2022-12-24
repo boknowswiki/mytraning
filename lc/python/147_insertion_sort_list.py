048b4351b02a860e46eaefb7ffebce50e3b24118
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-12-23 21:30:27 -0800

    147 linked list

--- lc/python/147_insertion_sort_list.py
+++ lc/python/147_insertion_sort_list.py
@@ -1,5 +1,38 @@
 #!/usr/bin/python -t
 
+
+# linked list
+# time O(n^2)
+# space O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        curr = head
+
+        while curr:
+            # At each iteration, we insert an element into the resulting list.
+            prev = dummy
+
+            # find the position to insert the current node
+            while prev.next and prev.next.val <= curr.val:
+                prev = prev.next
+
+            next = curr.next
+            # insert the current node to the new list
+            curr.next = prev.next
+            prev.next = curr
+
+            # moving on to the next iteration
+            curr = next
+
+        return dummy.next
+
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
