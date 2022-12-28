0cbdeea1bd0bb212ef0475fba5582e9336657542
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-12-27 20:57:57 -0800

    0025 linked list

---
+++ lc/python/0025_reverse_nodes_in_k_group.py
@@ -0,0 +1,71 @@
+# linked list
+# time O(n)
+# space O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        cur = head
+        tail = None
+        new_head = None
+
+        while cur:
+            cnt = 0
+
+            while cnt < k and cur:
+                cur = cur.next
+                cnt += 1
+
+            if cnt == k:
+                # Reverse k nodes and get the new head
+                revHead = self.reverseLinkedList(head, k)
+                
+                # new_head is the head of the final linked list
+                if not new_head:
+                    new_head = revHead
+
+                if tail:
+                    tail.next = revHead
+                    
+                tail = head 
+                head = cur
+        
+        # attach the final, possibly un-reversed portion
+        if tail:
+            tail.next = head
+        
+        return new_head if new_head else head                
+        
+
+    def reverseLinkedList(self, head, k):
+        
+        # Reverse k nodes of the given linked list.
+        # This function assumes that the list contains 
+        # atleast k nodes.
+        new_head, ptr = None, head
+        while k:
+            
+            # Keep track of the next node to process in the
+            # original list
+            next_node = ptr.next
+            
+            # Insert the node pointed to by "ptr"
+            # at the beginning of the reversed list
+            ptr.next = new_head
+            new_head = ptr
+            
+            # Move on to the next node
+            ptr = next_node
+            
+            # Decrement the count of nodes to be reversed by 1
+            k -= 1
+        
+        # Return the head of the reversed list
+        return new_head
