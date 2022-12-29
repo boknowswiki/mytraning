dc4692108f8fed07352131b3fb641cb795259a0f
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-12-28 20:57:13 -0800

    0160 two pointers

--- lc/python/0160_intersections_of_two_linked_lists.py
+++ lc/python/0160_intersections_of_two_linked_lists.py
@@ -1,3 +1,27 @@
+
+# two pointers
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        pA = headA
+        pB = headB
+
+        while pA != pB:
+            pA = headB if pA is None else pA.next
+            pB = headA if pB is None else pB.next
+
+        return pA
+        # Note: In the case lists do not intersect, the pointers for A and B
+        # will still line up in the 2nd iteration, just that here won't be
+        # a common node down the list and both will reach their respective ends
+        # at the same time. So pA will be NULL in that case.
+
 # linked list
 
 # Definition for singly-linked list.
