01cd9bfa20bf8d10bdefe5e6690e3a6fea40d33a
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-12-29 15:42:10 -0800

    392 two pointers

--- lc/python/392_is_subsequence.py
+++ lc/python/392_is_subsequence.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python -t
 
+# two pointers
+# time O(m)
+# space O(1)
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        LEFT_BOUND, RIGHT_BOUND = len(s), len(t)
+
+        p_left = p_right = 0
+        while p_left < LEFT_BOUND and p_right < RIGHT_BOUND:
+            # move both pointers or just the right pointer
+            if s[p_left] == t[p_right]:
+                p_left += 1
+            p_right += 1
+
+        return p_left == LEFT_BOUND
+
 #time O(logs*log(len(t)) space O(len(t))
 
 from collections import defaultdict
