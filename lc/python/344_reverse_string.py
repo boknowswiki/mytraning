c5a203e43a87e038ebe810c0fb3e74a1c7947099
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-12-29 11:36:55 -0800

    344 two pointers

--- lc/python/344_reverse_string.py
+++ lc/python/344_reverse_string.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python -t 
 
+# two pointer
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        n = len(s)
+        if n < 2:
+            return
+        
+        l = 0
+        r = n-1
+
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
+
+        return
+
 class Solution(object):
     def reverseString(self, s):
         """
