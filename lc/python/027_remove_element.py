0cfb8196b0b9dbf316930de816c5c2d311fb0362
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-12-28 20:21:18 -0800

    027 two pointers

--- lc/python/027_remove_element.py
+++ lc/python/027_remove_element.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python -t
 
+# two pointers
+# time O(n)
+# space O(1)
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        if not nums:
+            return 0
+
+        index = 0
+
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[index] = nums[i]
+                index += 1
+
+        return index
+
 class Solution(object):
     def removeElement(self, nums, val):
         """
