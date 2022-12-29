1acac8c43c2c83f6adf7882e6adcce2834d57714
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-12-28 20:48:13 -0800

    088 two pointers

--- lc/python/088_merge_sorted_array.py
+++ lc/python/088_merge_sorted_array.py
@@ -1,5 +1,41 @@
 #!/usr/bin/python -t
 
+# two pointers
+# time O(m+n)
+# space O(1)
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if not nums2:
+            return
+        if not nums1:
+            nums1 = nums2
+            return
+
+        index = m+n-1
+        index1 = m-1
+        index2 = n-1
+
+        while index1 >= 0 and index2 >= 0:
+            #print(f"index1 {index1}, index2 {index2}, index {index}")
+            if nums1[index1] > nums2[index2]:
+                nums1[index] = nums1[index1]
+                index1 -= 1
+            else:
+                nums1[index] = nums2[index2]
+                index2 -= 1
+            index -= 1
+
+        while index2 >= 0:
+            nums1[index] = nums2[index2]
+            index -= 1
+            index2 -= 1
+
+        return
+
 #Time O(m+n), space O(1)
 
 class Solution(object):
