8ed72676fc5be48ec994956e5fe2d58f9cd2cb13
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-12-29 11:48:28 -0800

    283 two pointers

--- lc/python/283_move_zeros.py
+++ lc/python/283_move_zeros.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python -t
 
+# two pointers
+
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if not nums:
+            return
+        n = len(nums)
+        index = 0
+
+        for i in range(n):
+            if nums[i] != 0:
+                nums[index] = nums[i]
+                index += 1
+
+        while index < n:
+            nums[index] = 0
+            index += 1
+
+        return
+
 #Time O(n) space O(1)
 
 class Solution(object):
