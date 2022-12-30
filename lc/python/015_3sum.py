4069b4434fae1baad2caef9082fe8e798db63cb7
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-12-30 14:23:42 -0800

    015 two pointers

--- lc/python/015_3sum.py
+++ lc/python/015_3sum.py
@@ -1,5 +1,38 @@
 #!/usr/bin/python -t
 
+# two pointers
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        if n < 3:
+            return []
+
+        nums.sort()
+
+        ret = []
+
+        for i in range(n-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            j = i+1
+            k = n-1
+            need = -nums[i]
+            while j < k:
+                if nums[j]+nums[k] == need:
+                    #found = [nums[i], nums[j], nums[k]]
+                    ret.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while j < k and j > i+1 and nums[j] == nums[j-1]: j += 1
+                    while j < k and k < n-1 and nums[k] == nums[k+1]: k -= 1
+                elif nums[j] + nums[k] < need:
+                    j += 1
+                else:
+                    k -= 1
+
+        return ret
+
 class Solution(object):
     def threeSum(self, nums):
         """
