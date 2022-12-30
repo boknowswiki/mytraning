06db7263ad57f1471b14124db8982765991e6120
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-12-30 14:29:48 -0800

    016 two pointers and binary search

--- lc/python/016_3sum_closest.py
+++ lc/python/016_3sum_closest.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python -t
 
+# binary search
+# time O(n^2logn)
+# space O(1)
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        diff = float('inf')
+        nums.sort()
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                complement = target - nums[i] - nums[j]
+                hi = bisect_right(nums, complement, j + 1)
+                lo = hi - 1
+                if hi < len(nums) and abs(complement - nums[hi]) < abs(diff):
+                    diff = complement - nums[hi]
+                if lo > j and abs(complement - nums[lo]) < abs(diff):
+                    diff = complement - nums[lo]
+            if diff == 0:
+                break
+        return target - diff
+    
+# two pointers
+
 #time O(n^2) space O(1)
 
 class Solution(object):
