feffe367c2cd22e2be4f0907541228c5e3d059cd
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2022-12-31 14:59:28 -0800

    167 two pointers

--- lc/python/167_two_sum_II.py
+++ lc/python/167_two_sum_II.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python -t
 
+# two pointers
+# time O(n)
+# space O(1)
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l = 0
+        r = len(numbers)-1
+
+        while l < r:
+            val = numbers[l] + numbers[r]
+            if val == target:
+                return [l+1, r+1]
+            elif val < target:
+                l += 1
+            else:
+                r -= 1
+
+        return [-1, -1]
+
 # binary search
 # time O(n)
 # space O(1)
