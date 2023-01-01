506ae1b362854de1ebe687814c3f20f38cdeb0bb
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-01-01 14:05:38 -0800

    0277 two pointers

---
+++ lc/python/0277_find_the_celebrity.py
@@ -0,0 +1,32 @@
+
+# two pointers
+# time O(n)
+# space O(1)
+
+# The knows API is already defined for you.
+# return a bool, whether a knows b
+# def knows(a: int, b: int) -> bool:
+
+from functools import lru_cache
+
+class Solution:
+    @lru_cache(maxsize=None)
+    def cachedKnows(self, a, b):
+        return knows(a, b)
+    
+    def findCelebrity(self, n: int) -> int:
+        self.n = n
+        celebrity_candidate = 0
+        for i in range(1, n):
+            if self.cachedKnows(celebrity_candidate, i):
+                celebrity_candidate = i
+        if self.is_celebrity(celebrity_candidate):
+            return celebrity_candidate
+        return -1
+
+    def is_celebrity(self, i):
+        for j in range(self.n):
+            if i == j: continue
+            if self.cachedKnows(i, j) or not self.cachedKnows(j, i):
+                return False
+        return True
