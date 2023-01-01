2b2b1d4c34dd50549e526608fac0f461ffc83ea3
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-01-01 14:26:49 -0800

    0360 two pointers and sort

---
+++ lc/python/0360_sort_transformed_array.py
@@ -0,0 +1,51 @@
+# two pointers
+# time O(n)
+# space O(1)
+
+class Solution:
+    def sortTransformedArray(self, nums: List[int], a: int, b: int, c: int) -> List[int]:
+        def transform(x):
+            # Return the transformed result for element 'x'
+            return (a * x * x) + (b * x) + c
+
+        answer = []
+        left, right = 0, len(nums) - 1
+        
+        if a < 0:
+            # When 'downward parabola' we will put the edge element (smaller elements) first.
+            while left <= right:
+                left_transformed_val = transform(nums[left])
+                right_transformed_val = transform(nums[right])
+                if left_transformed_val < right_transformed_val:
+                    answer.append(left_transformed_val)
+                    left += 1
+                else:
+                    answer.append(right_transformed_val)
+                    right -= 1
+        else:
+            while left <= right:
+                # When 'upward parabola' or a 'straight line' 
+                # we will put the edge element (bigger elements) first.
+                left_transformed_val = transform(nums[left])
+                right_transformed_val = transform(nums[right])
+                if left_transformed_val > right_transformed_val:
+                    answer.append(left_transformed_val)
+                    left += 1
+                else:
+                    answer.append(right_transformed_val)
+                    right -= 1
+            # Reverse the decreasing 'answer' array.
+            answer.reverse()
+        return answer
+      
+      
+# sort
+class Solution:
+    def sortTransformedArray(self, nums: List[int], a: int, b: int, c: int) -> List[int]:
+        answer = []
+        for num in nums:
+            # Push transformed value in the 'answer' array.
+            answer.append((a * num * num) + (b * num) + c)
+        # Sort the array of transformed values.
+        answer.sort()
+        return answer
