0f99b383dd79b10a3e8784a418772de611257463
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-01-02 21:50:50 -0800

    0481 string and two pointers

---
+++ lc/python/0481_magical_string.py
@@ -0,0 +1,10 @@
+# string and two pointers
+
+class Solution:
+    def magicalString(self, n: int) -> int:
+        s, i = ["1", "2", "2"], 2
+        while len(s) < n:
+            s += (["1"] if s[-1] == '2' else ["2"]) * int(s[i])
+            i += 1
+
+        return s[:n].count('1')
