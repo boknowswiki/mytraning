3efeddae05b0ec81d59c0dd693f9629a2243e076
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-01-07 14:17:13 -0800

    0389 hash map and bit manipulation

--- lc/python/0389_find_the_difference.py
+++ lc/python/0389_find_the_difference.py
@@ -17,3 +17,25 @@ class Solution:
                 return k
 
         return ""
+
+# bit manipulation
+# time O(n)
+# space O(1)
+
+
+    class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+
+        # Initialize ch with 0, because 0 ^ X = X
+        # 0 when XORed with any bit would not change the bits value.
+        ch = 0
+
+        # XOR all the characters of both s and t.
+        for char_ in s:
+            ch ^= ord(char_)
+
+        for char_ in t:
+            ch ^= ord(char_)
+
+        # What is left after XORing everything is the difference.
+        return chr(ch)
