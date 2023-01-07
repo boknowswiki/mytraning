951a198498581495947871c028535ac5cfaa98d4
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-01-07 14:23:38 -0800

    0409 hash map and greedy

---
+++ lc/python/0409_longest_palindrome.py
@@ -0,0 +1,12 @@
+# hash map and greedy
+# time O(n)
+# space O(n)
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        ans = 0
+        for v in collections.Counter(s).values():
+            ans += v // 2 * 2
+            if ans % 2 == 0 and v % 2 == 1:
+                ans += 1
+        return ans
