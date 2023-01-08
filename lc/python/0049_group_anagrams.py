5d34e1445ef907988494b77e880dfbc58feb748f
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-01-08 14:37:51 -0800

    0049 hash map

--- lc/python/0049_group_anagrams.py
+++ lc/python/0049_group_anagrams.py
@@ -1,5 +1,19 @@
 
 # hash map
+# time O(nk)
+# space O(nk)
+
+class Solution:
+    def groupAnagrams(strs):
+        ans = collections.defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
+    
+# hash map
 # time O(n*nlog(len(word)))
 # space O(n)
 
