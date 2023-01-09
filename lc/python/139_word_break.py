81a9d3f99fd4b739a51f2ad82e38a840ee11136d
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-01-08 22:19:54 -0800

    139 hash map and dfs

--- lc/python/139_word_break.py
+++ lc/python/139_word_break.py
@@ -1,5 +1,64 @@
 #!/usr/bin/python -t
 
+# hash map and dfs with memo
+# time O(n^2*m)
+# space O(n)
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        if not s:
+            return False
+
+        memo = dict()
+        def dfs(index):
+            if index == len(s):
+                return True
+
+            if index in memo:
+                return memo[index]
+
+            for word in wordDict:
+                if index + len(word) <= len(s) and s[index:index+len(word)] == word:
+                    memo[index] = True
+                    if dfs(index+len(word)):
+                        return True
+
+            memo[index] = False
+            return False
+        
+        return dfs(0)
+
+
+"""
+Time: O(N^2 * M). N is the length of the s. M is the number of word in wordDict.
+Note that s[i:i+len(word)]==word takes O(N) time.
+
+Space: O(N) for the recursion memory stack size.
+
+dfs(i) := will return starting at index i, if i to the end the string can be separated.
+"""
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        def dfs(i):
+            if i==len(s): return True
+            if i in history and not history[i]: return False
+            
+            for word in wordDict:
+                if i+len(word)<=len(s) and s[i:i+len(word)]==word:
+                    history[i] = True
+                    if dfs(i+len(word)): return True
+            history[i] = False
+            return False
+        
+        history = {}
+        return dfs(0)
+"""
+For interview preparation, similar problems, check out my GitHub.
+It took me a lots of time to make the solution. Becuase I want to help others like me.
+Please give me a star if you like it. Means a lot to me.
+https://github.com/wuduhren/leetcode-python
+"""
+
 #my dp solution
 #time O(n^2) space O(n)
 
