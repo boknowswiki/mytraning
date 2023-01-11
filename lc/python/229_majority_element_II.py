5412af5e1bfcbdbb8553ab871a6a434027c960b7
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-01-10 21:44:42 -0800

    229 hash map

--- lc/python/229_majority_element_II.py
+++ lc/python/229_majority_element_II.py
@@ -1,5 +1,39 @@
 #!/usr/bin/python -t
 
+# hash map and sorting
+# time O(n)
+# space O(1)
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
+        
+        # 1st pass
+        count1, count2, candidate1, candidate2 = 0, 0, None, None
+        for n in nums:
+            if candidate1 == n:
+                count1 += 1
+            elif candidate2 == n:
+                count2 += 1
+            elif count1 == 0:
+                candidate1 = n
+                count1 += 1
+            elif count2 == 0:
+                candidate2 = n
+                count2 += 1
+            else:
+                count1 -= 1
+                count2 -= 1
+        
+        # 2nd pass
+        result = []
+        for c in [candidate1, candidate2]:
+            if nums.count(c) > len(nums)//3:
+                result.append(c)
+
+        return result
+
 class Solution(object):
     def majorityElement(self, nums):
         """
