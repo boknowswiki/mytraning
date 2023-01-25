112866ff1ca873b4b1b87d58f4d93a63e8892217
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-01-24 21:38:16 -0800

    0314 bfs and dfs and binary tree

--- lc/python/0314_binary_tree_vertical_order_traversal.py
+++ lc/python/0314_binary_tree_vertical_order_traversal.py
@@ -1,3 +1,31 @@
+
+# binary tree and bfs
+# time O(n)
+# space O(n)
+
+from collections import defaultdict
+class Solution:
+    def verticalOrder(self, root: TreeNode) -> List[List[int]]:
+        if root is None:
+            return []
+
+        columnTable = defaultdict(list)
+        min_column = max_column = 0
+        queue = deque([(root, 0)])
+
+        while queue:
+            node, column = queue.popleft()
+
+            if node is not None:
+                columnTable[column].append(node.val)
+                min_column = min(min_column, column)
+                max_column = max(max_column, column)
+
+                queue.append((node.left, column - 1))
+                queue.append((node.right, column + 1))
+
+        return [columnTable[x] for x in range(min_column, max_column + 1)]
+
 # binary tree and dfs
 # time O(n)
 # space O(n)
