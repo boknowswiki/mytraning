7d0d747fbf18562d7f85b1437a25ec1396ad1e38
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-02-07 21:24:02 -0800

    140 dfs

--- lc/python/140_word_break_II.py
+++ lc/python/140_word_break_II.py
@@ -1,5 +1,51 @@
 #!/usr/bin/python -t
 
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        def dfs(i, v):
+            if i == len(s):
+                self.a.append(v[1:])
+                return
+
+            for w in wordDict:
+                if s[i:].startswith(w):
+                    dfs(i + len(w), v + ' ' + w)
+
+        self.a = []
+        dfs(0, '')
+        return self.a
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        wordSet = set(wordDict)
+        # table to map a string to its corresponding words break
+        # {string: [['word1', 'word2'...], ['word3', 'word4', ...]]}
+        memo = defaultdict(list)
+
+        #@lru_cache(maxsize=None)    # alternative memoization solution
+        def _wordBreak_topdown(s):
+            """ return list of word lists """
+            if not s:
+                return [[]]  # list of empty list
+
+            if s in memo:
+                # returned the cached solution directly.
+                return memo[s]
+
+            for endIndex in range(1, len(s)+1):
+                word = s[:endIndex]
+                if word in wordSet:
+                    # move forwards to break the postfix into words
+                    for subsentence in _wordBreak_topdown(s[endIndex:]):
+                        memo[s].append([word] + subsentence)
+            return memo[s]
+
+        # break the input string into lists of words list
+        _wordBreak_topdown(s)
+
+        # chain up the lists of words into sentences.
+        return [" ".join(words) for words in memo[s]]
+
 # dp + dfs + backtracing
 #time O(n^2)
 
