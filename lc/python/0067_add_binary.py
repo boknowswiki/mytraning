2b34806b331e84ec631a7f125b9cbb329ac3cefb
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-02-13 21:36:03 -0800

    0067 string and bit manipulate

--- lc/python/0067_add_binary.py
+++ lc/python/0067_add_binary.py
@@ -3,6 +3,13 @@
 # space O(n)
 
 class Solution:
+    def addBinary(self, a, b) -> str:
+        x, y = int(a, 2), int(b, 2)
+        while y:
+            x, y = x ^ y, (x & y) << 1
+        return bin(x)[2:]
+
+class Solution:
     def addBinary(self, a: str, b: str) -> str:
         a_len = len(a)
         b_len = len(b)
