1a4cb6de3ee75e4bf9465f60ca57232ded6a606c
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-02-20 21:00:46 -0800

    0540 binary search

---
+++ lc/python/0540_single_element_in_a_sorted_array.py
@@ -0,0 +1,17 @@
+# binary search
+# time O(logn)
+# space O(1)
+
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        lo = 0
+        hi = len(nums) - 1
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if mid % 2 == 1:
+                mid -= 1
+            if nums[mid] == nums[mid + 1]:
+                lo = mid + 2
+            else:
+                hi = mid
+        return nums[lo]
