7ed813ba8aef603c6fa6f544a5240e760a2cac8a
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-02-26 21:03:45 -0800

    0427 divide and conqer, tree and matrix

---
+++ lc/python/0427_construct_quad_tree.py
@@ -0,0 +1,52 @@
+# divid and conquer, tree and matrix
+# time O(n2logn)
+# space O(logn)
+
+"""
+# Definition for a QuadTree node.
+class Node:
+    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+"""
+
+class Node:
+    def __init__(self, val, isLeaf, topLeft=None, topRight=None, bottomLeft=None, bottomRight=None):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+        
+class Solution:
+    def construct(self, grid: List[List[int]]) -> 'Node':
+        return self.build(grid, 0, 0, len(grid))
+    
+
+    def sameValue(self, grid, x, y, sideLength):
+        value = grid[x][y]
+        for i in range(x, x + sideLength):
+            for j in range(y, y + sideLength):
+                if grid[i][j] != value:
+                    return False
+        
+        return True
+    
+
+    def build(self, grid, x, y, sideLength):
+        if self.sameValue(grid, x, y, sideLength):
+            return Node(grid[x][y], True)
+
+        root = Node(0, False)
+        newSideLength = sideLength // 2
+        root.topLeft = self.build(grid, x, y, newSideLength)
+        root.topRight = self.build(grid, x, y + newSideLength, newSideLength)
+        root.bottomLeft = self.build(grid, x + newSideLength, y, newSideLength)
+        root.bottomRight = self.build(grid, x + newSideLength, y + newSideLength, newSideLength)
+
+        return root
