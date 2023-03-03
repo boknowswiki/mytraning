5b0eb63a4b3706f0a3341544bba9cfe55634e174
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-03-02 21:45:34 -0800

    0028 kmp

--- lc/python/0028_find_the_index_of_the_first_occurrence_in_a_string.py
+++ lc/python/0028_find_the_index_of_the_first_occurrence_in_a_string.py
@@ -1,5 +1,36 @@
 # two pointers
 
+# time O(m+n)
+# space O(m)
+
+# kmp: https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        n = len(haystack)
+        m = len(needle)
+        if m == 0:
+            return 0
+        pi = [0] * m
+        j = 0
+        for i in range(1, m):
+            while j > 0 and needle[j] != needle[i]:
+                j = pi[j - 1]
+            if needle[j] == needle[i]:
+                j += 1
+            pi[i] = j
+        j = 0
+        for i in range(n):
+            while j > 0 and needle[j] != haystack[i]:
+                j = pi[j - 1]
+            if needle[j] == haystack[i]:
+                j += 1
+            if j == m:
+                return i - m + 1
+        return -1
+
+# time O(mn)
+# space O(1)
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         m = len(haystack)
