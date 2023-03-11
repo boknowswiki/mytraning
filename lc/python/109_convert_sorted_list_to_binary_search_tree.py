f948b16a9f8ccf1914e68e3604819e6dfc04ca7d
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-03-10 21:36:44 -0800

    109 binary tree and linked list

--- lc/python/109_convert_sorted_list_to_binary_search_tree.py
+++ lc/python/109_convert_sorted_list_to_binary_search_tree.py
@@ -2,6 +2,57 @@
 
 # binary tree and linked list
 # time O(n)
+# space O(n)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+        # Form an array out of the given linked list and then
+        # use the array to form the BST.
+        values = self.mapListToValues(head)
+
+        # l and r represent the start and end of the given array
+        def convertListToBST(l, r):
+
+            # Invalid case
+            if l > r:
+                return None
+
+            # Middle element forms the root.
+            mid = (l + r) // 2
+            node = TreeNode(values[mid])
+
+            # Base case for when there is only one element left in the array
+            if l == r:
+                return node
+
+            # Recursively form BST on the two halves
+            node.left = convertListToBST(l, mid - 1)
+            node.right = convertListToBST(mid + 1, r)
+            return node
+        return convertListToBST(0, len(values) - 1)
+
+
+    # Convert the given linked list to an array
+    def mapListToValues(self, head):
+        vals = []
+        while head:
+            vals.append(head.val)
+            head = head.next
+        return vals  
+
+# binary tree and linked list
+# time O(nlogn)
 # space O(1)
 
 # Definition for singly-linked list.
