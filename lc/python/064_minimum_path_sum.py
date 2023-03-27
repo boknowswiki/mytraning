8f97d11d3188571e6c367697692c2cc8d285c7bd
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-03-26 21:06:48 -0700

    064 dp

--- lc/python/064_minimum_path_sum.py
+++ lc/python/064_minimum_path_sum.py
@@ -1,5 +1,34 @@
 #!/usr/bin/python -t
 
+# dp
+# time O(mn)
+# space O(mn)
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        # dp[i][j] the minimium sum at i,j place
+        # dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+        # init dp[i][0], dp[0][j]
+        # dp[m-1][n-1]
+        m, n = len(grid), len(grid[0])
+        if m == 0 or n == 0:
+            return 0
+
+        dp = [[0]* n for _ in range(m)]
+        dp[0][0] = grid[0][0]
+
+        for i in range(1, m):
+            dp[i][0] = dp[i-1][0] + grid[i][0]
+
+        for j in range(1, n):
+            dp[0][j] = dp[0][j-1] + grid[0][j]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+
+        return dp[m-1][n-1]
+
 class Solution(object):
     def minPathSum(self, grid):
         """
