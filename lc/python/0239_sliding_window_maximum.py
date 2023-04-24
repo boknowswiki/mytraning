f4a98d730b1f7f6b44fc9f5ddcd042903963672f
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-04-23 21:41:38 -0700

    0239 heapq, sliding window, queue

---
+++ lc/python/0239_sliding_window_maximum.py
@@ -0,0 +1,65 @@
+# heapq, sliding window
+# time O(n+nlogn)
+# space O(n)
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        l = 0
+        q = []
+        ret = []
+
+        for r in range(n):
+            heapq.heappush(q, [-nums[r], r])
+
+            if len(q) >= k:
+                ret.append(-q[0][0])
+
+            while r-l+1 == k:
+                while len(q) > 0 and q[0][1] <= l:
+                    heapq.heappop(q)
+                l += 1
+
+        return ret
+
+# queue
+# time O(n)
+# space O(n)
+
+from collections import deque
+class Solution:
+    def maxSlidingWindow(self, nums: 'List[int]', k: 'int') -> 'List[int]':
+        # base cases
+        n = len(nums)
+        if n * k == 0:
+            return []
+        if k == 1:
+            return nums
+        
+        def clean_deque(i):
+            # remove indexes of elements not from sliding window
+            if deq and deq[0] == i - k:
+                deq.popleft()
+                
+            # remove from deq indexes of all elements 
+            # which are smaller than current element nums[i]
+            while deq and nums[i] > nums[deq[-1]]:
+                deq.pop()
+        
+        # init deque and output
+        deq = deque()
+        max_idx = 0
+        for i in range(k):
+            clean_deque(i)
+            deq.append(i)
+            # compute max in nums[:k]
+            if nums[i] > nums[max_idx]:
+                max_idx = i
+        output = [nums[max_idx]]
+        
+        # build output
+        for i in range(k, n):
+            clean_deque(i)          
+            deq.append(i)
+            output.append(nums[deq[0]])
+        return output
