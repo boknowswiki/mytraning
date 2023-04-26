d8060ebba8b9fabd0fffcce8ca768eb9feead5aa
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-04-25 21:31:43 -0700

    0295 heap

--- lc/python/0295_find_median_from_data_stream.py
+++ lc/python/0295_find_median_from_data_stream.py
@@ -24,6 +24,49 @@ class MedianFinder:
           
 # heap
 
+import heapq
+
+class MedianFinder:
+
+    def __init__(self):
+        self.mid = None
+        self.min_hq = []
+        self.max_hq = []
+        
+
+    def addNum(self, num: int) -> None:
+        if self.mid is None:
+            self.mid = float(num)
+            heapq.heappush(self.max_hq, -float(num))
+            return
+
+        if float(num) > self.mid:
+            heapq.heappush(self.min_hq, float(num))
+            
+        else:
+            heapq.heappush(self.max_hq, -float(num))
+
+        if len(self.max_hq) > len(self.min_hq) + 1:
+            heapq.heappush(self.min_hq, -heapq.heappop(self.max_hq))
+        if len(self.min_hq) > len(self.max_hq):
+            heapq.heappush(self.max_hq, -heapq.heappop(self.min_hq))
+
+        if (len(self.min_hq) + len(self.max_hq)) % 2 == 0:
+            self.mid = float(self.min_hq[0] -self.max_hq[0])/2.0
+        else:
+            self.mid = -self.max_hq[0]
+            
+        return
+
+    def findMedian(self) -> float:
+        return self.mid
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
+
 class MedianFinder:
 
     def __init__(self):
