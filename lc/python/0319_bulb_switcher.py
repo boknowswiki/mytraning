af92d358863c72efa5af18510bbdafeaee91afde
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-04-26 21:05:16 -0700

    0319 math

---
+++ lc/python/0319_bulb_switcher.py
@@ -0,0 +1,8 @@
+# math
+# time O(1)
+# space O(1)
+# solution: https://leetcode.com/problems/bulb-switcher/editorial/
+
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        return int(sqrt(n))
