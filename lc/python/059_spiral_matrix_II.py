6faee852db313c6206d878734d16120741126ab1
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-05-09 21:26:22 -0700

    059 matrix, array

--- lc/python/059_spiral_matrix_II.py
+++ lc/python/059_spiral_matrix_II.py
@@ -1,5 +1,48 @@
 #!/usr/bin/python -t
 
+# matrix, array
+# time O(n^2)
+# space O(1)
+
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        ret = [[0]*n for _ in range(n)]
+
+        left = up = 0
+        right = n-1
+        down = n-1
+        idx = 1
+
+        while idx <= n*n:
+            # left to right
+            for i in range(left, right+1):
+                ret[up][i] = idx
+                idx += 1
+
+            # up to down
+            for i in range(up+1, down+1):
+                ret[i][right] = idx
+                idx += 1
+
+            # make sure on a different row
+            if up != down:
+                for i in range(right-1, left-1, -1):
+                    ret[down][i] = idx
+                    idx += 1
+
+            # make sure on different col
+            if left != right:
+                for i in range(down-1, up, -1):
+                    ret[i][left] = idx
+                    idx += 1
+
+            left += 1
+            right -= 1
+            up += 1
+            down -= 1
+
+        return ret
+
 #time O(n) space O(1)
 
 class Solution(object):
