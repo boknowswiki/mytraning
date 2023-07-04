6eecbaeea235789b22fc7ea5150533228552f75d
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-07-03 21:56:43 -0700

    137 array, bit manipulations

--- lc/python/137_single_number_II.py
+++ lc/python/137_single_number_II.py
@@ -1,5 +1,34 @@
 #!/usr/bin/python -t
 
+# array, bit manipulations
+# time O(n)
+# space O(1)
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # Loner. 
+        loner = 0
+        
+        # Iterate over all bits
+        for shift in range(32):
+            bit_sum = 0
+            
+            # For this bit, iterate over all integers
+            for num in nums:
+                
+                # Compute the bit of num, and add it to bit_sum
+                bit_sum += (num >> shift) & 1
+            
+            # Compute the bit of loner and place it
+            loner_bit = bit_sum % 3
+            loner = loner | (loner_bit << shift)
+
+        # Do not mistaken sign bit for MSB.
+        if loner >= (1 << 31):
+            loner = loner - (1 << 32)
+        
+        return loner
+
 class Solution(object):
     def singleNumber(self, nums):
         """
