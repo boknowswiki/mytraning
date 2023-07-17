d4026617f4192eb2bcca81712b0e95f29c3d81c0
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-07-16 21:54:08 -0700

    445 linked list, stack

--- lc/python/445_add_two_numbers_II.py
+++ lc/python/445_add_two_numbers_II.py
@@ -1,5 +1,87 @@
 #!/usr/bin/python -t
 
+# linked list
+# time O(n)
+# space O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse(head):
+            prev = None
+
+            while head:
+                nxt = head.next
+                head.next = prev
+                prev = head
+                head = nxt
+
+            return prev
+
+        l1_rev = reverse(l1)
+        l2_rev = reverse(l2)
+
+        dummy = ListNode()
+        prev = dummy
+        c = 0
+
+        while l1_rev or l2_rev:
+            a = l1_rev.val if l1_rev else 0
+            b = l2_rev.val if l2_rev else 0
+
+            val = (a+b+c) % 10
+            c = (a+b+c) // 10
+            prev.next = ListNode(val=val)
+            prev = prev.next
+            if l1_rev:
+                l1_rev = l1_rev.next
+            if l2_rev:
+                l2_rev = l2_rev.next
+
+        if c:
+            prev.next = ListNode(val=c)
+            prev = prev.next
+
+
+        return reverse(dummy.next)
+
+# stack
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        s1 = []
+        s2 = []
+
+        while l1:
+            s1.append(l1.val)
+            l1 = l1.next
+        while l2:
+            s2.append(l2.val)
+            l2 = l2.next
+
+        total_sum = 0
+        carry = 0
+        ans = ListNode()
+        while s1 or s2:
+            if s1:
+                total_sum += s1.pop()
+            if s2:
+                total_sum += s2.pop()
+
+            ans.val = total_sum % 10
+            carry = total_sum // 10
+            head = ListNode(carry)
+            head.next = ans
+            ans = head
+            total_sum = carry
+
+        return ans.next if carry == 0 else ans
+
+        
 # Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, x):
