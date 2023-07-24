b0febd9ef92c6be8cfbd342863fe97bc08ab433a
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-07-23 21:22:28 -0700

    50 math

--- lc/python/050_pow_x_n.py
+++ lc/python/050_pow_x_n.py
@@ -1,5 +1,62 @@
 #!/usr/bin/python -t
 
+# time O(logn)
+# space O(logn)
+
+class Solution:
+    def binaryExp(self, x: float, n: int) -> float:
+        # Base case, to stop recursive calls.
+        if n == 0:
+            return 1
+       
+        # Handle case where, n < 0.
+        if n < 0:
+            return 1.0 / self.binaryExp(x, -1 * n)
+       
+        # Perform Binary Exponentiation.
+        # If 'n' is odd we perform Binary Exponentiation on 'n - 1' and multiply result with 'x'.
+        if n % 2 == 1:
+            return x * self.binaryExp(x * x, (n - 1) // 2)
+        # Otherwise we calculate result by performing Binary Exponentiation on 'n'.
+        else:
+            return self.binaryExp(x * x, n // 2)
+
+    def myPow(self, x: float, n: int) -> float:
+        return self.binaryExp(x, n)
+
+
+# math
+# time O(n)
+# space O(1)
+
+
+# time limitaion exceeded with following case:
+# x = 0.00001, n = 2147483647
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        ret = 1
+        multiple = True
+        if n == 0:
+            return 1
+        if n > 0:
+            multiple = True
+        else:
+            multiple = False
+
+        #print(f"multiple {multiple}")
+        n = abs(n)
+
+        for i in range(n):
+            if multiple:
+                ret *= x
+            else:
+                ret /= x
+
+            #print(f"ret {ret}")
+
+        return ret
+
 #time O(logn) space O(logn)
 
 class Solution(object):
