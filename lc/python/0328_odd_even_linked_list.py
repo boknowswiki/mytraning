a04d5b4f649256fcd2d1b25c775378f146df1024
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-08-05 21:29:40 -0700

    328 linked list

--- lc/python/0328_odd_even_linked_list.py
+++ lc/python/0328_odd_even_linked_list.py
@@ -10,6 +10,30 @@
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
+            return head
+
+        odd = head
+        even = head.next
+        even_head = even
+
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+
+        odd.next = even_head
+
+        return head
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
             return None
         
         odd_dummy = ListNode()
