f96c7678873918a4525c9447a76943e6985bb136
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-08-06 17:24:29 -0700

    74 binary search

--- lc/python/074_search_a_2D_matrix.py
+++ lc/python/074_search_a_2D_matrix.py
@@ -3,6 +3,28 @@
 # binary search
 #Time O(logmn) space O(1)
 
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        if m == 0:
+            return False
+
+        n = len(matrix[0])
+
+        l, r = 0, m*n-1
+
+        while l <= r:
+            mid = (l+r)//2
+            val = matrix[mid//n][mid%n]
+            if val == target:
+                return True
+            elif val < target:
+                l = mid+1
+            else:
+                r = mid-1
+
+        return False
+
 class Solution(object):
     def searchMatrix(self, matrix, target):
         """
