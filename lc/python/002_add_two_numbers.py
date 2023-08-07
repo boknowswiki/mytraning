eb8438e7f3cba20a2373a4799d13fcf751219eb8
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-08-06 17:10:49 -0700

    2 linked list

--- lc/python/002_add_two_numbers.py
+++ lc/python/002_add_two_numbers.py
@@ -10,6 +10,30 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode()
+        cur = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            total = v1+v2+carry
+            carry = total//10
+            node = ListNode(total%10)
+            cur.next = node
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
         prev = dummy
         c = 0
 
