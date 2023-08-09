0e5638218c3ca5924faa3960a9d70a1c8ff92e0b
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-08-08 21:24:09 -0700

    14 array, string

--- lc/python/014_longest_common_prefix.py
+++ lc/python/014_longest_common_prefix.py
@@ -1,5 +1,33 @@
 #!/usr/bin/python -t
 
+# array, string
+# time O(mn)
+# space O(1)
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        n = len(strs)
+        if n == 0:
+            return ""
+        if n == 1:
+            return strs[0]
+
+        ret = ""
+        min_len = min([len(s) for s in strs])
+        break_flag = False
+
+        for i in range(min_len):
+            for j in range(1, n):
+                if strs[j][i] != strs[j-1][i]:
+                    break_flag = True
+                    break
+                if j == n-1:
+                    ret += strs[j][i]
+
+            if break_flag:
+                break
+        return ret
+
 #time O(S) space O(1)
 #myself solution
 
