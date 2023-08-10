d75ea861c95db597b49b05e86c5727b051be0627
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-08-09 21:16:36 -0700

    81 binary search

--- lc/python/081_search_in_rotated_sorted_array_II.py
+++ lc/python/081_search_in_rotated_sorted_array_II.py
@@ -1,5 +1,39 @@
 #!/usr/bin/python -t
 
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        n = len(nums)
+        if n == 0:
+            return False
+        if n == 1:
+            return nums[0] == target
+
+        l = 0
+        r = n-1
+
+        while l <= r:
+            while l < r and nums[l] == nums[l+1]:
+                l += 1
+            while l < r and nums[r] == nums[r-1]:
+                r -= 1
+
+            mid = (l+r)//2
+
+            if nums[mid] == target:
+                return True
+            elif nums[l] <= nums[mid]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid+1
+                else:
+                    r = mid-1
+
+        return False
+
 # binary search
 # time O(logn)
 # space O(1)
