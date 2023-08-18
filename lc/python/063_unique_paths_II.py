da243cc54a33a10c07711083422bb785fc2a6171
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-08-18 11:03:19 -0700

    63 dp

--- lc/python/063_unique_paths_II.py
+++ lc/python/063_unique_paths_II.py
@@ -1,5 +1,34 @@
 #!/usr/bin/python -t
 
+# dp
+# time O(m*n)
+# space O(m*n)
+
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m,n = len(obstacleGrid), len(obstacleGrid[0])
+        if m == 0:
+            return 0
+
+        dp = [[0]*n for _ in range(m)]
+
+        for i in range(m):
+            if obstacleGrid[i][0] == 1:
+                break
+            dp[i][0] = 1
+
+        for j in range(n):
+            if obstacleGrid[0][j] == 1:
+                break
+            dp[0][j] = 1
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j] != 1:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+        return dp[m-1][n-1]
+
 class Solution(object):
     def uniquePathsWithObstacles(self, obstacleGrid):
         """
