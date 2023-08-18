5c7ba5eae50f9c64eee1b9723bd523ce5b332205
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-08-17 22:34:31 -0700

    1615 graph

---
+++ lc/python/1615_maximal_network_rank.py
@@ -0,0 +1,24 @@
+# graph
+# Here, E is the number of edges and V is the number of nodes in our graph respectively
+# time O(E+V^2)
+# space O(E)
+
+class Solution:
+    def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
+        maxRank = 0
+        adj = defaultdict(set)
+        # Construct adjency list 'adj', where adj[node] stores all nodes connected to 'node'.
+        for road in roads:
+            adj[road[0]].add(road[1])
+            adj[road[1]].add(road[0])
+
+        # Iterate on each possible pair of nodes.
+        for node1 in range(n):
+            for node2 in range(node1 + 1, n):
+                currentRank = len(adj[node1]) + len(adj[node2])
+                if node2 in adj[node1]:
+                    currentRank -= 1
+                # Find the current pair's respective network rank and store if it's maximum till now.
+                maxRank = max(maxRank, currentRank)
+        # Return the maximum network rank.
+        return maxRank
