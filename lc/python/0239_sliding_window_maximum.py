e63209c8e91a56b66419414f964986cd67e9f4cc
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-08-18 21:53:21 -0700

    239 heapq, sliding window

--- lc/python/0239_sliding_window_maximum.py
+++ lc/python/0239_sliding_window_maximum.py
@@ -1,4 +1,31 @@
 # heapq, sliding window
+# time O(n)
+# space O(k)
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        dq = deque()
+        res = []
+
+        for i in range(k):
+            while dq and nums[i] >= nums[dq[-1]]:
+                dq.pop()
+            dq.append(i)
+
+        res.append(nums[dq[0]])
+
+        for i in range(k, len(nums)):
+            if dq and dq[0] == i - k:
+                dq.popleft()
+            while dq and nums[i] >= nums[dq[-1]]:
+                dq.pop()
+
+            dq.append(i)
+            res.append(nums[dq[0]])
+
+        return res
+
+
 # time O(n+nlogn)
 # space O(n)
 
