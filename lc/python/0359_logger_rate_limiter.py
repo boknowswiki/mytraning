65df2afdab6dc1aa9ded4d9533f5fafc34534cf5
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-08-24 22:05:34 -0700

    359 hash table, queue

--- lc/python/0359_logger_rate_limiter.py
+++ lc/python/0359_logger_rate_limiter.py
@@ -18,3 +18,35 @@ class Logger:
             return True
 
         return False
+
+# queue and hash table
+
+from collections import deque
+
+class Logger(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self._msg_set = set()
+        self._msg_queue = deque()
+    
+    def shouldPrintMessage(self, timestamp, message):
+        """
+        Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        """
+        while self._msg_queue:
+            msg, ts = self._msg_queue[0]
+            if timestamp - ts >= 10:
+                self._msg_queue.popleft()
+                self._msg_set.remove(msg)
+            else:
+                break
+        
+        if message not in self._msg_set:
+            self._msg_set.add(message)
+            self._msg_queue.append((message, timestamp))
+            return True
+        else:
+            return False
