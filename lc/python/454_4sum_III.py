40d9add7146e5af7da61beadd55439fdb6496860
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-08-25 23:20:19 -0700

    454 hash table

--- lc/python/454_4sum_III.py
+++ lc/python/454_4sum_III.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python -t
 
+# hash table
+# time O(n^2)
+# space O(n)
+
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        cnt = 0
+        m = collections.defaultdict(int)
+        for a in nums1:
+            for b in nums2:
+                m[a + b] += 1
+                
+        for c in nums3:
+            for d in nums4:
+                cnt += m[-(c + d)]
+                
+        return cnt
+
 class Solution(object):
     def fourSumCount(self, A, B, C, D):
         """
