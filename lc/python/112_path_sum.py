608c1a353053646a9c9bd331be218d60f4c9356a
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-08-29 21:42:55 -0700

    112 binary tree

--- lc/python/112_path_sum.py
+++ lc/python/112_path_sum.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python -t
 
+# binary tree
+
+class Solution:
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+
+        de = [(root, sum - root.val), ]
+        while de:
+            node, curr_sum = de.pop()
+            if not node.left and not node.right and curr_sum == 0:  
+                return True
+            if node.right:
+                de.append((node.right, curr_sum - node.right.val))
+            if node.left:
+                de.append((node.left, curr_sum - node.left.val))
+        return False
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
