56098b011a39e25d296ccd6990829913c4ade88c
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-09-03 22:19:40 -0700

    98 bst, dfs, iteration, stack

--- lc/python/098_validate_binary_search_tree.py
+++ lc/python/098_validate_binary_search_tree.py
@@ -11,6 +11,35 @@ class Solution:
         if not root:
             return True
 
+        st = []
+        prev = -sys.maxsize-1
+        cur = root
+
+        while cur or st:
+            while cur:
+                st.append(cur)
+                cur = cur.left
+
+            cur = st.pop()
+            if cur.val <= prev:
+                return False
+
+            prev = cur.val
+            cur = cur.right
+
+        return True
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
         st = [(root, -sys.maxsize-1, sys.maxsize)]
 
         while st:
