5bea08388cd5ac532816e9ea970ba9de5b874217
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-09-18 21:23:45 -0700

    287 binary search

--- lc/python/287_find_the_duplicate_number.py
+++ lc/python/287_find_the_duplicate_number.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python -t
 
+# time O(n)
+# space O(1)
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        fast = slow = nums[0]
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        slow2 = nums[0]
+
+        while slow != slow2:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+
+        return slow        
+
 # binary search
 # time O(nlogn)
 # space O(1)
