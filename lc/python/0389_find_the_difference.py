502fd119e9255fce5ca00c4faf4f3e7a623ee019
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-09-24 21:12:04 -0700

    389 bit mask, hash table

--- lc/python/0389_find_the_difference.py
+++ lc/python/0389_find_the_difference.py
@@ -1,3 +1,19 @@
+# bit mask
+# time O(n)
+# space O(1)
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        ret = 0
+
+        for c in s:
+            ret ^= ord(c)
+
+        for c in t:
+            ret ^= ord(c)
+
+        return chr(ret)
+
 # hash map
 # time O(n)
 # space O(n)
