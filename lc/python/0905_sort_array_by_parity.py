c513f694e027aaaf3a54df52589b20c73a355695
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-09-27 20:52:03 -0700

    905 array, two pointers

--- lc/python/0905_sort_array_by_parity.py
+++ lc/python/0905_sort_array_by_parity.py
@@ -4,21 +4,20 @@
 
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        n = len(nums)
         l = 0
-        r = n-1
-        
-        while l <= r:
-            while l <= r and nums[l] % 2 == 0:
-                l += 1
+        r = len(nums)-1
 
-            while l <= r and nums[r] % 2 == 1:
+        while l < r:
+            while l < r and nums[l] % 2 == 0:
+                l += 1
+            
+            while l < r and nums[r] % 2 != 0:
                 r -= 1
 
-            if l <= r:
+            if l < r:
                 nums[l], nums[r] = nums[r], nums[l]
-            l += 1
-            r -= 1
+                l += 1
+                r -= 1
 
         return nums
 
