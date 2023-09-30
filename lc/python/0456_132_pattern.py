097cef919a879a50f1ffa451a2353781dcc51a97
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-09-29 22:15:01 -0700

    456 stack

--- lc/python/0456_132_pattern.py
+++ lc/python/0456_132_pattern.py
@@ -7,6 +7,34 @@ class Solution:
         n = len(nums)
         if n < 3:
             return False
+
+        st = []
+        min_array = [-1] * n
+        min_array[0] = nums[0]
+
+        for i in range(1, n):
+            min_array[i] = min(min_array[i-1], nums[i])
+
+        for j in range(n-1, -1, -1):
+            if nums[j] <= min_array[j]:
+                continue
+
+            while st and st[-1] <= min_array[j]:
+                st.pop()
+
+            if st and st[-1] < nums[j]:
+                return True
+
+            st.append(nums[j])
+
+        return False
+
+
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        n = len(nums)
+        if n < 3:
+            return False
         
         st = []
         second_num = -sys.maxsize-1
