6e1129c0744449a5f7bf635160fc9c40b46552c9
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-10-17 21:53:10 -0700

    285 dfs

--- lc/python/0285_inorder_successor_in_bst.py
+++ lc/python/0285_inorder_successor_in_bst.py
@@ -11,6 +11,44 @@
 
 class Solution:
     def inorderSuccessor(self, root: TreeNode, p: TreeNode) -> Optional[TreeNode]:
+        prev, successor = None, None
+
+        def dfs(node):
+            nonlocal prev, successor, p
+            if not node:
+                return
+
+            dfs(node.left)
+
+            if prev == p and not successor:
+                successor = node
+                return
+
+            prev = node
+
+            dfs(node.right)
+
+        if p.right:
+            left_most = p.right
+
+            while left_most.left:
+                left_most = left_most.left
+
+            successor = left_most
+        else:
+            dfs(root)
+
+        return successor
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderSuccessor(self, root: TreeNode, p: TreeNode) -> Optional[TreeNode]:
         if not root:
             return None
         self.ret = None
