fcb855156d94aa12b7987aeaa29f9093202b6ad2
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2023-10-26 21:32:07 -0700

    5 dp

--- lc/python/005_longest_palindromic_substring.py
+++ lc/python/005_longest_palindromic_substring.py
@@ -1,5 +1,33 @@
 #!/usr/bin/python -t
 
+# dp
+# time O(n^2)
+# space O(n^2)
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp =[[False]*n for _ in range(n)]
+        
+        ans = [0, 0]
+        for i in range(n):
+            dp[i][i] = True
+        
+        for i in range(n-1):
+            if s[i] == s[i+1]:
+                dp[i][i+1] = True
+                ans = [i, i+1]
+
+        for diff in range(2, n):
+            for i in range(n-diff):
+                j = i+diff
+                if s[i] == s[j] and dp[i+1][j-1]:
+                    dp[i][j] = True
+                    ans = [i, j]
+
+        i, j = ans
+        return s[i:j+1]
+
 class Solution(object):
     def longestPalindrome(self, s):
         """
