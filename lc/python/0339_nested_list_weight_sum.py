9faba3f50212b709f84af023cf5db6cb272b7db1
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2024-03-10 21:19:44 -0700

    Update 0339_nested_list_weight_sum.py
    queue and dfs

--- lc/python/0339_nested_list_weight_sum.py
+++ lc/python/0339_nested_list_weight_sum.py
@@ -1,3 +1,23 @@
+# queue
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        queue = deque(nestedList)
+
+        depth = 1
+        total = 0
+
+        while len(queue) > 0:
+            for i in range(len(queue)):
+                nested = queue.pop()
+                if nested.isInteger():
+                    total += nested.getInteger() * depth
+                else:
+                    queue.extendleft(nested.getList())
+            depth += 1
+
+        return total
+
 # dfs
 # time O(n)
 # space O(1)
