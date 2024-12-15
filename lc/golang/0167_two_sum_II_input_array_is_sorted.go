c7ee3cac0c46bebf4b58e030ca71b0ea431d9fab
Bo Tao <25923501+boknowswiki@users.noreply.github.com>
2024-12-14 19:25:52 -0800

    Update 0167_two_sum_II_input_array_is_sorted.go

--- lc/golang/0167_two_sum_II_input_array_is_sorted.go
+++ lc/golang/0167_two_sum_II_input_array_is_sorted.go
@@ -16,3 +16,38 @@ func twoSum(numbers []int, target int) []int {
 
     return []int{-1, -1}
 }
+
+
+// time O(nlogn)
+// space O(1)
+
+func bsearch(numbers []int, target int, start int) int {
+    end := len(numbers)-1
+
+    for start < end {
+        mid := (start+end)/2
+        if target == numbers[mid] {
+            return mid
+        } else if target > numbers[mid] {
+            start = mid+1
+        } else {
+            end = mid-1
+        }
+    }
+
+    if (start == end) && (target == numbers[start]) {
+        return start
+    }
+    return -1
+}
+
+func twoSum(numbers []int, target int) []int {
+    for i := 0; i < len(numbers); i++ {
+        j := bsearch(numbers, target - numbers[i], i+1)
+        if j != -1 {
+            return []int {i+1, j+1}
+        }
+    }
+
+    return []int{-1, -1}
+}
